df9ae763ecd3f61773539dd666811e3adba3cbb5
Florent Kermarrec <florent@enjoy-digital.fr>
2015-05-02 15:09:07 +0200

    software: only keep test_wishbone.py (software is now integrated in MiSoC)

---
+++ software/test_wishbone.py
@@ -0,0 +1,12 @@
+from misoclib.com.liteusb.software.wishbone import LiteUSBWishboneDriver
+
+FTDI_INTERFACE_A = 1
+FTDI_INTERFACE_B = 2
+
+wb = LiteUSBWishboneDriver("ft2232h", FTDI_INTERFACE_B, "asynchronous", 2, debug=False)
+wb.open()
+for i in range(64):
+    wb.write(0xe0003000, i)
+for i in range(64):
+    print("%08x" %wb.read(4*i))
+wb.close()
