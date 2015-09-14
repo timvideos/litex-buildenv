70d371c54dc18f8162bbdfb2cd09065b2b7e0fb4
Florent Kermarrec <florent@enjoy-digital.fr>
2015-09-14 14:14:03 +0200

    targets: reduce firmware_ram and HDMIIn fifo_depth on opsis (should fix the build)

--- targets/opsis_hdmi2usb.py
+++ targets/opsis_hdmi2usb.py
@@ -27,10 +27,10 @@ class VideomixerSoC(opsis_base_soc):
         opsis_base_soc.__init__(self, platform, **kwargs)
         self.submodules.hdmi_in0 = HDMIIn(platform.request("hdmi_in", 0),
                                           self.sdram.crossbar.get_master(),
-                                          fifo_depth=1024)
+                                          fifo_depth=512)
         self.submodules.hdmi_in1 = HDMIIn(platform.request("hdmi_in", 1),
                                           self.sdram.crossbar.get_master(),
-                                          fifo_depth=1024)
+                                          fifo_depth=512)
         self.submodules.hdmi_out0 = HDMIOut(platform.request("hdmi_out", 0),
                                             self.sdram.crossbar.get_master())
         self.submodules.hdmi_out1 = HDMIOut(platform.request("hdmi_out", 1),
