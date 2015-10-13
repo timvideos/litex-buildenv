63a4e904b2c03b3c712a15851c9a3656c94030b8
Yann Sionneau <yann@sionneau.net>
2015-10-13 19:54:21 +0200

    Some fixes + PEP8 cleanup

--- targets/arty_logic_analyzer.py
+++ targets/arty_logic_analyzer.py
@@ -1,29 +1,31 @@
 from targets.arty_base import *
 
 from liteeth.common import *
-from liteeth.phy import LiteEthPHY
 from liteeth.phy.mii import LiteEthPHYMII
 from liteeth.core import LiteEthUDPIPCore
 from liteeth.frontend.etherbone import LiteEthEtherbone
 
-from hdl.streamer import UDPStreamer
+from migen.fhdl.specials import Keep
+
 
 class EtherboneSoC(BaseSoC):
     csr_map = {
-        "ethphy":  18,
-        "ethcore": 19,
+        "ethphy":  20,
+        "ethcore": 21,
     }
     csr_map.update(BaseSoC.csr_map)
 
-    def __init__(self, platform,
-        mac_address=0x10e2d5000000,
-        ip_address="192.168.1.42",
-        **kwargs):
+    def __init__(self, platform, mac_address=0x10e2d5000000,
+                 ip_address="192.168.1.42", **kwargs):
         BaseSoC.__init__(self, platform, **kwargs)
 
         # Ethernet PHY and UDP/IP stack
-        self.submodules.ethphy = LiteEthPHYMII(platform.request("eth_clocks"), platform.request("eth"))
-        self.submodules.ethcore = LiteEthUDPIPCore(self.ethphy, mac_address, convert_ip(ip_address), self.clk_freq, with_icmp=False)
+        self.submodules.ethphy = LiteEthPHYMII(platform.request("eth_clocks"),
+                                               platform.request("eth"))
+        self.submodules.ethcore = LiteEthUDPIPCore(self.ethphy, mac_address,
+                                                   convert_ip(ip_address),
+                                                   self.clk_freq,
+                                                   with_icmp=False)
 
         # Etherbone bridge
         self.submodules.etherbone = LiteEthEtherbone(self.ethcore.udp, 20000)
@@ -45,24 +47,27 @@ TIMESPEC "TSise_sucks4" = FROM "GRPsys_clk" TO "GRPeth_tx_clk" TIG;
 TIMESPEC "TSise_sucks5" = FROM "GRPeth_rx_clk" TO "GRPsys_clk" TIG;
 TIMESPEC "TSise_sucks6" = FROM "GRPsys_clk" TO "GRPeth_rx_clk" TIG;
 """, eth_clocks_rx=platform.lookup_request("eth_clocks").rx,
-     eth_rx_clk=self.ethphy.crg.cd_eth_rx.clk,
-     eth_tx_clk=self.ethphy.crg.cd_eth_tx.clk)
+                                      eth_rx_clk=self.ethphy.crg.cd_eth_rx.clk,
+                                      eth_tx_clk=self.ethphy.crg.cd_eth_tx.clk)
 
 
 class LogicAnalyzerSoC(EtherboneSoC):
     csr_map = {
-        "logic_analyzer": 20
+        "logic_analyzer": 22
     }
     csr_map.update(EtherboneSoC.csr_map)
-    def __init__(self, platform):
+
+    def __init__(self, platform, **kwargs):
         from litescope.frontend.logic_analyzer import LiteScopeLogicAnalyzer
         from litescope.core.port import LiteScopeTerm
-        EtherboneSoC.__init__(self, platform)
+        EtherboneSoC.__init__(self, platform, **kwargs)
         debug = (
             platform.request("logic_analyzer_ios") # TODO
         )
         self.submodules.logic_analyzer = LiteScopeLogicAnalyzer(debug, 4096)
-        self.logic_analyzer.trigger.add_port(LiteScopeTerm(self.logic_analyzer.dw))
+        self.logic_analyzer.trigger.add_port(
+            LiteScopeTerm(self.logic_analyzer.dw)
+        )
 
     def do_exit(self, vns):
         self.logic_analyzer.export(vns, "software/logic_analyzer.csv")
