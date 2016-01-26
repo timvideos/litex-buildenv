timvideos/litex-buildenv

commit ca3f01c0db490ed459143aa49c8a60e42e25a90d

targets: Move common stuff in targets files to a common file.

targets/atlys_base.py
@@ -19,7 +19,7 @@ from liteeth.phy.mii import LiteEthPHYMII
 from liteeth.core.mac import LiteEthMAC
 
 from gateware import dna
-
+from targets.common import *
 
 class _CRG(Module):
     def __init__(self, platform, clk_freq):
@@ -110,22 +110,6 @@ class _CRG(Module):
         platform.add_period_constraint(self.cd_base50.clk, 20)
 
 
-def _get_firmware_data(firmware_filename):
-    data = []
-    try:
-        with open(firmware_filename, "rb") as firmware_file:
-            while True:
-                w = firmware_file.read(4)
-                if not w:
-                    break
-                data.append(struct.unpack(">I", w)[0])
-    except:
-        pass
-    return data
-
-def csr_map_update(csr_map, csr_peripherals):
-  csr_map.update(dict((n, v) for v, n in enumerate(csr_peripherals, start=max(csr_map.values()) + 1)))
-
 class BaseSoC(SDRAMSoC):
     default_platform = "atlys"
 
@@ -153,7 +137,7 @@ class BaseSoC(SDRAMSoC):
         self.submodules.crg = _CRG(platform, clk_freq)
         self.submodules.dna = dna.DNA()
 
-        self.submodules.firmware_ram = wishbone.SRAM(firmware_ram_size, init=_get_firmware_data(firmware_filename))
+        self.submodules.firmware_ram = wishbone.SRAM(firmware_ram_size, init=get_firmware_data(firmware_filename))
         self.register_mem("firmware_ram", self.mem_map["firmware_ram"], self.firmware_ram.bus, firmware_ram_size)
         self.add_constant("ROM_BOOT_ADDRESS", self.mem_map["firmware_ram"])
 

targets/common.py
@@ -1,0 +1,18 @@
+
+def get_firmware_data(firmware_filename):
+    data = []
+    try:
+        with open(firmware_filename, "rb") as firmware_file:
+            while True:
+                w = firmware_file.read(4)
+                if not w:
+                    break
+                data.append(struct.unpack(">I", w)[0])
+    except:
+        pass
+    return data
+
+
+def csr_map_update(csr_map, csr_peripherals):
+  csr_map.update(dict((n, v) for v, n in enumerate(csr_peripherals, start=max(csr_map.values()) + 1)))
+

targets/opsis_base.py
@@ -21,18 +21,7 @@ from liteeth.core.mac import LiteEthMAC
 from gateware import dna
 from gateware import i2c
 from gateware import i2c_hack
-
-class CSRMap(dict):
-    def __init__(self, offset, values):
-        dict.__init__(self)
-        self.offset = offset
-        for k, v in values.items():
-            self[k] = v
-
-    def __setitem__(self, k, v=None):
-        if v is None:
-            v = self.offset + len(self)
-        dict.__setitem__(self, k, v)
+from targets.common import *
 
 
 class _CRG(Module):
@@ -127,23 +116,6 @@ class _CRG(Module):
         platform.add_period_constraint(self.cd_base50.clk, 20)
 
 
-def _get_firmware_data(firmware_filename):
-    data = []
-    try:
-        with open(firmware_filename, "rb") as firmware_file:
-            while True:
-                w = firmware_file.read(4)
-                if not w:
-                    break
-                data.append(struct.unpack(">I", w)[0])
-    except:
-        pass
-    return data
-
-def csr_map_update(csr_map, csr_peripherals):
-  csr_map.update(dict((n, v) for v, n in enumerate(csr_peripherals, start=max(csr_map.values()) + 1)))
-
-
 class BaseSoC(SDRAMSoC):
     default_platform = "opsis"
 
@@ -180,7 +152,7 @@ class BaseSoC(SDRAMSoC):
 
         self.submodules.tofe_eeprom_i2c = i2c.I2C(platform.request("tofe_eeprom"))
 
-        self.submodules.firmware_ram = wishbone.SRAM(firmware_ram_size, init=_get_firmware_data(firmware_filename))
+        self.submodules.firmware_ram = wishbone.SRAM(firmware_ram_size, init=get_firmware_data(firmware_filename))
         self.register_mem("firmware_ram", self.mem_map["firmware_ram"], self.firmware_ram.bus, firmware_ram_size)
         self.add_constant("ROM_BOOT_ADDRESS", self.mem_map["firmware_ram"])
 

targets/pipistrello_base.py
@@ -1,5 +1,3 @@
-from gateware.hdmi_out import HDMIOut
-
 from fractions import Fraction
 import struct
 
@@ -16,6 +14,9 @@ from misoclib.soc.sdram import SDRAMSoC
 
 from gateware import dna
 from gateware import i2c_hack
+from gateware.hdmi_out import HDMIOut
+from targets.common import *
+
 
 class _CRG(Module):
     def __init__(self, platform, clk_freq):
@@ -101,20 +102,6 @@ class _CRG(Module):
                                   o_Q=clk.n)
 
 
-def _get_firmware_data(firmware_filename):
-    data = []
-    try:
-        with open(firmware_filename, "rb") as firmware_file:
-            while True:
-                w = firmware_file.read(4)
-                if not w:
-                    break
-                data.append(struct.unpack(">I", w)[0])
-    except:
-        pass
-    return data
-
-
 from mibuild.generic_platform import *
 PipistrelloCustom = [
     ("fx2_hack", 0,
@@ -124,9 +111,6 @@ PipistrelloCustom = [
     ),
     ("fx2_reset", 0, Pins("K13"), IOStandard("LVCMOS33")), #, Misc("PULLUP")),
 ]
-
-def csr_map_update(csr_map, csr_peripherals):
-  csr_map.update(dict((n, v) for v, n in enumerate(csr_peripherals, start=max(csr_map.values()) + 1)))
 
 
 class BaseSoC(SDRAMSoC):
@@ -161,7 +145,7 @@ class BaseSoC(SDRAMSoC):
         self.submodules.fx2_reset = gpio.GPIOOut(platform.request("fx2_reset"))
         self.submodules.fx2_hack = i2c_hack.I2CShiftReg(platform.request("fx2_hack"))
 
-        self.submodules.firmware_ram = wishbone.SRAM(firmware_ram_size, init=_get_firmware_data(firmware_filename))
+        self.submodules.firmware_ram = wishbone.SRAM(firmware_ram_size, init=get_firmware_data(firmware_filename))
         self.register_mem("firmware_ram", self.mem_map["firmware_ram"], self.firmware_ram.bus, firmware_ram_size)
         self.add_constant("ROM_BOOT_ADDRESS", self.mem_map["firmware_ram"])
 

targets/atlys_edid_debug.py
@@ -1,3 +1,4 @@
+from targets.common import *
 from targets.atlys_hdmi2usb import *
 
 from misoclib.com.uart.phy import UARTPHY

targets/atlys_hdmi2eth.py
@@ -1,3 +1,4 @@
+from targets.common import *
 from targets.atlys_base import *
 
 from liteeth.common import *

targets/opsis_hdmi2usb.py
@@ -1,3 +1,4 @@
+from targets.common import *
 from targets.opsis_base import *
 from targets.opsis_base import default_subtarget as BaseSoC
 

commit 254c281dc669f174de47855aff002f1d91f94f4e

gateware/targets: Make firmware a real gateware object.
 * Fixes romboot which was broken in (ca3f01c0db490ed459143aa49c8a60e42e25a90d)
   * Bare exceptions are *evil* :(

 * Make sure firmware fits in rom.

gateware/firmware.py
@@ -1,0 +1,40 @@
+import os
+import struct
+
+from migen.fhdl.std import *
+from migen.bus import wishbone
+
+
+class MemoryMustHaveContents(Memory):
+    @staticmethod
+    def emit_verilog(memory, ns, add_data_file):
+        assert memory.init, "ROM contents not found! {}".format(memory.filename)
+        return Memory.emit_verilog(memory, ns, add_data_file)
+
+
+class FirmwareROM(wishbone.SRAM):
+    def __init__(self, size, filename):
+        if os.path.exists(filename):
+            data = []
+            with open(filename, "rb") as firmware_file:
+                while True:
+                    w = firmware_file.read(4)
+                    if not w:
+                        break
+                    data.append(struct.unpack(">I", w)[0])
+            data_size = len(data)*4
+            assert data_size > 0
+            assert data_size < size, (
+                "Firmware is too big! {} bytes > {} bytes".format(
+                    data_size, size))
+            print("Firmware {} bytes ({} bytes left)".format(
+                data_size, size-data_size))
+            wishbone.SRAM.__init__(self, size, init=data)
+        else:
+            print("No firmware found! ({}) Won't compile.".format(
+                filename))
+            wishbone.SRAM.__init__(self, size)
+
+        self.mem.__class__ = MemoryMustHaveContents
+        self.mem.filename = filename
+

targets/atlys_base.py
@@ -19,6 +19,8 @@ from liteeth.phy.mii import LiteEthPHYMII
 from liteeth.core.mac import LiteEthMAC
 
 from gateware import dna
+from gateware import firmware
+
 from targets.common import *
 
 class _CRG(Module):
@@ -140,7 +142,7 @@ class BaseSoC(SDRAMSoC):
         self.submodules.crg = _CRG(platform, clk_freq)
         self.submodules.dna = dna.DNA()
 
-        self.submodules.firmware_ram = wishbone.SRAM(firmware_ram_size, init=get_firmware_data(firmware_filename))
+        self.submodules.firmware_ram = firmware.FirmwareROM(firmware_ram_size, firmware_filename)
         self.register_mem("firmware_ram", self.mem_map["firmware_ram"], self.firmware_ram.bus, firmware_ram_size)
         self.add_constant("ROM_BOOT_ADDRESS", self.mem_map["firmware_ram"])
 

targets/common.py
@@ -1,17 +1,5 @@
-
-def get_firmware_data(firmware_filename):
-    data = []
-    try:
-        with open(firmware_filename, "rb") as firmware_file:
-            while True:
-                w = firmware_file.read(4)
-                if not w:
-                    break
-                data.append(struct.unpack(">I", w)[0])
-    except:
-        pass
-    return data
-
+import os
+import struct
 
 def csr_map_update(csr_map, csr_peripherals):
   csr_map.update(dict((n, v) for v, n in enumerate(csr_peripherals, start=max(csr_map.values()) + 1)))

targets/opsis_base.py
@@ -19,8 +19,10 @@ from liteeth.phy.s6rgmii import LiteEthPHYRGMII
 from liteeth.core.mac import LiteEthMAC
 
 from gateware import dna
+from gateware import firmware
 from gateware import i2c
 from gateware import i2c_hack
+
 from targets.common import *
 
 
@@ -156,7 +158,7 @@ class BaseSoC(SDRAMSoC):
 
         self.submodules.tofe_eeprom_i2c = i2c.I2C(platform.request("tofe_eeprom"))
 
-        self.submodules.firmware_ram = wishbone.SRAM(firmware_ram_size, init=get_firmware_data(firmware_filename))
+        self.submodules.firmware_ram = firmware.FirmwareROM(firmware_ram_size, firmware_filename)
         self.register_mem("firmware_ram", self.mem_map["firmware_ram"], self.firmware_ram.bus, firmware_ram_size)
         self.add_constant("ROM_BOOT_ADDRESS", self.mem_map["firmware_ram"])
 

targets/pipistrello_base.py
@@ -13,8 +13,10 @@ from misoclib.mem.flash import spiflash
 from misoclib.soc.sdram import SDRAMSoC
 
 from gateware import dna
+from gateware import firmware
+from gateware import hdmi_out
 from gateware import i2c_hack
-from gateware.hdmi_out import HDMIOut
+
 from targets.common import *
 
 
@@ -145,7 +147,7 @@ class BaseSoC(SDRAMSoC):
         self.submodules.fx2_reset = gpio.GPIOOut(platform.request("fx2_reset"))
         self.submodules.fx2_hack = i2c_hack.I2CShiftReg(platform.request("fx2_hack"))
 
-        self.submodules.firmware_ram = wishbone.SRAM(firmware_ram_size, init=get_firmware_data(firmware_filename))
+        self.submodules.firmware_ram = firmware.FirmwareROM(firmware_ram_size, firmware_filename)
         self.register_mem("firmware_ram", self.mem_map["firmware_ram"], self.firmware_ram.bus, firmware_ram_size)
         self.add_constant("ROM_BOOT_ADDRESS", self.mem_map["firmware_ram"])
 
@@ -185,8 +187,8 @@ class VideomixerSoC(BaseSoC):
 
     def __init__(self, platform, **kwargs):
         BaseSoC.__init__(self, platform, **kwargs)
-        self.submodules.hdmi_out0 = HDMIOut(platform.request("hdmi", 0),
-                                            self.sdram.crossbar.get_master())
+        self.submodules.hdmi_out0 = hdmi_out.HDMIOut(
+            platform.request("hdmi", 0), self.sdram.crossbar.get_master())
 
         for k, v in _hdmi_infos.items():
             self.add_constant(k, v)
254c281dc669f174de47855aff002f1d91f94f4e
Tim 'mithro' Ansell <mithro@mithis.com>
2016-01-26 19:41:46 +1100

    gateware/targets: Make firmware a real gateware object.
     * Fixes romboot which was broken in (ca3f01c0db490ed459143aa49c8a60e42e25a90d)
       * Bare exceptions are *evil* :(
    
     * Make sure firmware fits in rom.

---
+++ gateware/firmware.py
@@ -0,0 +1,40 @@
+import os
+import struct
+
+from migen.fhdl.std import *
+from migen.bus import wishbone
+
+
+class MemoryMustHaveContents(Memory):
+    @staticmethod
+    def emit_verilog(memory, ns, add_data_file):
+        assert memory.init, "ROM contents not found! {}".format(memory.filename)
+        return Memory.emit_verilog(memory, ns, add_data_file)
+
+
+class FirmwareROM(wishbone.SRAM):
+    def __init__(self, size, filename):
+        if os.path.exists(filename):
+            data = []
+            with open(filename, "rb") as firmware_file:
+                while True:
+                    w = firmware_file.read(4)
+                    if not w:
+                        break
+                    data.append(struct.unpack(">I", w)[0])
+            data_size = len(data)*4
+            assert data_size > 0
+            assert data_size < size, (
+                "Firmware is too big! {} bytes > {} bytes".format(
+                    data_size, size))
+            print("Firmware {} bytes ({} bytes left)".format(
+                data_size, size-data_size))
+            wishbone.SRAM.__init__(self, size, init=data)
+        else:
+            print("No firmware found! ({}) Won't compile.".format(
+                filename))
+            wishbone.SRAM.__init__(self, size)
+
+        self.mem.__class__ = MemoryMustHaveContents
+        self.mem.filename = filename
+
