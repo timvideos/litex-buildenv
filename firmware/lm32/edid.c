timvideos/litex-buildenv

commit 102c2269db0eda1b60f26f05069e7c3a5f93bead

firmware/lm32: Make all strings end in \r\n rather then just \n.
Fixes #168.

This doesn't change the misoc BIOS however. Most people should never get to
that prompt.

firmware/lm32/edid.c
@@ -128,7 +128,7 @@ void get_monitor_name(const void *buf, char *name)
 
 	name[MAX_MONITOR_NAME_LEN] = 0;
 	memcpy(name, &data_block[5], MAX_MONITOR_NAME_LEN);
-	c = strchr(name, '\n');
+	c = strchr(name, '\r\n');
 	if(c)
 		*c = 0;
 }

firmware/lm32/ci.c
@@ -28,7 +28,7 @@ void print_board_dna(void) {
 	for(i=0; i<CSR_DNA_ID_SIZE; i++) {
 		printf("%02x", MMPTR(CSR_DNA_ID_ADDR+4*i));
 	}
-	printf("\n");
+	printf("\r\n");
 }
 
 static void help_video_matrix(void)
@@ -121,8 +121,8 @@ static void help(void)
 
 static void version(void)
 {
-	printf("gateware revision: %08x\n", identifier_revision_read());
-	printf("firmware revision: %08x, built "__DATE__" "__TIME__"\n", MSC_GIT_ID);
+	printf("gateware revision: %08x\r\n", identifier_revision_read());
+	printf("firmware revision: %08x, built "__DATE__" "__TIME__"\r\n", MSC_GIT_ID);
 }
 
 static void reboot(void)
@@ -132,7 +132,7 @@ static void reboot(void)
 
 static void status_enable(void)
 {
-	printf("Enabling status\n");
+	printf("Enabling status\r\n");
 	status_enabled = 1;
 #ifdef ENCODER_BASE
 	encoder_bandwidth_nbytes_clear_write(1);
@@ -141,7 +141,7 @@ static void status_enable(void)
 
 static void status_disable(void)
 {
-	printf("Disabling status\n");
+	printf("Disabling status\r\n");
 	status_enabled = 0;
 }
 
@@ -154,7 +154,7 @@ static void status_print(void)
 		"input0:  %dx%d",
 		hdmi_in0_resdetection_hres_read(),
 		hdmi_in0_resdetection_vres_read());
-	printf("\n");
+	printf("\r\n");
 #endif
 
 #ifdef CSR_HDMI_IN1_BASE
@@ -162,7 +162,7 @@ static void status_print(void)
 		"input1:  %dx%d",
 		hdmi_in1_resdetection_hres_read(),
 		hdmi_in1_resdetection_vres_read());
-	printf("\n");
+	printf("\r\n");
 #endif
 
 #ifdef CSR_HDMI_OUT0_BASE
@@ -176,7 +176,7 @@ static void status_print(void)
 			processor_get_source_name(processor_hdmi_out0_source));
 	else
 		printf("off");
-	printf("\n");
+	printf("\r\n");
 #endif
 
 #ifdef CSR_HDMI_OUT1_BASE
@@ -190,7 +190,7 @@ static void status_print(void)
 			processor_get_source_name(processor_hdmi_out1_source));
 	else
 		printf("off");
-	printf("\n");
+	printf("\r\n");
 #endif
 
 #ifdef ENCODER_BASE
@@ -207,7 +207,7 @@ static void status_print(void)
 		encoder_bandwidth_nbytes_clear_write(1);
 	} else
 		printf("off");
-	printf("\n");
+	printf("\r\n");
 #endif
 	printf("ddr: ");
 	debug_ddr();
@@ -220,37 +220,37 @@ static void status_service(void)
 	if(elapsed(&last_event, identifier_frequency_read())) {
 		if(status_enabled) {
 			status_print();
-			printf("\n");
+			printf("\r\n");
 		}
 	}
 }
 
 static void video_matrix_list(void)
 {
-	printf("Video sources:\n");
+	printf("Video sources:\r\n");
 #ifdef CSR_HDMI_IN0_BASE
-	printf("input0: %s\n", HDMI_IN0_MNEMONIC);
+	printf("input0: %s\r\n", HDMI_IN0_MNEMONIC);
 	puts(HDMI_IN0_DESCRIPTION);
 #endif
 #ifdef CSR_HDMI_IN1_BASE
-	printf("input1: %s\n", HDMI_IN1_MNEMONIC);
+	printf("input1: %s\r\n", HDMI_IN1_MNEMONIC);
 	puts(HDMI_IN1_DESCRIPTION);
 #endif
-	printf("pattern:\n");
-	printf("  Video pattern\n");
+	printf("pattern:\r\n");
+	printf("  Video pattern\r\n");
 	puts(" ");
-	printf("Video sinks:\n");
+	printf("Video sinks:\r\n");
 #ifdef CSR_HDMI_OUT0_BASE
-	printf("output0: %s\n", HDMI_OUT0_MNEMONIC);
+	printf("output0: %s\r\n", HDMI_OUT0_MNEMONIC);
 	puts(HDMI_OUT0_DESCRIPTION);
 #endif
 #ifdef CSR_HDMI_OUT1_BASE
-	printf("output1: %s\n", HDMI_OUT1_MNEMONIC);
+	printf("output1: %s\r\n", HDMI_OUT1_MNEMONIC);
 	puts(HDMI_OUT1_DESCRIPTION);
 #endif
 #ifdef ENCODER_BASE
-	printf("encoder:\n");
-	printf("  JPEG Encoder\n");
+	printf("encoder:\r\n");
+	printf("  JPEG Encoder\r\n");
 #endif
 	puts(" ");
 }
@@ -260,24 +260,24 @@ static void video_matrix_connect(int source, int sink)
 	if(source >= 0 && source <= VIDEO_IN_PATTERN)
 	{
 		if(sink >= 0 && sink <= VIDEO_OUT_HDMI_OUT1) {
-			printf("Connecting %s to output%d\n", processor_get_source_name(source), sink);
+			printf("Connecting %s to output%d\r\n", processor_get_source_name(source), sink);
 			if(sink == VIDEO_OUT_HDMI_OUT0)
 #ifdef CSR_HDMI_OUT0_BASE
 				processor_set_hdmi_out0_source(source);
 #else
-				printf("hdmi_out0 is missing.\n");
+				printf("hdmi_out0 is missing.\r\n");
 #endif
 			else if(sink == VIDEO_OUT_HDMI_OUT1)
 #ifdef CSR_HDMI_OUT1_BASE
 				processor_set_hdmi_out1_source(source);
 #else
-				printf("hdmi_out1 is missing.\n");
+				printf("hdmi_out1 is missing.\r\n");
 #endif
 			processor_update();
 		}
 #ifdef ENCODER_BASE
 		else if(sink == VIDEO_OUT_ENCODER) {
-			printf("Connecting %s to encoder\n", processor_get_source_name(source));
+			printf("Connecting %s to encoder\r\n", processor_get_source_name(source));
 			processor_set_encoder_source(source);
 			processor_update();
 		}
@@ -291,10 +291,10 @@ static void video_mode_list(void)
 	int i;
 
 	processor_list_modes(mode_descriptors);
-	printf("Available video modes:\n");
+	printf("Available video modes:\r\n");
 	for(i=0;i<PROCESSOR_MODE_COUNT;i++)
-		printf("mode %d: %s\n", i, &mode_descriptors[i*PROCESSOR_MODE_DESCLEN]);
-	printf("\n");
+		printf("mode %d: %s\r\n", i, &mode_descriptors[i*PROCESSOR_MODE_DESCLEN]);
+	printf("\r\n");
 }
 
 static void video_mode_set(int mode)
@@ -302,7 +302,7 @@ static void video_mode_set(int mode)
 	char mode_descriptors[PROCESSOR_MODE_COUNT*PROCESSOR_MODE_DESCLEN];
 	if(mode < PROCESSOR_MODE_COUNT) {
 		processor_list_modes(mode_descriptors);
-		printf("Setting video mode to %s\n", &mode_descriptors[mode*PROCESSOR_MODE_DESCLEN]);
+		printf("Setting video mode to %s\r\n", &mode_descriptors[mode*PROCESSOR_MODE_DESCLEN]);
 		config_set(CONFIG_KEY_RESOLUTION, mode);
 		processor_start(mode);
 	}
@@ -313,7 +313,7 @@ static void hdp_toggle(int source)
 #if defined(CSR_HDMI_IN0_BASE) || defined(CSR_HDMI_IN1_BASE)
 	int i;
 #endif
-	printf("Toggling HDP on output%d\n", source);
+	printf("Toggling HDP on output%d\r\n", source);
 #ifdef CSR_HDMI_IN0_BASE
 	if(source ==  VIDEO_IN_HDMI_IN0) {
 		hdmi_in0_edid_hpd_en_write(0);
@@ -321,7 +321,7 @@ static void hdp_toggle(int source)
 		hdmi_in0_edid_hpd_en_write(1);
 	}
 #else
-	printf("hdmi_in0 is missing.\n");
+	printf("hdmi_in0 is missing.\r\n");
 #endif
 #ifdef CSR_HDMI_IN1_BASE
 	if(source == VIDEO_IN_HDMI_IN1) {
@@ -330,20 +330,20 @@ static void hdp_toggle(int source)
 		hdmi_in1_edid_hpd_en_write(1);
 	}
 #else
-	printf("hdmi_in1 is missing.\n");
+	printf("hdmi_in1 is missing.\r\n");
 #endif
 }
 
 #ifdef CSR_HDMI_OUT0_BASE
 static void output0_on(void)
 {
-	printf("Enabling output0\n");
+	printf("Enabling output0\r\n");
 	hdmi_out0_fi_enable_write(1);
 }
 
 static void output0_off(void)
 {
-	printf("Disabling output0\n");
+	printf("Disabling output0\r\n");
 	hdmi_out0_fi_enable_write(0);
 }
 #endif
@@ -351,13 +351,13 @@ static void output0_off(void)
 #ifdef CSR_HDMI_OUT1_BASE
 static void output1_on(void)
 {
-	printf("Enabling output1\n");
+	printf("Enabling output1\r\n");
 	hdmi_out1_fi_enable_write(1);
 }
 
 static void output1_off(void)
 {
-	printf("Disabling output1\n");
+	printf("Disabling output1\r\n");
 	hdmi_out1_fi_enable_write(0);
 }
 #endif
@@ -365,20 +365,20 @@ static void output1_off(void)
 #ifdef ENCODER_BASE
 static void encoder_on(void)
 {
-	printf("Enabling encoder\n");
+	printf("Enabling encoder\r\n");
 	encoder_enable(1);
 }
 
 static void encoder_configure_quality(int quality)
 {
-	printf("Setting encoder quality to %d\n", quality);
+	printf("Setting encoder quality to %d\r\n", quality);
 	encoder_set_quality(quality);
 }
 
 
 static void encoder_off(void)
 {
-	printf("Disabling encoder\n");
+	printf("Disabling encoder\r\n");
 	encoder_enable(0);
 }
 #endif
@@ -409,7 +409,7 @@ static void debug_ddr(void)
 	burstbits = (2*DFII_NPHASES) << DFII_PIX_DATA_SIZE;
 	rdb = (nr*f >> (24 - log2(burstbits)))/1000000ULL;
 	wrb = (nw*f >> (24 - log2(burstbits)))/1000000ULL;
-	printf("read:%5dMbps  write:%5dMbps  all:%5dMbps\n", rdb, wrb, rdb + wrb);
+	printf("read:%5dMbps  write:%5dMbps  all:%5dMbps\r\n", rdb, wrb, rdb + wrb);
 }
 
 static char *readstr(void)
@@ -434,7 +434,7 @@ static char *readstr(void)
 			case '\r':
 			case '\n':
 				s[ptr] = 0x00;
-				putsnonl("\n");
+				putsnonl("\r\n");
 				ptr = 0;
 				return s;
 			default:
@@ -529,7 +529,7 @@ void ci_service(void)
 			else if(strcmp(token, "pattern") == 0)
 				source = VIDEO_IN_PATTERN;
 			else
-				printf("Unknown video source: '%s'\n", token);
+				printf("Unknown video source: '%s'\r\n", token);
 
 			/* get video sink */
 			token = get_token(&str);
@@ -541,7 +541,7 @@ void ci_service(void)
 			else if(strcmp(token, "encoder") == 0)
 				sink = VIDEO_OUT_ENCODER;
 			else
-				printf("Unknown video sink: '%s'\n", token);
+				printf("Unknown video sink: '%s'\r\n", token);
 
 			if (source >= 0 && sink >= 0)
 				video_matrix_connect(source, sink);
@@ -656,7 +656,7 @@ void ci_service(void)
 			}
 #endif
 			if(found == 0)
-				printf("%s port has no EDID capabilities\n", token);
+				printf("%s port has no EDID capabilities\r\n", token);
 		} else
 			help_debug();
 	} else {

firmware/lm32/encoder.c
@@ -154,7 +154,7 @@ int encoder_set_quality(int quality) {
 			encoder_quality = quality;
 			break;
 		default:
-			printf("Unsupported encoder quality (50, 75, 85 or 100)\n");
+			printf("Unsupported encoder quality (50, 75, 85 or 100)\r\n");
 			return 0;
 	}
 	return 1;

firmware/lm32/fx2.c
@@ -38,7 +38,7 @@ static inline uint8_t fx2_fw_get_value(size_t addr) {
 #endif
 		}
 	} else {
-		printf("fx2: Read from invalid address %02X (end: %02X)\n", addr, end_addr);
+		printf("fx2: Read from invalid address %02X (end: %02X)\r\n", addr, end_addr);
 	}
 	return r;
 }
@@ -68,7 +68,7 @@ static void fx2_load_init(void)
 static void fx2_load(void)
 {
 	fx2_load_init();
-	printf("fx2: Waiting for FX2 to load firmware.\n");
+	printf("fx2: Waiting for FX2 to load firmware.\r\n");
 
 	uint64_t i = 0;
 	while((i < FX2_WAIT_PERIOD)) {
@@ -79,13 +79,13 @@ static void fx2_load(void)
 				break;
 			}
 		} else if ((i % FX2_REPORT_PERIOD) == 0) {
-			printf("fx2: Waiting at %02X (end: %02X)\n", next_read_addr, end_addr);
+			printf("fx2: Waiting at %02X (end: %02X)\r\n", next_read_addr, end_addr);
 		}
 	}
 	if (i > 0) {
-		printf("fx2: Timeout loading!\n");
+		printf("fx2: Timeout loading!\r\n");
 	} else {
-		printf("fx2: Booted.\n");
+		printf("fx2: Booted.\r\n");
 	}
 }
 
@@ -94,7 +94,7 @@ bool fx2_service(bool verbose)
 	unsigned char status = fx2_hack_status_read();
 	if(status == FX2_HACK_SHIFT_REG_EMPTY) { // there's been a master READ
 		if (verbose) {
-			printf("fx2: read %02X (end: %02X)\n", next_read_addr, end_addr);
+			printf("fx2: read %02X (end: %02X)\r\n", next_read_addr, end_addr);
 		}
 		if (next_read_addr < end_addr) {
 			// Load next value into the system
@@ -102,12 +102,12 @@ bool fx2_service(bool verbose)
 			fx2_hack_status_write(FX2_HACK_STATUS_READY);
 			next_read_addr++;
 		} else {
-			printf("fx2: Finished loading firmware.\n");
+			printf("fx2: Finished loading firmware.\r\n");
 			fx2_load_init();
 		}
 		return true;
 	} else if (status != 0) {
-		printf("fx2: Bad status %02X\n", status);
+		printf("fx2: Bad status %02X\r\n", status);
 	}
 	return false;
 }
@@ -115,19 +115,19 @@ bool fx2_service(bool verbose)
 void fx2_reboot(enum fx2_fw_version fw)
 {
 	fx2_fw_active = fw;
-	printf("fx2: Turning off.\n");
+	printf("fx2: Turning off.\r\n");
 	fx2_reset_out_write(0);
 	fx2_wait();
 	fx2_reset_out_write(1);
-	printf("fx2: Turning on.\n");
+	printf("fx2: Turning on.\r\n");
 	fx2_load();
 }
 
 void fx2_debug(void) {
-	printf("Possible FX2 Firmware:\n");
-	printf(" [%s] USB JTAG (%02X)\n", fx2_fw_active == FX2FW_USBJTAG ? "*" : " ", FX2_MBFW_USBJTAG_END);
+	printf("Possible FX2 Firmware:\r\n");
+	printf(" [%s] USB JTAG (%02X)\r\n", fx2_fw_active == FX2FW_USBJTAG ? "*" : " ", FX2_MBFW_USBJTAG_END);
 #ifdef ENCODER_BASE
-	printf(" [%s] HDMI2USB (%02X)\n", fx2_fw_active == FX2FW_HDMI2USB ? "*" : " ", FX2_MBFW_HDMI2USB_END);
+	printf(" [%s] HDMI2USB (%02X)\r\n", fx2_fw_active == FX2FW_HDMI2USB ? "*" : " ", FX2_MBFW_HDMI2USB_END);
 #endif
 }
 

firmware/lm32/hdmi_in0.c
@@ -44,10 +44,10 @@ void hdmi_in0_isr(void)
 	address_max = address_min + HDMI_IN0_FRAMEBUFFERS_SIZE*FRAMEBUFFER_COUNT;
 	if((hdmi_in0_dma_slot0_status_read() == DVISAMPLER_SLOT_PENDING)
 		&& ((hdmi_in0_dma_slot0_address_read() < address_min) || (hdmi_in0_dma_slot0_address_read() > address_max)))
-		printf("dvisampler0: slot0: stray DMA\n");
+		printf("dvisampler0: slot0: stray DMA\r\n");
 	if((hdmi_in0_dma_slot1_status_read() == DVISAMPLER_SLOT_PENDING)
 		&& ((hdmi_in0_dma_slot1_address_read() < address_min) || (hdmi_in0_dma_slot1_address_read() > address_max)))
-		printf("dvisampler0: slot1: stray DMA\n");
+		printf("dvisampler0: slot1: stray DMA\r\n");
 
 	if((hdmi_in0_resdetection_hres_read() != hdmi_in0_hres)
 	  || (hdmi_in0_resdetection_vres_read() != hdmi_in0_vres)) {
@@ -71,7 +71,7 @@ void hdmi_in0_isr(void)
 			hdmi_in0_fb_slot_indexes[0] = hdmi_in0_next_fb_index;
 			hdmi_in0_next_fb_index = (hdmi_in0_next_fb_index + 1) & FRAMEBUFFER_MASK;
 		} else
-			printf("dvisampler0: slot0: unexpected frame length: %d\n", length);
+			printf("dvisampler0: slot0: unexpected frame length: %d\r\n", length);
 		hdmi_in0_dma_slot0_address_write(hdmi_in0_framebuffer_base(hdmi_in0_fb_slot_indexes[0]));
 		hdmi_in0_dma_slot0_status_write(DVISAMPLER_SLOT_LOADED);
 	}
@@ -82,7 +82,7 @@ void hdmi_in0_isr(void)
 			hdmi_in0_fb_slot_indexes[1] = hdmi_in0_next_fb_index;
 			hdmi_in0_next_fb_index = (hdmi_in0_next_fb_index + 1) & FRAMEBUFFER_MASK;
 		} else
-			printf("dvisampler0: slot1: unexpected frame length: %d\n", length);
+			printf("dvisampler0: slot1: unexpected frame length: %d\r\n", length);
 		hdmi_in0_dma_slot1_address_write(hdmi_in0_framebuffer_base(hdmi_in0_fb_slot_indexes[1]));
 		hdmi_in0_dma_slot1_status_write(DVISAMPLER_SLOT_LOADED);
 	}
@@ -151,7 +151,7 @@ void hdmi_in0_print_status(void)
 	hdmi_in0_data0_wer_update_write(1);
 	hdmi_in0_data1_wer_update_write(1);
 	hdmi_in0_data2_wer_update_write(1);
-	printf("dvisampler0: ph:%4d %4d %4d // charsync:%d%d%d [%d %d %d] // WER:%3d %3d %3d // chansync:%d // res:%dx%d\n",
+	printf("dvisampler0: ph:%4d %4d %4d // charsync:%d%d%d [%d %d %d] // WER:%3d %3d %3d // chansync:%d // res:%dx%d\r\n",
 		hdmi_in0_d0, hdmi_in0_d1, hdmi_in0_d2,
 		hdmi_in0_data0_charsync_char_synced_read(),
 		hdmi_in0_data1_charsync_char_synced_read(),
@@ -177,7 +177,7 @@ static int wait_idelays(void)
 	  || hdmi_in0_data1_cap_dly_busy_read()
 	  || hdmi_in0_data2_cap_dly_busy_read()) {
 		if(elapsed(&ev, identifier_frequency_read() >> 6) == 0) {
-			printf("dvisampler0: IDELAY busy timeout\n");
+			printf("dvisampler0: IDELAY busy timeout\r\n");
 			return 0;
 		}
 	}
@@ -283,16 +283,16 @@ int hdmi_in0_phase_startup(void)
 		attempts++;
 		hdmi_in0_calibrate_delays();
 		if(hdmi_in0_debug)
-			printf("dvisampler0: delays calibrated\n");
+			printf("dvisampler0: delays calibrated\r\n");
 		ret = hdmi_in0_init_phase();
 		if(ret) {
 			if(hdmi_in0_debug)
-				printf("dvisampler0: phase init OK\n");
+				printf("dvisampler0: phase init OK\r\n");
 			return 1;
 		} else {
-			printf("dvisampler0: phase init failed\n");
+			printf("dvisampler0: phase init failed\r\n");
 			if(attempts > 3) {
-				printf("dvisampler0: giving up\n");
+				printf("dvisampler0: giving up\r\n");
 				hdmi_in0_calibrate_delays();
 				return 0;
 			}
@@ -303,7 +303,7 @@ int hdmi_in0_phase_startup(void)
 static void hdmi_in0_check_overflow(void)
 {
 	if(hdmi_in0_frame_overflow_read()) {
-		printf("dvisampler0: FIFO overflow\n");
+		printf("dvisampler0: FIFO overflow\r\n");
 		hdmi_in0_frame_overflow_write(1);
 	}
 }
@@ -338,7 +338,7 @@ void hdmi_in0_service(void)
 	if(hdmi_in0_connected) {
 		if(!hdmi_in0_edid_hpd_notif_read()) {
 			if(hdmi_in0_debug)
-				printf("dvisampler0: disconnected\n");
+				printf("dvisampler0: disconnected\r\n");
 			hdmi_in0_connected = 0;
 			hdmi_in0_locked = 0;
 			hdmi_in0_clocking_pll_reset_write(1);
@@ -353,14 +353,14 @@ void hdmi_in0_service(void)
 					}
 				} else {
 					if(hdmi_in0_debug)
-						printf("dvisampler0: lost PLL lock\n");
+						printf("dvisampler0: lost PLL lock\r\n");
 					hdmi_in0_locked = 0;
 					hdmi_in0_clear_framebuffers();
 				}
 			} else {
 				if(hdmi_in0_clocking_locked_filtered()) {
 					if(hdmi_in0_debug)
-						printf("dvisampler0: PLL locked\n");
+						printf("dvisampler0: PLL locked\r\n");
 					hdmi_in0_phase_startup();
 					if(hdmi_in0_debug)
 						hdmi_in0_print_status();
@@ -371,7 +371,7 @@ void hdmi_in0_service(void)
 	} else {
 		if(hdmi_in0_edid_hpd_notif_read()) {
 			if(hdmi_in0_debug)
-				printf("dvisampler0: connected\n");
+				printf("dvisampler0: connected\r\n");
 			hdmi_in0_connected = 1;
 			hdmi_in0_clocking_pll_reset_write(0);
 		}

firmware/lm32/hdmi_in1.c
@@ -44,10 +44,10 @@ void hdmi_in1_isr(void)
 	address_max = address_min + HDMI_IN1_FRAMEBUFFERS_SIZE*FRAMEBUFFER_COUNT;
 	if((hdmi_in1_dma_slot0_status_read() == DVISAMPLER_SLOT_PENDING)
 		&& ((hdmi_in1_dma_slot0_address_read() < address_min) || (hdmi_in1_dma_slot0_address_read() > address_max)))
-		printf("dvisampler1: slot0: stray DMA\n");
+		printf("dvisampler1: slot0: stray DMA\r\n");
 	if((hdmi_in1_dma_slot1_status_read() == DVISAMPLER_SLOT_PENDING)
 		&& ((hdmi_in1_dma_slot1_address_read() < address_min) || (hdmi_in1_dma_slot1_address_read() > address_max)))
-		printf("dvisampler1: slot1: stray DMA\n");
+		printf("dvisampler1: slot1: stray DMA\r\n");
 
 	if((hdmi_in1_resdetection_hres_read() != hdmi_in1_hres)
 	  || (hdmi_in1_resdetection_vres_read() != hdmi_in1_vres)) {
@@ -71,7 +71,7 @@ void hdmi_in1_isr(void)
 			hdmi_in1_fb_slot_indexes[0] = hdmi_in1_next_fb_index;
 			hdmi_in1_next_fb_index = (hdmi_in1_next_fb_index + 1) & FRAMEBUFFER_MASK;
 		} else
-			printf("dvisampler1: slot0: unexpected frame length: %d\n", length);
+			printf("dvisampler1: slot0: unexpected frame length: %d\r\n", length);
 		hdmi_in1_dma_slot0_address_write(hdmi_in1_framebuffer_base(hdmi_in1_fb_slot_indexes[0]));
 		hdmi_in1_dma_slot0_status_write(DVISAMPLER_SLOT_LOADED);
 	}
@@ -82,7 +82,7 @@ void hdmi_in1_isr(void)
 			hdmi_in1_fb_slot_indexes[1] = hdmi_in1_next_fb_index;
 			hdmi_in1_next_fb_index = (hdmi_in1_next_fb_index + 1) & FRAMEBUFFER_MASK;
 		} else
-			printf("dvisampler1: slot1: unexpected frame length: %d\n", length);
+			printf("dvisampler1: slot1: unexpected frame length: %d\r\n", length);
 		hdmi_in1_dma_slot1_address_write(hdmi_in1_framebuffer_base(hdmi_in1_fb_slot_indexes[1]));
 		hdmi_in1_dma_slot1_status_write(DVISAMPLER_SLOT_LOADED);
 	}
@@ -152,7 +152,7 @@ void hdmi_in1_print_status(void)
 	hdmi_in1_data0_wer_update_write(1);
 	hdmi_in1_data1_wer_update_write(1);
 	hdmi_in1_data2_wer_update_write(1);
-	printf("dvisampler1: ph:%4d %4d %4d // charsync:%d%d%d [%d %d %d] // WER:%3d %3d %3d // chansync:%d // res:%dx%d\n",
+	printf("dvisampler1: ph:%4d %4d %4d // charsync:%d%d%d [%d %d %d] // WER:%3d %3d %3d // chansync:%d // res:%dx%d\r\n",
 		hdmi_in1_d0, hdmi_in1_d1, hdmi_in1_d2,
 		hdmi_in1_data0_charsync_char_synced_read(),
 		hdmi_in1_data1_charsync_char_synced_read(),
@@ -178,7 +178,7 @@ static int wait_idelays(void)
 	  || hdmi_in1_data1_cap_dly_busy_read()
 	  || hdmi_in1_data2_cap_dly_busy_read()) {
 		if(elapsed(&ev, identifier_frequency_read() >> 6) == 0) {
-			printf("dvisampler1: IDELAY busy timeout\n");
+			printf("dvisampler1: IDELAY busy timeout\r\n");
 			return 0;
 		}
 	}
@@ -284,16 +284,16 @@ int hdmi_in1_phase_startup(void)
 		attempts++;
 		hdmi_in1_calibrate_delays();
 		if(hdmi_in1_debug)
-			printf("dvisampler1: delays calibrated\n");
+			printf("dvisampler1: delays calibrated\r\n");
 		ret = hdmi_in1_init_phase();
 		if(ret) {
 			if(hdmi_in1_debug)
-				printf("dvisampler1: phase init OK\n");
+				printf("dvisampler1: phase init OK\r\n");
 			return 1;
 		} else {
-			printf("dvisampler1: phase init failed\n");
+			printf("dvisampler1: phase init failed\r\n");
 			if(attempts > 3) {
-				printf("dvisampler1: giving up\n");
+				printf("dvisampler1: giving up\r\n");
 				hdmi_in1_calibrate_delays();
 				return 0;
 			}
@@ -304,7 +304,7 @@ int hdmi_in1_phase_startup(void)
 static void hdmi_in1_check_overflow(void)
 {
 	if(hdmi_in1_frame_overflow_read()) {
-		printf("dvisampler1: FIFO overflow\n");
+		printf("dvisampler1: FIFO overflow\r\n");
 		hdmi_in1_frame_overflow_write(1);
 	}
 }
@@ -339,7 +339,7 @@ void hdmi_in1_service(void)
 	if(hdmi_in1_connected) {
 		if(!hdmi_in1_edid_hpd_notif_read()) {
 			if(hdmi_in1_debug)
-				printf("dvisampler1: disconnected\n");
+				printf("dvisampler1: disconnected\r\n");
 			hdmi_in1_connected = 0;
 			hdmi_in1_locked = 0;
 			hdmi_in1_clocking_pll_reset_write(1);
@@ -354,14 +354,14 @@ void hdmi_in1_service(void)
 					}
 				} else {
 					if(hdmi_in1_debug)
-						printf("dvisampler1: lost PLL lock\n");
+						printf("dvisampler1: lost PLL lock\r\n");
 					hdmi_in1_locked = 0;
 					hdmi_in1_clear_framebuffers();
 				}
 			} else {
 				if(hdmi_in1_clocking_locked_filtered()) {
 					if(hdmi_in1_debug)
-						printf("dvisampler1: PLL locked\n");
+						printf("dvisampler1: PLL locked\r\n");
 					hdmi_in1_phase_startup();
 					if(hdmi_in1_debug)
 						hdmi_in1_print_status();
@@ -372,7 +372,7 @@ void hdmi_in1_service(void)
 	} else {
 		if(hdmi_in1_edid_hpd_notif_read()) {
 			if(hdmi_in1_debug)
-				printf("dvisampler1: connected\n");
+				printf("dvisampler1: connected\r\n");
 			hdmi_in1_connected = 1;
 			hdmi_in1_clocking_pll_reset_write(0);
 		}

firmware/lm32/hdmi_out0.c
@@ -22,42 +22,42 @@ void hdmi_out0_print_edid(void) {
     i2c_start_cond(&hdmi_out0_i2c);
     b = i2c_write(&hdmi_out0_i2c, 0xa0);
     if (!b && hdmi_out0_debug_enabled)
-        printf("hdmi_out0: NACK while writing slave address!\n");
+        printf("hdmi_out0: NACK while writing slave address!\r\n");
     b = i2c_write(&hdmi_out0_i2c, 0x00);
     if (!b && hdmi_out0_debug_enabled)
-        printf("hdmi_out0: NACK while writing eeprom address!\n");
+        printf("hdmi_out0: NACK while writing eeprom address!\r\n");
     i2c_start_cond(&hdmi_out0_i2c);
     b = i2c_write(&hdmi_out0_i2c, 0xa1);
     if (!b && hdmi_out0_debug_enabled)
-        printf("hdmi_out0: NACK while writing slave address (2)!\n");
+        printf("hdmi_out0: NACK while writing slave address (2)!\r\n");
     for (eeprom_addr = 0 ; eeprom_addr < 128 ; eeprom_addr++) {
         b = i2c_read(&hdmi_out0_i2c, eeprom_addr == 127 && extension_number == 0 ? 0 : 1);
         sum +=b;
         printf("%02X ", b);
         if(!((eeprom_addr+1) % 16))
-            printf("\n");
+            printf("\r\n");
         if(eeprom_addr == 126)
             extension_number = b;
         if(eeprom_addr == 127 && sum != 0)
         {
-            printf("Checksum ERROR in EDID block 0\n");
+            printf("Checksum ERROR in EDID block 0\r\n");
             i2c_stop_cond(&hdmi_out0_i2c);
             return;
         }
     }
     for(e = 0; e < extension_number; e++)
     {
-        printf("\n");
+        printf("\r\n");
         sum = 0;
         for (eeprom_addr = 0 ; eeprom_addr < 128 ; eeprom_addr++) {
             b = i2c_read(&hdmi_out0_i2c, eeprom_addr == 127 && e == extension_number - 1 ? 0 : 1);
             sum += b;
             printf("%02X ", b);
             if(!((eeprom_addr+1) % 16))
-                printf("\n");
+                printf("\r\n");
             if(eeprom_addr == 127 && sum != 0)
             {
-                printf("Checksum ERROR in EDID extension block %d\n", e);
+                printf("Checksum ERROR in EDID extension block %d\r\n", e);
                 i2c_stop_cond(&hdmi_out0_i2c);
                 return;
             }

firmware/lm32/hdmi_out1.c
@@ -22,42 +22,42 @@ void hdmi_out1_print_edid(void) {
     i2c_start_cond(&hdmi_out1_i2c);
     b = i2c_write(&hdmi_out1_i2c, 0xa0);
     if (!b && hdmi_out1_debug_enabled)
-        printf("hdmi_out1: NACK while writing slave address!\n");
+        printf("hdmi_out1: NACK while writing slave address!\r\n");
     b = i2c_write(&hdmi_out1_i2c, 0x00);
     if (!b && hdmi_out1_debug_enabled)
-        printf("hdmi_out1: NACK while writing eeprom address!\n");
+        printf("hdmi_out1: NACK while writing eeprom address!\r\n");
     i2c_start_cond(&hdmi_out1_i2c);
     b = i2c_write(&hdmi_out1_i2c, 0xa1);
     if (!b && hdmi_out1_debug_enabled)
-        printf("hdmi_out1: NACK while writing slave address (2)!\n");
+        printf("hdmi_out1: NACK while writing slave address (2)!\r\n");
     for (eeprom_addr = 0 ; eeprom_addr < 128 ; eeprom_addr++) {
         b = i2c_read(&hdmi_out1_i2c, eeprom_addr == 127 && extension_number == 0 ? 0 : 1);
         sum +=b;
         printf("%02X ", b);
         if(!((eeprom_addr+1) % 16))
-            printf("\n");
+            printf("\r\n");
         if(eeprom_addr == 126)
             extension_number = b;
         if(eeprom_addr == 127 && sum != 0)
         {
-            printf("Checksum ERROR in EDID block 0\n");
+            printf("Checksum ERROR in EDID block 0\r\n");
             i2c_stop_cond(&hdmi_out1_i2c);
             return;
         }
     }
     for(e = 0; e < extension_number; e++)
     {
-        printf("\n");
+        printf("\r\n");
         sum = 0;
         for (eeprom_addr = 0 ; eeprom_addr < 128 ; eeprom_addr++) {
             b = i2c_read(&hdmi_out1_i2c, eeprom_addr == 127 && e == extension_number - 1 ? 0 : 1);
             sum += b;
             printf("%02X ", b);
             if(!((eeprom_addr+1) % 16))
-                printf("\n");
+                printf("\r\n");
             if(eeprom_addr == 127 && sum != 0)
             {
-                printf("Checksum ERROR in EDID extension block %d\n", e);
+                printf("Checksum ERROR in EDID extension block %d\r\n", e);
                 i2c_stop_cond(&hdmi_out1_i2c);
                 return;
             }

commit 22275a2f7d726e0dfd36184598cebc2c96049dc9

firmware/lm32: Fix bad search/replace.
```
 CC       edid.o
edid.c: In function ‘get_monitor_name’:
edid.c:131:19: warning: multi-character character constant [-Wmultichar]
  c = strchr(name, '\r\n');
                   ^
```

Was broken in 102c2269db0eda1b60f26f05069e7c3a5f93bead

firmware/lm32/edid.c
@@ -128,7 +128,7 @@ void get_monitor_name(const void *buf, char *name)
 
 	name[MAX_MONITOR_NAME_LEN] = 0;
 	memcpy(name, &data_block[5], MAX_MONITOR_NAME_LEN);
-	c = strchr(name, '\r\n');
+	c = strchr(name, '\n');
 	if(c)
 		*c = 0;
 }