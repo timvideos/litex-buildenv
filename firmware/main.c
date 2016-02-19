3139b5479b37351c775067b81236de77a1764556
Florent Kermarrec <florent@enjoy-digital.fr>
2016-02-19 12:55:12 +0100

    firmware: add console service

--- firmware/main.c
+++ firmware/main.c
@@ -4,6 +4,93 @@
 
 #include <irq.h>
 #include <uart.h>
+#include <console.h>
+
+static char *readstr(void)
+{
+	char c[2];
+	static char s[64];
+	static int ptr = 0;
+
+	if(readchar_nonblock()) {
+		c[0] = readchar();
+		c[1] = 0;
+		switch(c[0]) {
+			case 0x7f:
+			case 0x08:
+				if(ptr > 0) {
+					ptr--;
+					putsnonl("\x08 \x08");
+				}
+				break;
+			case 0x07:
+				break;
+			case '\r':
+			case '\n':
+				s[ptr] = 0x00;
+				putsnonl("\n");
+				ptr = 0;
+				return s;
+			default:
+				if(ptr >= (sizeof(s) - 1))
+					break;
+				putsnonl(c);
+				s[ptr] = c[0];
+				ptr++;
+				break;
+		}
+	}
+
+	return NULL;
+}
+
+static char *get_token(char **str)
+{
+	char *c, *d;
+
+	c = (char *)strchr(*str, ' ');
+	if(c == NULL) {
+		d = *str;
+		*str = *str+strlen(*str);
+		return d;
+	}
+	*c = 0;
+	d = *str;
+	*str = c+1;
+	return d;
+}
+
+static void prompt(void)
+{
+	printf("RUNTIME>");
+}
+
+static void help(void)
+{
+	puts("Available commands:");
+	puts("help        - this command");
+	puts("reboot      - reboot CPU");
+}
+
+static void reboot(void)
+{
+	asm("call r0");
+}
+
+static void console_service(void)
+{
+	char *str;
+	char *token;
+
+	str = readstr();
+	if(str == NULL) return;
+	token = get_token(&str);
+	if(strcmp(token, "help") == 0)
+		help();
+	else if(strcmp(token, "reboot") == 0)
+		reboot();
+	prompt();
+}
 
 int main(void)
 {
@@ -12,8 +99,13 @@ int main(void)
 	uart_init();
 
 	puts("\nArty CPU testing software built "__DATE__" "__TIME__);
+	prompt();
 
-	while(1);
+	while(1) {
+		console_service();
+	}
 
 	return 0;
 }
+
+
