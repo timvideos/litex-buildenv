94bce988d9e0978c0b0e70b2e576290950936934
Florent Kermarrec <florent@enjoy-digital.fr>
2016-04-07 10:47:24 +0200

    opsis_video: remove hdmi_out for now

--- opsis_video.py
+++ opsis_video.py
@@ -3,15 +3,12 @@
 from opsis_base import *
 
 from gateware.hdmi_in import HDMIIn
-from gateware.hdmi_out import HDMIOut
 
 
 base_cls = MiniSoC
 
 class VideoMixerSoC(base_cls):
         csr_peripherals = (
-            "hdmi_out0",
-            "hdmi_out1",
             "hdmi_in0",
             "hdmi_in0_edid_mem",
             "hdmi_in1",
@@ -33,32 +30,7 @@ class VideoMixerSoC(base_cls):
             self.submodules.hdmi_in1 = HDMIIn(platform.request("hdmi_in", 1),
                                               self.sdram.crossbar.get_master(),
                                               fifo_depth=512)
-            self.submodules.hdmi_out0 = HDMIOut(platform.request("hdmi_out", 0),
-                                                self.sdram.crossbar.get_master())
-            # Share clocking with hdmi_out0 since no PLL_ADV left.
-            self.submodules.hdmi_out1 = HDMIOut(platform.request("hdmi_out", 1),
-                                                self.sdram.crossbar.get_master(),
-                                                self.hdmi_out0.driver.clocking)
 
-            # all PLL_ADV are used: router needs help...
-            platform.add_platform_command("""INST PLL_ADV LOC=PLL_ADV_X0Y0;""")
-            # FIXME: Fix the HDMI out so this can be removed.
-            platform.add_platform_command(
-                """PIN "hdmi_out_pix_bufg.O" CLOCK_DEDICATED_ROUTE = FALSE;""")
-            platform.add_platform_command(
-                """PIN "hdmi_out_pix_bufg_1.O" CLOCK_DEDICATED_ROUTE = FALSE;""")
-            platform.add_platform_command(
-                """
-NET "{pix0_clk}" TNM_NET = "GRPpix0_clk";
-NET "{pix1_clk}" TNM_NET = "GRPpix1_clk";
-""",
-                pix0_clk=self.hdmi_out0.driver.clocking.cd_pix.clk,
-                pix1_clk=self.hdmi_out1.driver.clocking.cd_pix.clk,
-            )
-            self.platform.add_false_path_constraints(
-                self.crg.cd_sys.clk,
-                self.hdmi_out0.driver.clocking.cd_pix.clk,
-                self.hdmi_out1.driver.clocking.cd_pix.clk)
 
 def main():
     parser = argparse.ArgumentParser(description="Opsis LiteX SoC")
