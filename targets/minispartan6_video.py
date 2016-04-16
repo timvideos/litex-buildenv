e7fb3d1c9977d1e432a0e19fdbaf73752bdf1a46
Tim 'mithro' Ansell <mithro@mithis.com>
2016-04-16 16:49:58 +1000

    targets/minispartan6_video: Adding TIG constraints.

--- targets/minispartan6_video.py
+++ targets/minispartan6_video.py
@@ -31,14 +31,14 @@ class VideoMixerSoC(BaseSoC):
         # FIXME: Fix the HDMI out so this can be removed.
         platform.add_platform_command(
             """PIN "hdmi_out_pix_bufg.O" CLOCK_DEDICATED_ROUTE = FALSE;""")
-#        platform.add_platform_command(
-#            """
-#NET "{pix0_clk}" TNM_NET = "GRPpix0_clk";
-#TIMESPEC "TSise_sucks7" = FROM "GRPpix0_clk" TO "GRPsys_clk" TIG;
-#TIMESPEC "TSise_sucks8" = FROM "GRPsys_clk" TO "GRPpix0_clk" TIG;
-#""", 
-#            pix0_clk=self.hdmi_out0.driver.clocking.cd_pix.clk,
-#        )
+        platform.add_platform_command(
+            """
+NET "{pix0_clk}" TNM_NET = "GRPpix0_clk";
+TIMESPEC "TSise_sucks7" = FROM "GRPpix0_clk" TO "GRPsys_clk" TIG;
+TIMESPEC "TSise_sucks8" = FROM "GRPsys_clk" TO "GRPpix0_clk" TIG;
+""",
+            pix0_clk=self.hdmi_out0.driver.clocking.cd_pix.clk,
+        )
 
         for k, v in sorted(platform.hdmi_infos.items()):
             self.add_constant(k, v)
