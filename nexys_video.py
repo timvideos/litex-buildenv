50fb7a422fd7f89f9cfbae2fd79b1c71b3399277
Florent Kermarrec <florent@enjoy-digital.fr>
2016-04-20 10:33:29 +0200

    nexys_video: use our video timing generator, no remaining vhdl files :)

--- nexys_video.py
+++ nexys_video.py
@@ -6,6 +6,8 @@ from litevideo.output.hdmi.s7 import S7HDMIOutClocking
 from litevideo.output.hdmi.s7 import S7HDMIOutEncoderSerializer
 from litevideo.output.hdmi.s7 import S7HDMIOutPHY
 
+from litevideo.output.core import TimingGenerator
+
 
 class VideoOutSoC(BaseSoC):
     def __init__(self, platform, *args, **kwargs):
@@ -14,43 +16,38 @@ class VideoOutSoC(BaseSoC):
         pads = platform.request("hdmi_out")
         self.comb += pads.scl.eq(1)
 
-        vga_hsync = Signal()
-        vga_vsync = Signal()
-        vga_red = Signal(8)
-        vga_green = Signal(8)
-        vga_blue = Signal(8)
-        vga_blank = Signal()
-
         # # #
 
+        self.submodules.vtg = ClockDomainsRenamer("pix")(TimingGenerator(1))
+        self.comb += [
+            self.vtg.timing.valid.eq(1),
 
-        self.specials += Instance("vga_gen",
-                i_pixel_clk=ClockSignal("pix"),
+            self.vtg.timing.hres.eq(1920),
+            self.vtg.timing.hsync_start.eq(1920+88),
+            self.vtg.timing.hsync_end.eq(1920+88+44),
+            self.vtg.timing.hscan.eq(2200),
 
-                o_vga_hsync=vga_hsync,
-                o_vga_vsync=vga_vsync,
-                o_vga_red=vga_red,
-                o_vga_green=vga_green,
-                o_vga_blue=vga_blue,
-                o_vga_blank=vga_blank,
-        )
+            self.vtg.timing.vres.eq(1080),
+            self.vtg.timing.vsync_start.eq(1080+4),
+            self.vtg.timing.vsync_end.eq(1080+4+5),
+            self.vtg.timing.vscan.eq(1125),
+
+            self.vtg.pixels.valid.eq(1)
+        ]
 
         self.submodules.hdmi_clocking = S7HDMIOutClocking(self.crg.clk100)
         self.submodules.hdmi_phy = S7HDMIOutPHY(pads)
         self.comb += [
-            self.hdmi_phy.hsync.eq(vga_hsync),
-            self.hdmi_phy.vsync.eq(vga_vsync),
-            self.hdmi_phy.de.eq(~vga_blank),
-            self.hdmi_phy.r.eq(vga_red),
-            self.hdmi_phy.g.eq(vga_green),
-            self.hdmi_phy.b.eq(vga_blue)
+            self.hdmi_phy.hsync.eq(self.vtg.phy.hsync),
+            self.hdmi_phy.vsync.eq(self.vtg.phy.vsync),
+            self.hdmi_phy.de.eq(self.vtg.phy.de),
+            self.hdmi_phy.r.eq(0x00),
+            self.hdmi_phy.g.eq(0x00),
+            self.hdmi_phy.b.eq(0xff),
+            self.vtg.phy.ready.eq(1)
         ]
         self.submodules.clk_es = S7HDMIOutEncoderSerializer(pads.clk_p, pads.clk_n, bypass_encoder=True)
         self.comb += self.clk_es.data.eq(Signal(10, reset=0b0000011111))
-
-        # # #
-
-        platform.add_source_dir(os.path.join("gateware", "hdmi_out"))
 
 
 def main():
