73f335371db10008a2d221b777350f0b584abde6
Florent Kermarrec <florent@enjoy-digital.fr>
2016-04-20 15:27:58 +0200

    nexys_video: use new TimingGenerator

--- nexys_video.py
+++ nexys_video.py
@@ -3,9 +3,7 @@
 from nexys_base import *
 
 from litevideo.output.hdmi.s7 import S7HDMIOutClocking
-
 from litevideo.output.hdmi.s7 import S7HDMIOutPHY
-
 from litevideo.output.core import TimingGenerator
 
 
@@ -18,33 +16,31 @@ class VideoOutSoC(BaseSoC):
 
         # # #
 
-        self.submodules.vtg = ClockDomainsRenamer("pix")(TimingGenerator(1))
+        self.submodules.vtg = ClockDomainsRenamer("pix")(TimingGenerator())
         self.comb += [
-            self.vtg.timing.valid.eq(1),
+            self.vtg.sink.valid.eq(1),
 
-            self.vtg.timing.hres.eq(1280),
-            self.vtg.timing.hsync_start.eq(1390),
-            self.vtg.timing.hsync_end.eq(1430),
-            self.vtg.timing.hscan.eq(1650),
+            self.vtg.sink.hres.eq(1280),
+            self.vtg.sink.hsync_start.eq(1390),
+            self.vtg.sink.hsync_end.eq(1430),
+            self.vtg.sink.hscan.eq(1650),
 
-            self.vtg.timing.vres.eq(720),
-            self.vtg.timing.vsync_start.eq(725),
-            self.vtg.timing.vsync_end.eq(730),
-            self.vtg.timing.vscan.eq(750),
-
-            self.vtg.pixels.valid.eq(1)
+            self.vtg.sink.vres.eq(720),
+            self.vtg.sink.vsync_start.eq(725),
+            self.vtg.sink.vsync_end.eq(730),
+            self.vtg.sink.vscan.eq(750)
         ]
 
         self.submodules.hdmi_clocking = S7HDMIOutClocking(self.crg.clk100, pads)
         self.submodules.hdmi_phy = S7HDMIOutPHY(pads)
         self.comb += [
-            self.hdmi_phy.hsync.eq(self.vtg.phy.hsync),
-            self.hdmi_phy.vsync.eq(self.vtg.phy.vsync),
-            self.hdmi_phy.de.eq(self.vtg.phy.de),
+            self.hdmi_phy.hsync.eq(self.vtg.source.hsync),
+            self.hdmi_phy.vsync.eq(self.vtg.source.vsync),
+            self.hdmi_phy.de.eq(self.vtg.source.de),
             self.hdmi_phy.r.eq(0x00),
             self.hdmi_phy.g.eq(0x00),
             self.hdmi_phy.b.eq(0xff),
-            self.vtg.phy.ready.eq(1)
+            self.vtg.source.ready.eq(1)
         ]
 
 def main():
