451f99a5f108e0a834dbb18393208a908806037a
Florent Kermarrec <florent@enjoy-digital.fr>
2016-04-21 13:22:53 +0200

    nexys_video: integrate HW ColorBarsPattern

--- nexys_video.py
+++ nexys_video.py
@@ -5,6 +5,7 @@ from nexys_base import *
 from litevideo.output.hdmi.s7 import S7HDMIOutClocking
 from litevideo.output.hdmi.s7 import S7HDMIOutPHY
 from litevideo.output.core import TimingGenerator
+from litevideo.output.pattern import ColorBarsPattern
 
 
 class VideoOutSoC(BaseSoC):
@@ -16,27 +17,31 @@ class VideoOutSoC(BaseSoC):
         # # #
 
         self.submodules.timing = ClockDomainsRenamer("pix")(TimingGenerator())
+        self.submodules.pattern = ClockDomainsRenamer("pix")(ColorBarsPattern())
         self.comb += [
             self.timing.sink.valid.eq(1),
-
             self.timing.sink.hres.eq(1280),
             self.timing.sink.hsync_start.eq(1390),
             self.timing.sink.hsync_end.eq(1430),
             self.timing.sink.hscan.eq(1650),
-
             self.timing.sink.vres.eq(720),
             self.timing.sink.vsync_start.eq(725),
             self.timing.sink.vsync_end.eq(730),
-            self.timing.sink.vscan.eq(750)
+            self.timing.sink.vscan.eq(750),
+
+            self.pattern.sink.valid.eq(1),
+            self.pattern.sink.hres.eq(1280),
         ]
 
         self.submodules.clocking = S7HDMIOutClocking(self.crg.clk100, pads)
         self.submodules.phy = S7HDMIOutPHY(pads)
+
         self.comb += [
             self.timing.source.connect(self.phy.sink),
-            self.phy.sink.r.eq(0x00),
-            self.phy.sink.g.eq(0x00),
-            self.phy.sink.b.eq(0xff)
+            self.phy.sink.r.eq(self.pattern.source.r),
+            self.phy.sink.g.eq(self.pattern.source.g),
+            self.phy.sink.b.eq(self.pattern.source.b),
+            self.pattern.source.ready.eq(self.timing.source.de)
         ]
 
 
