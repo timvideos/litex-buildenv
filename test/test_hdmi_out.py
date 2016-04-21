de621ed0c3f8305f7cb57351604c1f7cd8d52239
Florent Kermarrec <florent@enjoy-digital.fr>
2016-04-21 21:38:19 +0200

    test: test_hdmi_out: 1080p60 and 720p60 working

--- test/test_hdmi_out.py
+++ test/test_hdmi_out.py
@@ -4,20 +4,42 @@ wb = RemoteClient("192.168.1.50", 1234, csr_data_width=8)
 wb.open()
 regs = wb.regs
 
+
+def config_1080p60():
+    write_mmcm_reg(0x8, 0x1000 + (2 << 6) + 3)
+    write_mmcm_reg(0xa, 0x1000 + (1 << 6) + 1)
+
+    regs.initiator_hres.write(1920)
+    regs.initiator_hsync_start.write(1920+88)
+    regs.initiator_hsync_end.write(1920+88+44)
+    regs.initiator_hscan.write(2200)
+
+    regs.initiator_vres.write(1080)
+    regs.initiator_vsync_start.write(1080+4)
+    regs.initiator_vsync_end.write(1080+4+5)
+    regs.initiator_vscan.write(1125)
+
+    regs.initiator_enable.write(1)
+
+
+def config_720p60():
+    write_mmcm_reg(0x8, 0x1000 + (4 << 6)  + 6)
+    write_mmcm_reg(0xa, 0x1000 + (2  << 6) + 2)
+
+    regs.initiator_hres.write(1280)
+    regs.initiator_hsync_start.write(1390)
+    regs.initiator_hsync_end.write(1430)
+    regs.initiator_hscan.write(1650)
+
+    regs.initiator_vres.write(720)
+    regs.initiator_vsync_start.write(725)
+    regs.initiator_vsync_end.write(730)
+    regs.initiator_vscan.write(750)
+
+    regs.initiator_enable.write(1)
+
+
 # # #
-
-regs.initiator_hres.write(1280)
-regs.initiator_hsync_start.write(1390)
-regs.initiator_hsync_end.write(1430)
-regs.initiator_hscan.write(1650)
-
-regs.initiator_vres.write(720)
-regs.initiator_vsync_start.write(725)
-regs.initiator_vsync_end.write(730)
-regs.initiator_vscan.write(750)
-
-regs.initiator_enable.write(1)
-
 
 def read_mmcm_reg(address):
     regs.clocking_drp_addr.write(address)
@@ -36,15 +58,10 @@ def read_mmcm_config():
         print("%d : %04x" %(i, read_mmcm_reg(i)))
 
 
-read_mmcm_config()
-clkreg1 = read_mmcm_reg(0x8)
-print("0x%04x, high: %d, low: %d" %(clkreg1, (clkreg1 >> 6) & (2**6-1), clkreg1 & (2**6-1)))
-clkreg1 = read_mmcm_reg(0xa)
-print("0x%04x, high: %d, low: %d" %(clkreg1, (clkreg1 >> 6) & (2**6-1), clkreg1 & (2**6-1)))
+# # #
 
-write_mmcm_reg(0x8, 0x1000 + (10 << 6) + 10)
-write_mmcm_reg(0xa, 0x1000 +  (2 << 6) +  2)
-
+config_1080p60()
+config_720p60()
 
 # # #
 
