814e4d90821a0a06f4b0c82c96f9cf31b188ce13
Florent Kermarrec <florent@enjoy-digital.fr>
2016-04-21 12:19:52 +0200

    nexyx_video: use stream on phy

--- nexys_video.py
+++ nexys_video.py
@@ -12,36 +12,33 @@ class VideoOutSoC(BaseSoC):
         BaseSoC.__init__(self, platform, *args, **kwargs)
 
         pads = platform.request("hdmi_out")
-        self.comb += pads.scl.eq(1)
 
         # # #
 
-        self.submodules.vtg = ClockDomainsRenamer("pix")(TimingGenerator())
+        self.submodules.timing = ClockDomainsRenamer("pix")(TimingGenerator())
         self.comb += [
-            self.vtg.sink.valid.eq(1),
+            self.timing.sink.valid.eq(1),
 
-            self.vtg.sink.hres.eq(1280),
-            self.vtg.sink.hsync_start.eq(1390),
-            self.vtg.sink.hsync_end.eq(1430),
-            self.vtg.sink.hscan.eq(1650),
+            self.timing.sink.hres.eq(1280),
+            self.timing.sink.hsync_start.eq(1390),
+            self.timing.sink.hsync_end.eq(1430),
+            self.timing.sink.hscan.eq(1650),
 
-            self.vtg.sink.vres.eq(720),
-            self.vtg.sink.vsync_start.eq(725),
-            self.vtg.sink.vsync_end.eq(730),
-            self.vtg.sink.vscan.eq(750)
+            self.timing.sink.vres.eq(720),
+            self.timing.sink.vsync_start.eq(725),
+            self.timing.sink.vsync_end.eq(730),
+            self.timing.sink.vscan.eq(750)
         ]
 
-        self.submodules.hdmi_clocking = S7HDMIOutClocking(self.crg.clk100, pads)
-        self.submodules.hdmi_phy = S7HDMIOutPHY(pads)
+        self.submodules.clocking = S7HDMIOutClocking(self.crg.clk100, pads)
+        self.submodules.phy = S7HDMIOutPHY(pads)
         self.comb += [
-            self.hdmi_phy.hsync.eq(self.vtg.source.hsync),
-            self.hdmi_phy.vsync.eq(self.vtg.source.vsync),
-            self.hdmi_phy.de.eq(self.vtg.source.de),
-            self.hdmi_phy.r.eq(0x00),
-            self.hdmi_phy.g.eq(0x00),
-            self.hdmi_phy.b.eq(0xff),
-            self.vtg.source.ready.eq(1)
+            self.timing.source.connect(self.phy.sink),
+            self.phy.sink.r.eq(0x00),
+            self.phy.sink.g.eq(0x00),
+            self.phy.sink.b.eq(0xff)
         ]
+
 
 def main():
     parser = argparse.ArgumentParser(description="Nexys LiteX SoC")
