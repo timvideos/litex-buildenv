05a9b0226784810dd729c7186b66ae6751975e74
ssk1328 <shashankgangrade@gmail.com>
2016-06-24 00:03:54 +0530

    Incorporating changes

--- firmware/lm32/heartbeat.c
+++ firmware/lm32/heartbeat.c
@@ -26,33 +26,28 @@ void hb_status(int val)
 void hb_service(int source)
 {
 	static int last_event;
-	static int counter;		
+	static bool counter;		
 		if (heartbeat_status==1) {	
 		if(elapsed(&last_event, identifier_frequency_read()/5)) {
 			hb_fill(counter, source);
-			if(counter==5) 
-				counter = 6;	//BLUE in color_bar[6]
-			else 
-				counter = 5;	//RED in color_bar[5]
+			counter = !counter;	
 		}
 	}
 }
 
-void hb_fill(int color_v, int source)
+void hb_fill(bool color_v, int source)
 {
 	int addr, i, j;
+	unsigned int color;
+
 	volatile unsigned int *framebuffer = (unsigned int *)(MAIN_RAM_BASE + pattern_framebuffer_base());
 
-#ifdef CSR_HDMI_IN0_BASE
 	if (source == VIDEO_IN_HDMI_IN0) {
 		framebuffer = (unsigned int *)(MAIN_RAM_BASE + hdmi_in0_framebuffer_base(hdmi_in0_fb_index));
 	}
-#endif
-#ifdef CSR_HDMI_IN1_BASE
 	if (source == VIDEO_IN_HDMI_IN1) {
 		framebuffer = (unsigned int *)(MAIN_RAM_BASE + hdmi_in1_framebuffer_base(hdmi_in1_fb_index));
 	}
-#endif
 	if (source == VIDEO_IN_PATTERN) {
 		framebuffer = (unsigned int *)(MAIN_RAM_BASE + pattern_framebuffer_base());
 	}
@@ -62,10 +57,15 @@ void hb_fill(int color_v, int source)
 	8 pixel = 8 memory locations in vertical
 	Toggles between the colors defined in color_bar array from pattern.c
 	*/
+	if (color_v == 0)
+    	color = YCBCR422_BLUE;
+	else
+    	color = YCBCR422_RED;
+
 	addr = 0 + (processor_h_active/2)*(processor_v_active-8) + (processor_h_active/2) - 4;	
 	for (i=0; i<4; i++){
 		for (j=0; j<8; j++){
-			framebuffer[addr+i+(processor_h_active/2)*j] = color_bar[color_v];
+			framebuffer[addr+i+(processor_h_active/2)*j] = color;
 		}
 	}	
 }
--- firmware/lm32/heartbeat.h
+++ firmware/lm32/heartbeat.h
@@ -1,8 +1,10 @@
 #ifndef __HEARTBEAT_H
 #define __HEARTBEAT_H
 
+typedef enum { false, true } bool;
+
 void hb_status(int val);
 void hb_service(int source) ;
-void hb_fill(int color_v, int source);
+void hb_fill(bool color_v, int source);
 
 #endif /* __HEARTBEAT_H */
