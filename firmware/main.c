ee16cb2142a4dcedf021549ec7bae8e47ba07bea
Florent Kermarrec <florent@enjoy-digital.fr>
2016-07-01 12:35:11 +0200

    firmware: activate hdmi_out and draw video pattern

--- firmware/main.c
+++ firmware/main.c
@@ -17,10 +17,42 @@
 #include "telnet.h"
 #include "mdio.h"
 #include "oled.h"
-
+#include "pattern.h"
 
 static const unsigned char mac_addr[6] = {0x10, 0xe2, 0xd5, 0x00, 0x00, 0x00};
 static const unsigned char ip_addr[4] = {192, 168, 1, 50};
+
+/* hdmi_out functions */
+
+static void hdmi_out_write_mmcm_reg(uint32_t address, uint32_t data) {
+	hdmi_out0_driver_clocking_drp_addr_write(address);
+    hdmi_out0_driver_clocking_drp_di_write(data);
+    hdmi_out0_driver_clocking_drp_dwe_write(1);
+    hdmi_out0_driver_clocking_drp_den_write(1);
+}
+
+static void hdmi_out_config_720p60(void) {
+    hdmi_out_write_mmcm_reg(0x8, 0x1000 + (4 << 6)  + 6);
+    hdmi_out_write_mmcm_reg(0xa, 0x1000 + (2  << 6) + 2);
+
+    hdmi_out0_core_initiator_hres_write(1280);
+    hdmi_out0_core_initiator_hsync_start_write(1390);
+    hdmi_out0_core_initiator_hsync_end_write(1430);
+    hdmi_out0_core_initiator_hscan_write(1650);
+
+	hdmi_out0_core_initiator_vres_write(720);
+    hdmi_out0_core_initiator_vsync_start_write(725);
+    hdmi_out0_core_initiator_vsync_end_write(730);
+    hdmi_out0_core_initiator_vscan_write(750);
+
+    hdmi_out0_core_initiator_enable_write(0);
+    hdmi_out0_core_initiator_base_write(0x00200000);
+    hdmi_out0_core_initiator_length_write(1280*720*2);
+
+    hdmi_out0_core_initiator_enable_write(1);
+}
+
+/* hdmi_out functions */
 
 int main(void)
 {
@@ -29,8 +61,6 @@ int main(void)
 	uart_init();
 
 	puts("\nNexys CPU testing software built "__DATE__" "__TIME__);
-
-	time_init();
 
 #ifdef CSR_ETHPHY_MDIO_W_ADDR
 	mdio_status();
@@ -45,12 +75,16 @@ int main(void)
 	oled_init();
 	oled_refresh();
 #endif
+	pattern_fill_framebuffer(1280, 720);
+	hdmi_out_config_720p60();
 	ci_prompt();
+	time_init();
 	while(1) {
 		ci_service();
 #ifdef ETHMAC_BASE
 		ethernet_service();
 #endif
+		pattern_service();
 	}
 
 	return 0;
