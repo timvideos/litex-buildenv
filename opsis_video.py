f90d6f2d85baaf4df63c1899db2098396dff1b91
Florent Kermarrec <florent@enjoy-digital.fr>
2016-10-28 10:30:18 +0200

    opsis_video: increase fifo_depth on VideoOut

--- opsis_video.py
+++ opsis_video.py
@@ -39,13 +39,13 @@ class VideoMixerSoC(base_cls):
                                             platform.request("hdmi_out", 0),
                                             self.sdram.crossbar.get_port(mode="read", dw=16, cd="hdmi_out0_pix", reverse=True),
                                             mode="ycbcr422",
-                                            fifo_depth=512)
+                                            fifo_depth=4096)
         # hdmi out 1 : Share clocking with hdmi_out0 since no PLL_ADV left.
         self.submodules.hdmi_out1 = VideoOut(platform.device,
                                             platform.request("hdmi_out", 1),
                                             self.sdram.crossbar.get_port(mode="read", dw=16, cd="hdmi_out1_pix", reverse=True),
                                             mode="ycbcr422",
-                                            fifo_depth=512,
+                                            fifo_depth=4096,
                                             external_clocking=self.hdmi_out0.driver.clocking)
 
         # all PLL_ADV are used: router needs help...
