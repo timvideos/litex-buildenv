5b06a14efb7caff89f5ea73184cc180631b174af
Florent Kermarrec <florent@enjoy-digital.fr>
2016-11-30 12:07:03 +0100

    opsis_hdmi2usb: add analyzer on encoder

--- opsis_hdmi2usb.py
+++ opsis_hdmi2usb.py
@@ -4,13 +4,15 @@ from opsis_video import *
 from gateware.encoder import EncoderDMAReader, Encoder
 from gateware.streamer import USBStreamer
 
-base_cls = VideoMixerSoC
+from litescope import LiteScopeAnalyzer
 
+base_cls = VideoMixerSoC
 
 class HDMI2USBSoC(base_cls):
     csr_peripherals = (
         "encoder_reader",
         "encoder",
+        "analyzer",
     )
     csr_map_update(base_cls.csr_map, csr_peripherals)
     mem_map = {
@@ -30,7 +32,7 @@ class HDMI2USBSoC(base_cls):
             self.encoder.source.connect(self.encoder_streamer.sink)
         ]
         self.add_wb_slave(mem_decoder(self.mem_map["encoder"]), self.encoder.bus)
-        self.add_memory_region("encoder", self.mem_map["encoder"]+self.shadow_base, 0x2000)
+        self.add_memory_region("encoder", self.mem_map["encoder"] + self.shadow_base, 0x2000)
 
         self.platform.add_period_constraint(self.encoder_streamer.cd_usb.clk, 10.0) # XXX
 
@@ -38,6 +40,19 @@ class HDMI2USBSoC(base_cls):
         self.platform.add_false_path_constraints(
             self.crg.cd_sys.clk,
             self.encoder_streamer.cd_usb.clk)
+
+        analyzer_signals = [
+            self.encoder.sink.data,
+            self.encoder.sink.valid,
+            self.encoder.sink.ready,
+            self.encoder.source.data,
+            self.encoder.source.valid,
+            self.encoder.source.ready,
+        ]
+        self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 1024)
+
+    def do_exit(self, vns):
+        self.analyzer.export_csv(vns, "test/analyzer.csv")
 
 
 def main():
@@ -53,6 +68,7 @@ def main():
                       compile_gateware=not args.nocompile_gateware,
                       csr_csv="test/csr.csv")
     vns = builder.build()
+    soc.do_exit(vns)
 
 if __name__ == "__main__":
     main()
