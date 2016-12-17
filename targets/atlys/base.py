ca4c82baff580702b6304def0cdf649056c608f1
Tim 'mithro' Ansell <mithro@mithis.com>
2016-12-17 20:35:11 +0100

    Slowly importing the Atlys module.

--- targets/atlys/base.py
+++ targets/atlys/base.py
@@ -1,3 +1,4 @@
+# Support for the Digilent Atlys board - digilentinc.com/atlys/
 from fractions import Fraction
 
 from litex.gen import *
@@ -7,20 +8,16 @@ from litex.gen.genlib.misc import WaitTimer
 from litex.soc.cores.flash import spi_flash
 from litex.soc.integration.soc_sdram import *
 from litex.soc.integration.builder import *
-from litex.soc.cores.gpio import GPIOIn, GPIOOut
-from litex.soc.interconnect.csr import AutoCSR
-from litex.soc.cores.uart.bridge import UARTWishboneBridge
 
-from litedram.modules import MT41J128M16
+from litedram.modules import P3R1GE4JGF
 from litedram.phy import s6ddrphy
 from litedram.core import ControllerSettings
 
+
+from gateware import cas
 from gateware import dna
 #from gateware import git_info
-#from gateware import i2c
-#from gateware import i2c_hack
 #from gateware import platform_info
-from gateware import shared_uart
 
 
 def csr_map_update(csr_map, csr_peripherals):
@@ -28,36 +25,12 @@ def csr_map_update(csr_map, csr_peripherals):
         for v, n in enumerate(csr_peripherals, start=max(csr_map.values()) + 1)))
 
 
-class FrontPanelGPIO(Module, AutoCSR):
-    def __init__(self, platform, clk_freq):
-        switches = Signal(1)
-        leds = Signal(2)
-
-        self.reset = Signal()
-
-        # # #
-
-        self.submodules.switches = GPIOIn(switches)
-        self.submodules.leds = GPIOOut(leds)
-        self.comb += [
-           switches[0].eq(~platform.request("pwrsw")),
-           platform.request("hdled").eq(~leds[0]),
-           platform.request("pwled").eq(~leds[1]),
-        ]
-
-        # generate a reset when power switch is pressed for 1 second
-        self.submodules.reset_timer = WaitTimer(clk_freq)
-        self.comb += [
-            self.reset_timer.wait.eq(switches[0]),
-            self.reset.eq(self.reset_timer.done)
-        ]
 
 
 class _CRG(Module):
     def __init__(self, platform, clk_freq):
         # Clock domains for the system (soft CPU and related components run at).
         self.clock_domains.cd_sys = ClockDomain()
-        self.clock_domains.cd_sys2x = ClockDomain()
         # Clock domains for the DDR interface.
         self.clock_domains.cd_sdram_half = ClockDomain()
         self.clock_domains.cd_sdram_full_wr = ClockDomain()
@@ -112,22 +85,22 @@ class _CRG(Module):
             i_CLKFBIN=pll_fb, o_CLKFBOUT=pll_fb, o_LOCKED=pll_lckd,
             p_CLK_FEEDBACK="CLKFBOUT",
             p_CLKFBOUT_MULT=m*p//n, p_CLKFBOUT_PHASE=0.,
-            # (400MHz) ddr3 wr/rd full clock
+            # (300MHz) sdram wr rd
             o_CLKOUT0=unbuf_sdram_full, p_CLKOUT0_DUTY_CYCLE=.5,
-            p_CLKOUT0_PHASE=0., p_CLKOUT0_DIVIDE=p//8,
+            p_CLKOUT0_PHASE=0., p_CLKOUT0_DIVIDE=p//4,
             # unused?
             o_CLKOUT1=unbuf_unused, p_CLKOUT1_DUTY_CYCLE=.5,
             p_CLKOUT1_PHASE=0., p_CLKOUT1_DIVIDE=p//8,
-            # (200MHz) sdram_half - ddr3 dqs adr ctrl off-chip
+            # (150MHz) sdram_half - sdram dqs adr ctrl
             o_CLKOUT2=unbuf_sdram_half_a, p_CLKOUT2_DUTY_CYCLE=.5,
-            p_CLKOUT2_PHASE=230., p_CLKOUT2_DIVIDE=p//4,
-            # (200MHz) off-chip ddr - ddr3 half clock
+            p_CLKOUT2_PHASE=270., p_CLKOUT2_DIVIDE=p//2,
+            # (150MHz) off-chip ddr
             o_CLKOUT3=unbuf_sdram_half_b, p_CLKOUT3_DUTY_CYCLE=.5,
-            p_CLKOUT3_PHASE=210., p_CLKOUT3_DIVIDE=p//4,
-            # (100MHz) sys2x - 2x system clock
-            o_CLKOUT4=unbuf_sys2x, p_CLKOUT4_DUTY_CYCLE=.5,
-            p_CLKOUT4_PHASE=0., p_CLKOUT4_DIVIDE=p//2,
-            # ( 50MHz) periph / sys - system clock
+            p_CLKOUT3_PHASE=250., p_CLKOUT3_DIVIDE=p//2,
+            # ( 50MHz) periph
+            o_CLKOUT4=unbuf_periph, p_CLKOUT4_DUTY_CYCLE=.5,
+            p_CLKOUT4_PHASE=0., p_CLKOUT4_DIVIDE=12,
+            # ( 75MHz) sysclk
             o_CLKOUT5=unbuf_sys, p_CLKOUT5_DUTY_CYCLE=.5,
             p_CLKOUT5_PHASE=0., p_CLKOUT5_DIVIDE=p//1,
         )
@@ -140,29 +113,26 @@ class _CRG(Module):
         self.sync.por += If(por != 0, por.eq(por - 1))
         self.specials += AsyncResetSynchronizer(self.cd_por, reset)
 
-        # System clock - 50MHz
+        # System clock - 75MHz
         self.specials += Instance("BUFG", i_I=unbuf_sys, o_O=self.cd_sys.clk)
         self.comb += self.cd_por.clk.eq(self.cd_sys.clk)
         self.specials += AsyncResetSynchronizer(self.cd_sys, ~pll_lckd | (por > 0))
 
-        # sys2x
-        self.specials += Instance("BUFG", i_I=unbuf_sys2x, o_O=self.cd_sys2x.clk)
-        self.specials += AsyncResetSynchronizer(self.cd_sys2x, ~pll_lckd | (por > 0))
 
         # SDRAM clocks
         # ------------------------------------------------------------------------------
-        self.clk8x_wr_strb = Signal()
-        self.clk8x_rd_strb = Signal()
+        self.clk4x_wr_strb = Signal()
+        self.clk4x_rd_strb = Signal()
 
         # sdram_full
         self.specials += Instance("BUFPLL", p_DIVIDE=4,
-                                  i_PLLIN=unbuf_sdram_full, i_GCLK=self.cd_sys2x.clk,
+                                  i_PLLIN=unbuf_sdram_full, i_GCLK=self.cd_sys.clk,
                                   i_LOCKED=pll_lckd,
                                   o_IOCLK=self.cd_sdram_full_wr.clk,
-                                  o_SERDESSTROBE=self.clk8x_wr_strb)
+                                  o_SERDESSTROBE=self.clk4x_wr_strb)
         self.comb += [
             self.cd_sdram_full_rd.clk.eq(self.cd_sdram_full_wr.clk),
-            self.clk8x_rd_strb.eq(self.clk8x_wr_strb),
+            self.clk4x_rd_strb.eq(self.clk4x_wr_strb),
         ]
         # sdram_half
         self.specials += Instance("BUFG", i_I=unbuf_sdram_half_a, o_O=self.cd_sdram_half.clk)
@@ -203,17 +173,11 @@ class _CRG(Module):
 class BaseSoC(SoCSDRAM):
     csr_peripherals = (
         "spiflash",
-        "front_panel",
+#        "cas",
         "ddrphy",
         "dna",
 #        "git_info",
 #        "platform_info",
-#        "fx2_reset",
-#        "fx2_hack",
-#        "opsis_eeprom_i2c",
-        "tofe_ctrl",
-        "tofe_lsio_leds",
-        "tofe_lsio_sws",
     )
     csr_map_update(SoCSDRAM.csr_map, csr_peripherals)
 
@@ -222,9 +186,8 @@ class BaseSoC(SoCSDRAM):
     }
     mem_map.update(SoCSDRAM.mem_map)
 
-
     def __init__(self, platform, **kwargs):
-        clk_freq = 50*1000000
+        clk_freq = 75*1000000
         SoCSDRAM.__init__(self, platform, clk_freq,
             integrated_rom_size=0x8000,
             integrated_sram_size=0x4000,
@@ -235,17 +198,7 @@ class BaseSoC(SoCSDRAM):
 
         self.submodules.dna = dna.DNA()
         #self.submodules.git_info = git_info.GitInfo()
-        #self.submodules.platform_info = platform_info.PlatformInfo("opsis", self.__class__.__name__[:8])
-
-        #self.submodules.opsis_eeprom_i2c = i2c.I2C(platform.request("opsis_eeprom"))
-        #self.submodules.fx2_reset = gpio.GPIOOut(platform.request("fx2_reset"))
-        #self.submodules.fx2_hack = i2c_hack.I2CShiftReg(platform.request("opsis_eeprom"))
-
-        self.submodules.tofe_eeprom_i2c = i2c.I2C(platform.request("tofe_eeprom"))
-
-        self.submodules.suart = shared_uart.SharedUART(self.clk_freq, 115200)
-        self.suart.add_uart_pads(platform.request('fx2_serial'))
-        self.submodules.uart = self.suart.uart
+        #self.submodules.platform_info = platform_info.PlatformInfo("atlys", self.__class__.__name__[:8])
 
         self.submodules.spiflash = spi_flash.SpiFlash(
             platform.request("spiflash4x"),
@@ -257,14 +210,12 @@ class BaseSoC(SoCSDRAM):
         self.register_mem("spiflash", self.mem_map["spiflash"],
             self.spiflash.bus, size=platform.gateware_size)
 
-        # front panel (ATX)
-        self.submodules.front_panel = FrontPanelGPIO(platform, clk_freq)
-        self.comb += self.crg.reset.eq(self.front_panel.reset)
-
+        self.submodules.cas = cas.ControlAndStatus(platform, clk_freq)
         # sdram
-        sdram_module = MT41J128M16(self.clk_freq, "1:4")
-        self.submodules.ddrphy = s6ddrphy.S6QuarterRateDDRPHY(
+        sdram_module = P3R1GE4JGF(self.clk_freq, "1:2")
+        self.submodules.ddrphy = s6ddrphy.S6HalfRateDDRPHY(
             platform.request("ddram"),
+            sdram_module.memtype,
             rd_bitslip=0,
             wr_bitslip=4,
             dqs_ddr_alignment="C0")
@@ -274,42 +225,8 @@ class BaseSoC(SoCSDRAM):
                             sdram_module.timing_settings,
                             controller_settings=controller_settings)
         self.comb += [
-            self.ddrphy.clk8x_wr_strb.eq(self.crg.clk8x_wr_strb),
-            self.ddrphy.clk8x_rd_strb.eq(self.crg.clk8x_rd_strb),
-        ]
-
-        # TOFE board
-        tofe_ctrl = Signal(3) # rst, sda, scl
-        ptofe_ctrl = platform.request('tofe')
-        self.submodules.tofe_ctrl = GPIOOut(tofe_ctrl)
-        self.comb += [
-            ptofe_ctrl.rst.eq(~tofe_ctrl[0]),
-            ptofe_ctrl.sda.eq(~tofe_ctrl[1]),
-            ptofe_ctrl.scl.eq(~tofe_ctrl[2]),
-        ]
-
-        # TOFE LowSpeedIO board
-        # ---------------------------------
-        # UARTs
-        self.suart.add_uart_pads(platform.request('tofe_lsio_serial'))
-        self.suart.add_uart_pads(platform.request('tofe_lsio_pmod_serial'))
-        # LEDs
-        tofe_lsio_leds = Signal(4)
-        self.submodules.tofe_lsio_leds = GPIOOut(tofe_lsio_leds)
-        self.comb += [
-            platform.request('tofe_lsio_user_led', 0).eq(tofe_lsio_leds[0]),
-            platform.request('tofe_lsio_user_led', 1).eq(tofe_lsio_leds[1]),
-            platform.request('tofe_lsio_user_led', 2).eq(tofe_lsio_leds[2]),
-            platform.request('tofe_lsio_user_led', 3).eq(tofe_lsio_leds[3]),
-        ]
-        # Switches
-        tofe_lsio_sws = Signal(4)
-        self.submodules.tofe_lsio_sws = GPIOIn(tofe_lsio_sws)
-        self.comb += [
-            tofe_lsio_sws[0].eq(~platform.request('tofe_lsio_user_sw', 0)),
-            tofe_lsio_sws[1].eq(~platform.request('tofe_lsio_user_sw', 1)),
-            tofe_lsio_sws[2].eq(~platform.request('tofe_lsio_user_sw', 2)),
-            tofe_lsio_sws[3].eq(~platform.request('tofe_lsio_user_sw', 3)),
+            self.ddrphy.clk4x_wr_strb.eq(self.crg.clk4x_wr_strb),
+            self.ddrphy.clk4x_rd_strb.eq(self.crg.clk4x_rd_strb),
         ]
 
 
