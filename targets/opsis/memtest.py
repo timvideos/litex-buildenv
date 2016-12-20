2e7b5fc9ea556ff63590d5f56a3b82c67514fa07
Tim 'mithro' Ansell <mithro@mithis.com>
2016-12-20 14:14:29 +0100

    Temp commit before working on merging Florent's changes.

--- targets/opsis/memtest.py
+++ targets/opsis/memtest.py
@@ -1,4 +1,6 @@
-from litedram.frontend.bist import LiteDRAMBISTGenerator, LiteDRAMBISTChecker
+from litedram.frontend.bist import LiteDRAMBISTGenerator
+from litedram.frontend.bist import LiteDRAMBISTChecker
+from litedram.frontend.bist import LiteDRAMBISTCheckerScope
 
 from litescope import LiteScopeAnalyzer
 
@@ -10,6 +12,7 @@ class MemTestSoC(BaseSoC):
     csr_peripherals = (
         "generator",
         "checker",
+        "checker_scope",
         "analyzer",
     )
     csr_map_update(BaseSoC.csr_map, csr_peripherals)
@@ -17,18 +20,13 @@ class MemTestSoC(BaseSoC):
     def __init__(self, platform, *args, **kwargs):
         BaseSoC.__init__(self, platform, *args, **kwargs)
 
-        self.submodules.generator = LiteDRAMBISTGenerator(self.sdram.crossbar.get_port(mode="write"))
-        #self.submodules.checker = LiteDRAMBISTChecker(self.sdram.crossbar.get_port(mode="read", dw=16)) #, cd="hdmi_out1_pix"))
-        self.submodules.checker = LiteDRAMBISTChecker(self.sdram.crossbar.get_port(mode="read")) #, cd="hdmi_out1_pix"))
+        self.submodules.generator = LiteDRAMBISTGenerator(self.sdram.crossbar.get_port(mode="write", dw=32), random=False)
+        self.submodules.checker = LiteDRAMBISTChecker(self.sdram.crossbar.get_port(mode="read", dw=32, reverse=True), random=False)
+        self.submodules.checker_scope = LiteDRAMBISTCheckerScope(self.checker)
 
-        analyzer_signals = [
-            self.checker.core.cmd_counter,
-            self.checker.core.data_counter,
-            self.checker.core.data_error,
-            self.checker.core.dma.source.data,  # expected
-            self.checker.core.gen.o,            # actual
-        ]
-        self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 2048)
+        analyzer_signals = self.checker_scope.signals()
+
+        self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 1024)
 
     def do_exit(self, vns, filename="test/analyzer.csv"):
         self.analyzer.export_csv(vns, filename)
--- test/test_memtest.py
+++ test/test_memtest.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 import time
+from collections import namedtuple
 
 from litescope.software.driver.analyzer import LiteScopeAnalyzerDriver
 
 from common import *
+
+MemError = namedtuple("MemError", ("address", "expected", "actual"))
 
 
 def main():
@@ -14,23 +17,32 @@ def main():
 
     main_ram = wb.mems.main_ram
     print("DDR at 0x{:x} -- {} Megabytes".format(main_ram.base, int(main_ram.size/(1024*1024))))
-
+    print()
     # init
-    print("Init")
+    print("Reset")
     wb.regs.generator_reset.write(1)
     wb.regs.checker_reset.write(1)
 
     wb.regs.generator_reset.write(0)
     wb.regs.checker_reset.write(0)
 
-    analyzer = LiteScopeAnalyzerDriver(wb.regs, "analyzer", config_csv='{}/analyzer.csv'.format(make_testdir(args)), debug=True)
-    analyzer.configure_trigger(cond={'checker_litedrambistchecker_data_error': 1})
-    analyzer.configure_subsampler(1)
-#    analyzer.run(offset=16, length=1024)
+    assert not wb.regs.checker_scope_error.read()
+    print()
 
-    base = int(256e3)
+    analyzer = LiteScopeAnalyzerDriver(wb.regs, "analyzer", config_csv='{}/analyzer.csv'.format(make_testdir(args)), debug=True)
+    print("Init analyzer")
+    #analyzer.configure_trigger(cond={'checker_core_data_error': 1})
+    analyzer.configure_trigger(cond={'data_error': 1})
+    analyzer.configure_trigger(cond={})
+    analyzer.configure_subsampler(1)
+    analyzer.run(offset=16, length=1024)
+    assert not analyzer.done()
+    print()
+
+    base = int(512e3)
     #length = int((main_ram.size-base)*8/16) # 64e6 == 64 megabytes
-    length = int(2e6)
+    #length = int(2e6)
+    length = int(1)
 
     # write
     print("Write")
@@ -39,35 +51,65 @@ def main():
 
     assert not wb.regs.generator_done.read()
     wb.regs.generator_start.write(1)
-    print("Waiting", end='')
+    print("Waiting", end=' ')
     while wb.regs.generator_done.read() == 0:
         print(".", end='', flush=True)
         time.sleep(0.1)
+    assert wb.regs.generator_done.read() == 1
     print()
 
     # read
-    print("Read")
     assert wb.regs.checker_err_count.read() == 0
 
+    print("Read")
     write_and_check(wb.regs.checker_base, base)
     write_and_check(wb.regs.checker_length, length)
 
     assert not wb.regs.checker_done.read()
+    assert not analyzer.done()
     wb.regs.checker_start.write(1)
-    print("Waiting", end='')
+    print("Waiting")
     while wb.regs.checker_done.read() == 0:
-        print("Errors:", wb.regs.checker_err_count.read())
+        print("  Errors:", wb.regs.checker_err_count.read())
         time.sleep(0.1)
-    print()
+    assert wb.regs.checker_done.read() == 1
+    print("Final errors:", wb.regs.checker_err_count.read())
 
     errors = wb.regs.checker_err_count.read()
-    if errors != 0:
-        addr = wb.regs.checker_err_addr.read()
-        expect = wb.regs.checker_err_expect.read()
-        actual = wb.regs.checker_err_actual.read()
-        print("Error @ {} - {:x} != {:x}".format(addr, expect, actual))
 
-    assert errors == 0, errors
+    if errors != 0:
+        # Check litescope
+        assert analyzer.done()
+        analyzer.upload()
+        analyzer.save('sim.vcd')
+
+        data = {v.name: v for v in analyzer.dump().variables}
+
+        okay = set()
+        errors = {}
+        for i in range(0, len(data['clk'].values)):
+            address = hex(data['data_address'].values[i])
+
+            if data['data_error'].values[i] != 1:
+                okay.add(address)
+                continue
+
+            expected = data['data_expected'].values[i]
+            actual = data['data_actual'].values[i]
+
+            r = MemError(address, hex(expected), hex(actual))
+            if address in errors:
+                assert errors[address] == r
+            else:
+                errors[address] = r
+
+        import pprint
+        print(len(errors))
+        pprint.pprint(errors)
+
+        from IPython import embed
+        embed()
+        #print("Error @ {} - {:x} != {:x}".format(addr, expect, actual))
 
     print("Done!")
 
