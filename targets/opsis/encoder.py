232c914d776dd191669e57b6ea5c84425995efe0
Tim 'mithro' Ansell <mithro@mithis.com>
2017-01-10 19:36:39 +1100

    Adding an encoder only target.

---
+++ targets/opsis/encoder.py
@@ -0,0 +1,57 @@
+from litex.gen.fhdl.decorators import ClockDomainsRenamer
+from litex.gen.fhdl.specials import Keep
+from litex.soc.integration.soc_core import mem_decoder
+from litex.soc.interconnect import stream
+
+from gateware.encoder import EncoderDMAReader, EncoderBuffer, Encoder
+from gateware.streamer import USBStreamer
+
+from targets.utils import csr_map_update
+from targets.opsis.net import SoC as BaseSoC
+
+
+class EncoderSoC(BaseSoC):
+    csr_peripherals = (
+        "encoder_reader",
+        "encoder",
+    )
+    csr_map_update(BaseSoC.csr_map, csr_peripherals)
+    mem_map = {
+        "encoder": 0x50000000,  # (shadow @0xd0000000)
+    }
+    mem_map.update(BaseSoC.mem_map)
+
+    def __init__(self, platform, *args, **kwargs):
+        BaseSoC.__init__(self, platform, *args, **kwargs)
+
+        encoder_port = self.sdram.crossbar.get_port()
+        self.submodules.encoder_reader = EncoderDMAReader(encoder_port)
+        encoder_cdc = stream.AsyncFIFO([("data", 128)], 4)
+        encoder_cdc = ClockDomainsRenamer({"write": "sys",
+                                           "read": "encoder"})(encoder_cdc)
+        encoder_buffer = ClockDomainsRenamer("encoder")(EncoderBuffer())
+        encoder = Encoder(platform)
+        encoder_streamer = USBStreamer(platform, platform.request("fx2"))
+        self.submodules += encoder_cdc, encoder_buffer, encoder, encoder_streamer
+
+        self.comb += [
+            self.encoder_reader.source.connect(encoder_cdc.sink),
+            encoder_cdc.source.connect(encoder_buffer.sink),
+            encoder_buffer.source.connect(encoder.sink),
+            encoder.source.connect(encoder_streamer.sink)
+        ]
+        self.add_wb_slave(mem_decoder(self.mem_map["encoder"]), encoder.bus)
+        self.add_memory_region("encoder",
+            self.mem_map["encoder"] + self.shadow_base, 0x2000)
+
+        self.platform.add_period_constraint(encoder_streamer.cd_usb.clk, 10.0)
+
+        self.specials += Keep(encoder_streamer.cd_usb.clk)
+        self.specials += Keep(self.crg.cd_encoder.clk)
+        self.platform.add_false_path_constraints(
+            self.crg.cd_sys.clk,
+            self.crg.cd_encoder.clk,
+            encoder_streamer.cd_usb.clk)
+
+
+SoC = EncoderSoC
