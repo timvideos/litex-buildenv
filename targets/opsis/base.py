b4f7a751f654707f3caf24beab6ebb4581695f02
Tim 'mithro' Ansell <mithro@mithis.com>
2017-01-10 12:12:23 +1100

    Fixing the DCM a little.

--- targets/opsis/base.py
+++ targets/opsis/base.py
@@ -186,14 +186,20 @@ class _CRG(Module):
         dcm_base50_locked = Signal()
         self.specials += [
             Instance("DCM_CLKGEN", name="DCM_CRG_PERIPH",
-                     p_CLKFXDV_DIVIDE=2, p_CLKFX_DIVIDE=4,
-                     p_CLKFX_MD_MAX=1.0, p_CLKFX_MULTIPLY=2,
-                     p_CLKIN_PERIOD=10.0, p_SPREAD_SPECTRUM="NONE",
+                     p_CLKIN_PERIOD=10.0,
+                     p_CLKFX_MULTIPLY=2,
+                     p_CLKFX_DIVIDE=4,
+                     p_CLKFX_MD_MAX=2.0,
+                     p_CLKFXDV_DIVIDE=2,
+                     p_SPREAD_SPECTRUM="NONE",
                      p_STARTUP_WAIT="FALSE",
 
-                     i_CLKIN=clk100a, o_CLKFX=self.cd_base50.clk,
+                     i_CLKIN=clk100a,
+                     o_CLKFX=self.cd_base50.clk,
                      o_LOCKED=dcm_base50_locked,
-                     i_FREEZEDCM=0, i_RST=ResetSignal()),
+                     i_FREEZEDCM=0,
+                     i_RST=ResetSignal(),
+                     ),
             AsyncResetSynchronizer(self.cd_base50,
                 self.cd_sys.rst | ~dcm_base50_locked)
         ]
