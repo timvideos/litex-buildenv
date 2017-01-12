f9e2b9879879a1ffa20e3014fe27e1b7d5ae05ff
Florent Kermarrec <florent@enjoy-digital.fr>
2017-01-12 05:14:33 +0100

    gateware/freq_count: refactoring wip4, should be fine now :)

--- gateware/freq_measurement.py
+++ gateware/freq_measurement.py
@@ -6,61 +6,29 @@ from litex.soc.interconnect.csr import *
 
 class Sampler(Module):
     def __init__(self, measure_width, counter_width):
-        self.sample_in = Signal(measure_width)
-        self.inc = Signal(measure_width)
-        self.last_total = Signal(counter_width)
-        self.end_period = Signal()
-
-        last_sample = Signal(measure_width)
-        curr_total = Signal(counter_width)
-
-        # # #
-
-        self.sync += last_sample.eq(self.sample_in)
-        # Resetting souce clock domain is unreliable, so just use wrapping
-        # property of unsigned arithmetic to reset the count each sample.
-        self.comb += self.inc.eq(self.sample_in - last_sample)
-        self.sync += \
-            If(self.end_period,
-                curr_total.eq(0)
-            ).Else(
-                curr_total.eq(curr_total + self.inc)
-            )
-
-        # During period reset, curr_total won't latch the final value, so
-        # store it in separate location.
-        self.sync += \
-            If(self.end_period,
-                self.last_total.eq(curr_total)
-            )
-
-
-class Measurement(Module):
-    def __init__(self, measure_clock, measure_width, counter_width):
-        self.value = Signal(counter_width)
+        self.i = Signal(measure_width)
         self.latch = Signal()
+        self.value = Signal(counter_width)
 
-        ev_count = ClockDomainsRenamer("measure")(GrayCounter(measure_width))
-        gray_decoder = GrayDecoder(measure_width)
-        sampler = Sampler(measure_width, counter_width)
-        self.submodules += ev_count, gray_decoder, sampler
-
-        self.clock_domains.cd_measure = ClockDomain(reset_less=True)
-        if isinstance(measure_clock, str):
-            self.comb += self.cd_measure.clk.eq(ClockSignal(measure_clock))
-        else:
-            self.comb += self.cd_measure.clk.eq(measure_clock)
-
-        self.comb += ev_count.ce.eq(1)
-        self.specials += MultiReg(ev_count.q, gray_decoder.i)
 
         # # #
 
-        self.comb += [
-            sampler.end_period.eq(self.latch),
-            sampler.sample_in.eq(gray_decoder.o),
-            self.value.eq(sampler.last_total)
-        ]
+        counter = Signal(counter_width)
+        counter_inc = Signal(measure_width)
+        
+        # use wrapping property of unsigned arithmeric to reset the counter
+        # each cycle (reseting measure clock domain is unreliable)
+        i_d = Signal(measure_width)
+        self.sync += i_d.eq(self.i)
+        self.comb += counter_inc.eq(self.i - i_d)
+
+        self.sync += \
+            If(self.latch,
+                counter.eq(0),
+                self.value.eq(counter),
+            ).Else(
+                counter.eq(counter + counter_inc)
+            )
 
 
 class FrequencyMeasurement(Module, AutoCSR):
@@ -70,20 +38,31 @@ class FrequencyMeasurement(Module, AutoCSR):
 
         # # #
 
-        core = Measurement(measure_clock, measure_width, counter_width)
-        self.submodules += core
+        # create measure clock domain
+        self.clock_domains.cd_measure = ClockDomain(reset_less=True)
+        self.comb += self.cd_measure.clk.eq(measure_clock)
 
-        self.comb += self.value.status.eq(core.value)
-
+        # mesure period
         period_done = Signal()
         period_counter = Signal(counter_width)
-        self.comb += [
-            period_done.eq(period_counter == measure_period),
-            core.latch.eq(period_done)
-        ]
+        self.comb += period_done.eq(period_counter == measure_period)
         self.sync += \
             If(period_done,
                 period_counter.eq(0),
             ).Else(
                 period_counter.eq(period_counter + 1)
             )
+
+        # measurement
+        event_counter = ClockDomainsRenamer("measure")(GrayCounter(measure_width))
+        gray_decoder = GrayDecoder(measure_width)
+        sampler = Sampler(measure_width, counter_width)
+        self.submodules += event_counter, gray_decoder, sampler
+
+        self.specials += MultiReg(event_counter.q, gray_decoder.i)
+        self.comb += [
+            event_counter.ce.eq(1),
+            sampler.latch.eq(period_done),
+            sampler.i.eq(gray_decoder.o),
+            self.value.status.eq(sampler.value)
+        ]
