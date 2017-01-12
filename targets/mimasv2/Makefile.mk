6e6c119f378f729e76e27cfc3c964e3d24467be1
Tim 'mithro' Ansell <mithro@mithis.com>
2017-01-12 14:52:02 +1100

    More Makefile refactoring.

--- targets/mimasv2/Makefile.mk
+++ targets/mimasv2/Makefile.mk
@@ -1,8 +1,8 @@
 # mimasv2 loading
-load-gateware-mimasv2:
+gateware-load-mimasv2:
 	MimasV2Config.py /dev/ttyACM0 $(TARGET_BUILD_DIR)/gateware/top.bin
 
-load-firmware-mimasv2:
+firmware-load-mimasv2:
 	flterm --port=/dev/ttyACM0 --kernel=$(TARGET_BUILD_DIR)/software/firmware/firmware.bin
 
-.PHONY: load-gateware-mimasv2 load-firmware-mimasv2
+.PHONY: gateware-load-mimasv2 firmware-load-mimasv2
