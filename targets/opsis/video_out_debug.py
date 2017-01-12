09745efbbd52da9b14bba52432daa322b950186a
Tim 'mithro' Ansell <mithro@mithis.com>
2017-01-12 20:56:06 +1100

    Disable analyzer on video_out_debug for now.

--- targets/opsis/video_out_debug.py
+++ targets/opsis/video_out_debug.py
@@ -12,7 +12,7 @@ from targets.opsis.net import NetSoC as BaseSoC
 class VideoDebugSoC(BaseSoC):
     csr_peripherals = (
         "hdmi_out0",
-        "analyzer"
+#        "analyzer"
     )
     csr_map_update(BaseSoC.csr_map, csr_peripherals)
 
@@ -42,44 +42,44 @@ NET "{pix0_clk}" TNM_NET = "GRPpix0_clk";
             self.crg.cd_sys.clk,
             self.hdmi_out0.driver.clocking.cd_pix.clk)
 
-        analyzer_signals = [
-            #self.hdmi_out0.core.timing.hactive,
-            #self.hdmi_out0.core.timing.vactive,
-            #self.hdmi_out0.core.timing.active,
-            #self.hdmi_out0.core.timing.hcounter,
-            #self.hdmi_out0.core.timing.vcounter,
-            self.hdmi_out0.core.timing.sink.valid,
-            self.hdmi_out0.core.timing.sink.ready,
-            self.hdmi_out0.core.timing.source.valid,
-            self.hdmi_out0.core.timing.source.ready,
-
-            #self.hdmi_out0.core.dma.offset,
-            self.hdmi_out0.core.dma.sink.valid,
-            self.hdmi_out0.core.dma.sink.ready,
-            self.hdmi_out0.core.dma.source.valid,
-            self.hdmi_out0.core.dma.source.ready,
-
-            # FIXME: These don't seem to be valid anymore?
-            #dram_port.counter,
-            #dram_port.rdata_chunk,
-            #dram_port.cmd_buffer.sink.valid,
-            #dram_port.cmd_buffer.sink.ready,
-            #dram_port.cmd_buffer.source.valid,
-            #dram_port.cmd_buffer.source.ready,
-            #dram_port.rdata_buffer.sink.valid,
-            #dram_port.rdata_buffer.sink.ready,
-            #dram_port.rdata_buffer.source.valid,
-            #dram_port.rdata_buffer.source.ready,
-            #dram_port.rdata_converter.sink.valid,
-            #dram_port.rdata_converter.sink.ready,
-            #dram_port.rdata_converter.source.valid,
-            #dram_port.rdata_converter.source.ready,
-
-        ]
-        self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 1024)
-
-    def do_exit(self, vns, filename="test/analyzer.csv"):
-        self.analyzer.export_csv(vns, filename)
+##        analyzer_signals = [
+##            #self.hdmi_out0.core.timing.hactive,
+##            #self.hdmi_out0.core.timing.vactive,
+##            #self.hdmi_out0.core.timing.active,
+##            #self.hdmi_out0.core.timing.hcounter,
+##            #self.hdmi_out0.core.timing.vcounter,
+##            self.hdmi_out0.core.timing.sink.valid,
+##            self.hdmi_out0.core.timing.sink.ready,
+##            self.hdmi_out0.core.timing.source.valid,
+##            self.hdmi_out0.core.timing.source.ready,
+##
+##            #self.hdmi_out0.core.dma.offset,
+##            self.hdmi_out0.core.dma.sink.valid,
+##            self.hdmi_out0.core.dma.sink.ready,
+##            self.hdmi_out0.core.dma.source.valid,
+##            self.hdmi_out0.core.dma.source.ready,
+##
+##            # FIXME: These don't seem to be valid anymore?
+##            #dram_port.counter,
+##            #dram_port.rdata_chunk,
+##            #dram_port.cmd_buffer.sink.valid,
+##            #dram_port.cmd_buffer.sink.ready,
+##            #dram_port.cmd_buffer.source.valid,
+##            #dram_port.cmd_buffer.source.ready,
+##            #dram_port.rdata_buffer.sink.valid,
+##            #dram_port.rdata_buffer.sink.ready,
+##            #dram_port.rdata_buffer.source.valid,
+##            #dram_port.rdata_buffer.source.ready,
+##            #dram_port.rdata_converter.sink.valid,
+##            #dram_port.rdata_converter.sink.ready,
+##            #dram_port.rdata_converter.source.valid,
+##            #dram_port.rdata_converter.source.ready,
+##
+##        ]
+##        self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 1024)
+##
+##    def do_exit(self, vns, filename="test/analyzer.csv"):
+##        self.analyzer.export_csv(vns, filename)
 
 
 SoC = VideoDebugSoC
