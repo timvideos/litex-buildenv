f881bf5cb8aaceddb60b713bed9363f6e880e996
Michael Farrell <micolous+git@gmail.com>
2017-01-14 00:11:21 +1100

    pattern: Fix text spacing, add colour text support, add some more useful info to screen.

--- firmware/pattern.c
+++ firmware/pattern.c
@@ -10,6 +10,7 @@
 #include "pattern.h"
 #include "processor.h"
 #include "uptime.h"
+#include "version_data.h"
 
 #define PATTERN_FRAMEBUFFER_BASE 0x02000000 + 0x100000
 
@@ -132,28 +133,31 @@ static int inc_color(int color) {
 	return color%8;
 }
 
-static void pattern_draw_text(int x, int y, char *ptr) {
+static void pattern_draw_text_color(int x, int y, char *ptr, long background_color, long text_color) {
 	int i, j, k;
 	int adr;
 	volatile unsigned int *framebuffer = (unsigned int *)(MAIN_RAM_BASE + PATTERN_FRAMEBUFFER_BASE);
 	for(i=0; ptr[i] != '\0'; i++) {
-		for(j=0; j<7; j++) {
-			for(k=0; k<5; k++) {
-				adr = 5*(x + i) + k + (2*8*y + 2*j)*processor_h_active/2;
-				if((font5x7[5*(ptr[i] - ' ') + k] >> j) & 0x1) {
-					framebuffer[adr + 0*processor_h_active/2] = YCBCR422_BLACK;
-					framebuffer[adr + 1*processor_h_active/2] = YCBCR422_BLACK;
-				}
-				else {
-					framebuffer[adr + 0*processor_h_active/2] = YCBCR422_WHITE;
-					framebuffer[adr + 1*processor_h_active/2] = YCBCR422_WHITE;
+		for(j=-1; j<8; j++) {
+			for(k=-1; k<6; k++) {
+				adr = 6*(x + i) + k + (2*9*y + 2*j)*processor_h_active/2;
+				if (k >= 0 && k < 5 && j >= 0 && j < 7 && ((font5x7[5*(ptr[i] - ' ') + k] >> j) & 0x1)) {
+					framebuffer[adr + 0*processor_h_active/2] = text_color;
+					framebuffer[adr + 1*processor_h_active/2] = text_color;
+				} else {
+					framebuffer[adr + 0*processor_h_active/2] = background_color;
+					framebuffer[adr + 1*processor_h_active/2] = background_color;
 				}
 			}
 		}
 	}
 }
 
-void pattern_next() {
+static void pattern_draw_text(int x, int y, char *ptr) {
+	pattern_draw_text_color(x, y, ptr, YCBCR422_WHITE, YCBCR422_BLACK);
+}
+
+void pattern_next(void) {
 	pattern++;
 	pattern = pattern % MAX_PATTERN;
 	pattern_fill_framebuffer(processor_h_active, processor_v_active);
@@ -184,7 +188,7 @@ void pattern_fill_framebuffer(int h_active, int w_active)
 				framebuffer[i] = 0x801080ff;
 		}
 	}
-	
+
 	// draw a border around that.
 	for (i=0; i<h_active*2; i++) {
 		framebuffer[i] = YCBCR422_WHITE;
@@ -207,10 +211,22 @@ void pattern_fill_framebuffer(int h_active, int w_active)
 			framebuffer[(i*h_active)+j + (1*h_active/2)] = YCBCR422_WHITE;
 		}		
 	}
-	
-	pattern_draw_text(1, 1, "HDMI2USB");
-	pattern_draw_text(1, 3, "timvideos.us");
-	pattern_draw_text(1, 4, "enjoy-digital.fr");
+
+	// Show version information
+	pattern_draw_text(1, 1, "Hi! I am HDMI2USB ");
+	pattern_draw_text(19, 1, (char*)git_describe);
+	pattern_draw_text(1, 2, "code.timvideos.us / enjoy-digital.fr");
+	pattern_draw_text_color(6, 2, "tim", YCBCR422_WHITE, YCBCR422_RED);
+	pattern_draw_text_color(9, 2, "videos", YCBCR422_WHITE, YCBCR422_BLUE);
+	pattern_draw_text_color(27, 2, "digital", YCBCR422_WHITE, YCBCR422_CYAN);
+	pattern_draw_text(1, 3, "Running on ");
+	pattern_draw_text(12, 3, (char*)board);
+	pattern_draw_text(1, 4, "Built: "__DATE__" "__TIME__);
+
+#ifndef HIDE_ADVERT
+	pattern_draw_text_color(1, 7, "Want to hack on FOSS video capture systems?", YCBCR422_BLUE, YCBCR422_WHITE);
+	pattern_draw_text_color(1, 8, "Get in touch with us! #timvideos on Freenode IRC", YCBCR422_RED, YCBCR422_WHITE);
+#endif
 	flush_l2_cache();
 }
 
