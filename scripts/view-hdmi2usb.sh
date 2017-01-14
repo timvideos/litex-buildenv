85f29b91574860734c14211ed8c4dee48ed5111f
Tim 'mithro' Ansell <mithro@mithis.com>
2017-01-15 09:31:26 +1100

    List the video devices rather then silly count thing.

--- scripts/view-hdmi2usb.sh
+++ scripts/view-hdmi2usb.sh
@@ -2,24 +2,22 @@
 
 if [ ! -d /dev/hdmi2usb ]; then
 	I=0
-	while true; do
+	for DEV in $(ls /dev/video*); do
 		echo
-		if ! v4l-info /dev/video$I > /dev/null 2>&1; then
-			echo "No /dev/video$I"
+		if ! v4l-info $DEV > /dev/null 2>&1; then
+			echo "No $DEV"
 			break
 		else
-			echo "/dev/video$I - exists!"
+			echo "$DEV - exists!"
 		fi
 
-		if v4l-info /dev/video$I | grep 'HDMI2USB' > /dev/null 2>&1; then
-			echo "Device /dev/video$I *is* a HDMI2USB"
-			export HDMI2USB=/dev/video$I
+		if v4l-info $DEV | grep 'HDMI2USB' > /dev/null 2>&1; then
+			echo "Device $DEV *is* a HDMI2USB"
+			export HDMI2USB=$DEV
 			break
 		else
-			echo "Device /dev/video$I is *not* a HDMI2USB"
+			echo "Device $DEV is *not* a HDMI2USB"
 		fi
-
-		I=$((I+1))
 	done
 else
 	HDMI2USB=$(ls /dev/hdmi2usb/by-num/all*/video | head -n1)
