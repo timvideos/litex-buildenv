c7d42ad7d931dab44c8bf5f3c8e574e5a1ed4859
Tim 'mithro' Ansell <mithro@mithis.com>
2017-01-14 15:37:17 +1100

    Fix opsis-flash target in Makefile.

--- targets/opsis/Makefile.mk
+++ targets/opsis/Makefile.mk
@@ -12,7 +12,7 @@ reset-opsis:
 	opsis-mode-switch --verbose --mode=serial
 
 flash-opsis:
-	opsis-mode-switch --verbose --flash-gateware=$(TARGET_BUILD_DIR)/gateware/top.bit
+	opsis-mode-switch --verbose --flash-gateware=$(TARGET_BUILD_DIR)/gateware/top.bin
 	opsis-mode-switch --verbose --flash-lm32-firmware=$(TARGET_BUILD_DIR)/software/firmware/firmware.fbi
 
 .PHONY: gateware-load-opsis firmware-load-opsis reset-opsis flash-opsis
