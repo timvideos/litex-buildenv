f17550b49126f434448a45f60fbe4d1be4a7236b
Tim 'mithro' Ansell <mithro@mithis.com>
2017-01-17 15:29:53 +1100

    Getting back to a working romboot.

--- targets/mimasv2/Makefile.mk
+++ targets/mimasv2/Makefile.mk
@@ -1,8 +1,8 @@
 # mimasv2 loading
 gateware-load-mimasv2:
-	MimasV2Config.py /dev/ttyACM0 $(TARGET_BUILD_DIR)/gateware/top.bin
+	python3 $$(which MimasV2Config.py) /dev/ttyACM0 $(TARGET_BUILD_DIR)/gateware/top.bin
 
 firmware-load-mimasv2:
-	flterm --port=/dev/ttyACM0 --kernel=$(TARGET_BUILD_DIR)/software/firmware/firmware.bin
+	flterm --port=/dev/ttyACM0 --kernel=$(TARGET_BUILD_DIR)/software/firmware/firmware.bin --speed=19200
 
 .PHONY: gateware-load-mimasv2 firmware-load-mimasv2
