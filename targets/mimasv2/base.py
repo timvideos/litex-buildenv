a4b7a12ac3d3cac9e3861d76e77ce6304495fb16
Tim 'mithro' Ansell <mithro@mithis.com>
2017-01-18 13:14:58 +1100

    Revert "Drop mimasv2 frequency as it's -2 speed grade part."
    This reverts commit 743f7961a4c6a12c132da5e939e97ed6e4537283.

--- targets/mimasv2/base.py
+++ targets/mimasv2/base.py
@@ -185,7 +185,7 @@ class BaseSoC(SoCSDRAM):
     mem_map.update(SoCSDRAM.mem_map)
 
     def __init__(self, platform, **kwargs):
-        clk_freq = 50*1000*1000
+        clk_freq = (83 + Fraction(1, 3))*1000*1000
         SoCSDRAM.__init__(self, platform, clk_freq,
             #integrated_rom_size=0x8000,
             integrated_rom_size=None,
