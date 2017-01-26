fd662900b9f594c765eb4d1689f10116713f5d6a
Florent Kermarrec <florent@enjoy-digital.fr>
2017-01-26 18:49:05 +0100

    firmware: add video pattern in memory and test video out

--- firmware/main.c
+++ firmware/main.c
@@ -12,6 +12,39 @@
 #include <system.h>
 
 #include "ci.h"
+#include "pattern.h"
+
+/* hdmi_out functions */
+
+static void hdmi_out_write_mmcm_reg(unsigned int address, unsigned int data) {
+	hdmi_out_driver_clocking_drp_addr_write(address);
+    hdmi_out_driver_clocking_drp_di_write(data);
+    hdmi_out_driver_clocking_drp_dwe_write(1);
+    hdmi_out_driver_clocking_drp_den_write(1);
+}
+
+static void hdmi_out_config_720p60(void) {
+    hdmi_out_write_mmcm_reg(0x8, 0x1000 + (4 << 6)  + 6);
+    hdmi_out_write_mmcm_reg(0xa, 0x1000 + (2  << 6) + 2);
+
+    hdmi_out_core_initiator_hres_write(1280);
+    hdmi_out_core_initiator_hsync_start_write(1390);
+    hdmi_out_core_initiator_hsync_end_write(1430);
+    hdmi_out_core_initiator_hscan_write(1650);
+
+	hdmi_out_core_initiator_vres_write(720);
+    hdmi_out_core_initiator_vsync_start_write(725);
+    hdmi_out_core_initiator_vsync_end_write(730);
+    hdmi_out_core_initiator_vscan_write(750);
+
+    hdmi_out_core_initiator_enable_write(0);
+    hdmi_out_core_initiator_base_write(0x00200000);
+    hdmi_out_core_initiator_length_write(1280*720*2);
+
+    hdmi_out_core_initiator_enable_write(1);
+}
+
+/* hdmi_out functions */
 
 int main(void)
 {
@@ -21,10 +54,14 @@ int main(void)
 
 	puts("\nNeTV2 CPU testing software built "__DATE__" "__TIME__);
 
+	pattern_fill_framebuffer(1280, 720);
+	hdmi_out_config_720p60();
+
 	time_init();
 	ci_prompt();
 	while(1) {
 		ci_service();
+		pattern_service();
 	}
 
 	return 0;
--- netv2_video.py
+++ netv2_video.py
@@ -12,7 +12,7 @@ base_cls = BaseSoC
 
 class VideoOutSoC(base_cls):
     csr_map = {
-        "hdmi_out0": 26
+        "hdmi_out": 26
     }
     csr_map.update(base_cls.csr_map)
 
@@ -24,23 +24,23 @@ class VideoOutSoC(base_cls):
         mode = "ycbcr422"
 
         if mode == "ycbcr422":
-            hdmi_out0_dram_port = self.sdram.crossbar.get_port(mode="read", dw=16, cd="pix", reverse=True)
-            self.submodules.hdmi_out0 = VideoOut(platform.device,
+            hdmi_out_dram_port = self.sdram.crossbar.get_port(mode="read", dw=16, cd="pix", reverse=True)
+            self.submodules.hdmi_out = VideoOut(platform.device,
                                                  platform.request("hdmi_out"),
-                                                 hdmi_out0_dram_port,
+                                                 hdmi_out_dram_port,
                                                  "ycbcr422")
         elif mode == "rgb":
-            hdmi_out0_dram_port = self.sdram.crossbar.get_port(mode="read", dw=32, cd="pix", reverse=True)
-            self.submodules.hdmi_out0 = VideoOut(platform.device,
+            hdmi_out_dram_port = self.sdram.crossbar.get_port(mode="read", dw=32, cd="pix", reverse=True)
+            self.submodules.hdmi_out = VideoOut(platform.device,
                                                  platform.request("hdmi_out"),
-                                                 hdmi_out0_dram_port,
+                                                 hdmi_out_dram_port,
                                                  "rgb")
 
         self.platform.add_period_constraint(self.crg.cd_sys.clk, 10.0)
-        self.platform.add_period_constraint(self.hdmi_out0.driver.clocking.cd_pix.clk, 10.0)
+        self.platform.add_period_constraint(self.hdmi_out.driver.clocking.cd_pix.clk, 10.0)
         self.platform.add_false_path_constraints(
             self.crg.cd_sys.clk,
-            self.hdmi_out0.driver.clocking.cd_pix.clk)
+            self.hdmi_out.driver.clocking.cd_pix.clk)
 
 
 def main():
