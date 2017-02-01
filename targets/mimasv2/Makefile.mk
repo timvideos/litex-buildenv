4d46c22e265409c3d5f2041e59a4453d6ef9bbea
Joel Stanley <joel@jms.id.au>
2017-02-02 10:01:49 +1030

    mimasv2: Make serial port path configurable
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- targets/mimasv2/Makefile.mk
+++ targets/mimasv2/Makefile.mk
@@ -1,18 +1,19 @@
 # mimasv2 loading
 
 TARGET ?= base
+PORT ?= /dev/ttyACM0
 
 gateware-load-mimasv2:
-	python3 $$(which MimasV2Config.py) /dev/ttyACM0 $(TARGET_BUILD_DIR)/gateware/top.bin
+	python3 $$(which MimasV2Config.py) $(PORT) $(TARGET_BUILD_DIR)/gateware/top.bin
 
 image-load-mimasv2:
 	python mkimage.py
-	python3 $$(which MimasV2Config.py) /dev/ttyACM0 $(TARGET_BUILD_DIR)/flash.bin
+	python3 $$(which MimasV2Config.py) $(PORT) $(TARGET_BUILD_DIR)/flash.bin
 
 firmware-load-mimasv2:
-	flterm --port=/dev/ttyACM0 --kernel=$(TARGET_BUILD_DIR)/software/firmware/firmware.bin --speed=19200
+	flterm --port=$(PORT) --kernel=$(TARGET_BUILD_DIR)/software/firmware/firmware.bin --speed=19200
 
 firmware-connect-mimasv2:
-	flterm --port=/dev/ttyACM0 --speed=19200
+	flterm --port=$(PORT) --speed=19200
 
 .PHONY: gateware-load-mimasv2 firmware-load-mimasv2
