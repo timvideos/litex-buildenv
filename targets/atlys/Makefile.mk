1b6a778a456016a8f9a09df800fdc836db0048a1
Tim 'mithro' Ansell <mithro@mithis.com>
2017-02-19 18:53:03 -0800

    Adding flash-atlys command.

--- targets/atlys/Makefile.mk
+++ targets/atlys/Makefile.mk
@@ -11,4 +11,8 @@ firmware-load-atlys:
 reset-atlys:
 	atlys-mode-switch --verbose --mode=jtag
 
-.PHONY: gateware-load-atlys firmware-load-atlys reset-atlys
+flash-atlys:
+	atlys-mode-switch --verbose --flash-gateware=$(TARGET_BUILD_DIR)/gateware/top.bin
+	atlys-mode-switch --verbose --flash-lm32-firmware=$(TARGET_BUILD_DIR)/software/firmware/firmware.fbi
+
+.PHONY: gateware-load-atlys firmware-load-atlys reset-atlys flash-opsis
