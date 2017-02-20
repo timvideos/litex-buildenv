575a85b8960acabce6466b75bf860ac9eab46611
Tim 'mithro' Ansell <mithro@mithis.com>
2017-02-19 18:51:05 -0800

    Fixing the Atlys loading/flashing.

--- targets/atlys/Makefile.mk
+++ targets/atlys/Makefile.mk
@@ -6,12 +6,9 @@ gateware-load-atlys: tftp
 	atlys-mode-switch --verbose --load-gateware $(TARGET_BUILD_DIR)/gateware/top.bit
 
 firmware-load-atlys:
-	atlys-mode-switch --verbose --mode=serial
-	flterm --port=/dev/hdmi2usb/by-num/atlys0/tty --kernel=$(TARGET_BUILD_DIR)/software/firmware/firmware.bin
+	flterm --port=$$(atlys-mode-switch --get-serial-device) --kernel=$(TARGET_BUILD_DIR)/software/firmware/firmware.bin
 
 reset-atlys:
-	atlys-mode-switch --verbose --mode=serial
 	atlys-mode-switch --verbose --mode=jtag
-	atlys-mode-switch --verbose --mode=serial
 
 .PHONY: gateware-load-atlys firmware-load-atlys reset-atlys
