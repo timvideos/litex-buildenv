0b07d303bc3079707bf5ee798a5bcecace00071b
Tim 'mithro' Ansell <mithro@mithis.com>
2017-03-04 20:15:30 +1100

    firmware/heartbeat.c: Remove trailing white space.

--- firmware/heartbeat.c
+++ firmware/heartbeat.c
@@ -18,13 +18,13 @@ static bool heartbeat_status = false;
 
 void hb_status(bool val)
 {
-	heartbeat_status = val; 
+	heartbeat_status = val;
 }
 
 void hb_service(int sink)
 {
 	static int last_event;
-	static int counter;		
+	static int counter;
 	static bool color_v;
 
 	if (heartbeat_status==1) {
@@ -73,10 +73,10 @@ void hb_fill(bool color_v, int sink)
 	else
 		color = YCBCR422_RED;
 
-	addr = 0 + (processor_h_active/2)*(processor_v_active-8) + (processor_h_active/2) - 4;	
+	addr = 0 + (processor_h_active/2)*(processor_v_active-8) + (processor_h_active/2) - 4;
 	for (i=0; i<4; i++){
 		for (j=0; j<8; j++){
 			framebuffer[addr+i+(processor_h_active/2)*j] = color;
 		}
-	}	
+	}
 }
