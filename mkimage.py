501a54e0909d1e4ca4a5d2c405924bd5eeb60947
Tim 'mithro' Ansell <mithro@mithis.com>
2017-03-04 13:35:41 +1100

    mkimage: Allowing overriding gateware/bios/firmware with nothing.

--- mkimage.py
+++ mkimage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import argparse
 
 from make import make_args, make_builddir, make_platform
@@ -18,7 +19,7 @@ def main():
     args = parser.parse_args()
 
     builddir = make_builddir(args)
-    gateware = "{}/gateware/top.bin".format(builddir)
+    gateware = os.path.join(builddir, "gateware", "top.bin")
     if args.override_gateware:
         if args.override_gateware.lower() == "none":
             gateware = None
@@ -29,7 +30,7 @@ def main():
             "Gateware file not found! "
             "Use --override-gateware=none for no gateware.")
 
-    bios = "{}/software/bios/bios.bin".format(builddir)
+    bios = os.path.join(builddir, "software", "bios", "bios.bin")
     if args.override_bios:
         if args.override_bios.lower() == "none":
             bios = None
@@ -40,7 +41,7 @@ def main():
             "BIOS file not found! "
             "Use --override-bios=none for no BIOS.")
 
-    firmware = "{}/software/firmware/firmware.fbi".format(builddir)
+    firmware = os.path.join(builddir, "software", "firmware", "firmware.fbi")
     if args.override_firmware:
         if args.override_firmware.lower() == "none":
             firmware = None
@@ -57,45 +58,54 @@ def main():
     bios_pos = platform.gateware_size
     firmware_pos = platform.gateware_size + BIOS_SIZE
 
-    output = "{}/flash.bin".format(builddir)
+    output = os.path.join(builddir, "flash.bin")
+    print()
     with open(output, "wb") as f:
-        print()
+        # FPGA gateware
         if gateware:
             gateware_data = open(gateware, "rb").read()
-            print(("Gateware @ 0x{:08x} ({:10} bytes) {:60}"
-                   " - Xilinx FPGA Bitstream"
-                   ).format(gateware_pos, len(gateware_data), gateware))
-            print(" ".join("{:02x}".format(i) for i in gateware_data[:64]))
-            assert len(gateware_data) < platform.gateware_size
-            f.seek(0)
-            f.write(gateware_data)
         else:
-            print("Skipping gateware.")
+            gateware_data = b""
+            gateware = "Skipped"
+
+        print(("Gateware @ 0x{:08x} ({:10} bytes) {:60}"
+               " - Xilinx FPGA Bitstream"
+               ).format(gateware_pos, len(gateware_data), gateware))
+        print(" ".join("{:02x}".format(i) for i in gateware_data[:64]))
+        assert len(gateware_data) < platform.gateware_size
+        f.seek(0)
+        f.write(gateware_data)
 
         if bios:
             bios_data = open(bios, "rb").read()
-            assert len(bios_data) < BIOS_SIZE
-            print(("    BIOS @ 0x{:08x} ({:10} bytes) {:60}"
-                   " - LiteX BIOS with CRC"
-                   ).format(bios_pos, len(bios_data), bios))
-            print(" ".join("{:02x}".format(i) for i in bios_data[:64]))
-            f.seek(bios_pos)
-            f.write(bios_data)
         else:
-            print("Skipping BIOS.")
+            bios_data = b""
+            bios = "Skipped"
+
+        # LiteX BIOS
+        assert len(bios_data) < BIOS_SIZE
+        f.seek(bios_pos)
+        f.write(bios_data)
+        print(("    BIOS @ 0x{:08x} ({:10} bytes) {:60}"
+               " - LiteX BIOS with CRC"
+               ).format(bios_pos, len(bios_data), bios))
+        print(" ".join("{:02x}".format(i) for i in bios_data[:64]))
 
         if firmware:
             firmware_data = open(firmware, "rb").read()
-            print(("Firmware @ 0x{:08x} ({:10} bytes) {:60}"
-                   " - HDMI2USB Firmware in FBI format (loaded into DRAM)"
-                   ).format(firmware_pos, len(firmware_data), firmware))
-            print(" ".join("{:02x}".format(i) for i in firmware_data[:64]))
-            f.seek(firmware_pos)
-            f.write(firmware_data)
         else:
-            print("Skipping firmware.")
-            firmware_data = []
+            firmware_data = b""
+            firmware = "Skipped"
 
+        # SoftCPU firmware
+        print(("Firmware @ 0x{:08x} ({:10} bytes) {:60}"
+               " - HDMI2USB Firmware in FBI format (loaded into DRAM)"
+               ).format(firmware_pos, len(firmware_data), firmware))
+        print(" ".join("{:02x}".format(i) for i in firmware_data[:64]))
+        f.seek(firmware_pos)
+        f.write(firmware_data)
+
+        # Result
         remain = platform.spiflash_total_size - (
             firmware_pos+len(firmware_data))
         print("-"*40)
@@ -111,6 +121,7 @@ def main():
     print("Flash image: {}".format(output))
     flash_image_data = open(output, "rb").read()
     print(" ".join("{:02x}".format(i) for i in flash_image_data[:64]))
+
 
 if __name__ == "__main__":
     main()
