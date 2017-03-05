93473f328319500211456d5a56d2759354a9ea22
Tim 'mithro' Ansell <mithro@mithis.com>
2017-03-05 20:14:15 +1100

    Working on travis build.
     * Trying to get prebuilt stuff working again.

--- .travis/build.sh
+++ .travis/build.sh
@@ -46,13 +46,22 @@ function build() {
 	echo ""
 	echo ""
 	echo "============================================="
-	echo "- $PLATFORM $TARGET"
+	echo "- $PLATFORM $TARGET $CPU"
 	echo "============================================="
 	# Output the commands available to make it easier to debug.
 	echo ""
-	echo "- make help ($PLATFORM $TARGET)"
+	echo "- make help ($PLATFORM $TARGET $CPU)"
 	echo "---------------------------------------------"
 	make help
+	echo "============================================="
+
+	echo ""
+	echo ""
+	echo ""
+	echo "- make test ($PLATFORM $TARGET $CPU)"
+	echo "---------------------------------------------"
+	make test || return 1
+	echo "============================================="
 
 	# We build the firmware first as it is very quick to build and
 	# will let us find a whole classes of errors quickly.
@@ -60,7 +69,7 @@ function build() {
 	echo ""
 	echo ""
 	echo ""
-	echo "- make firmware ($PLATFORM $TARGET) (prerun)"
+	echo "- make firmware ($PLATFORM $TARGET $CPU) (prerun)"
 	echo "---------------------------------------------"
 	make firmware || return 1
 	echo "- Firmware version data"
@@ -81,7 +90,7 @@ function build() {
 #			return 1
 		fi
 	fi
-	echo "---------------------------------------------"
+	echo "============================================="
 
 	# https://github.com/timvideos/HDMI2USB-misoc-firmware/issues/83
 	# We have to clean after doing this otherwise if the gateware
@@ -90,14 +99,15 @@ function build() {
 	echo ""
 	echo ""
 	echo ""
-	echo "- make firmware-clean ($PLATFORM $TARGET) (prerun)"
+	echo "- make firmware-clean ($PLATFORM $TARGET $CPU) (prerun)"
 	echo "---------------------------------------------"
 	make firmware-clean
+	echo "============================================="
 
 	echo ""
 	echo ""
 	echo ""
-	echo "- make gateware ($PLATFORM $TARGET)"
+	echo "- make gateware ($PLATFORM $TARGET $CPU)"
 	echo "---------------------------------------------"
 	if [ $HAVE_XILINX_ISE -eq 0 ]; then
 		echo "Skipping gateware"
@@ -105,89 +115,135 @@ function build() {
 		FILTER=$PWD/.travis/run-make-gateware-filter.py \
 			make gateware || return 1
 	fi
+	echo "============================================="
+
+	echo ""
+	echo ""
+	echo ""
+	echo "- make firmware ($PLATFORM $TARGET $CPU)"
+	echo "---------------------------------------------"
+	make firmware || return 1
+	echo "============================================="
+
+	echo ""
+	echo ""
+	echo ""
+	echo "- make image ($PLATFORM $TARGET $CPU)"
+	echo "---------------------------------------------"
+	make image || return 1
+	echo "============================================="
 
 	if [ ! -z "$PROGS" ]; then
 		for PROG in $PROGS; do
 			echo ""
 			echo ""
 			echo ""
-			echo "- make load ($PROG $PLATFORM $TARGET)"
+			echo "- make load ($PROG $PLATFORM $TARGET $CPU)"
 			echo "---------------------------------------------"
 			# Allow the programming to fail.
 			PROG=$PROG make load || true
 		done
 	fi
 
-##	# Copy built files
-##	if [ -z "$GH_TOKEN" ]; then
-##		# Only if run by travis display error
-##		if [ ! -z $TRAVIS_BUILD_NUMBER  ]; then
-##			echo ""
-##			echo ""
-##			echo ""
-##			echo "- No Github token so unable to copy built files"
-##		fi
-##	elif [ -z "$TRAVIS_BRANCH" ]; then
-##		echo ""
-##		echo ""
-##		echo ""
-##		echo "- No branch name, unable to copy built files"
-##	else
-##		# Look at repo we are running in to determine where to try pushing to if in a fork
-##		COPY_REPO_OWNER=$(echo $TRAVIS_REPO_SLUG|awk -F'/' '{print $1}')
-##		echo "COPY_REPO_OWNER = $COPY_REPO_OWNER"
-##		COPY_REPO="HDMI2USB-firmware-prebuilt"
-##		GIT_REVISION=$TRAVIS_BRANCH/$(git describe)
-##		COPY_DEST="archive/$GIT_REVISION/$PLATFORM/$TARGET"
-##		ORIG_COMMITTER_NAME=$(git log -1 --pretty=%an)
-##		ORIG_COMMITTER_EMAIL=$(git log -1 --pretty=%ae)
-##		echo ""
-##		echo ""
-##		echo ""
-##		echo "- Uploading built files to github.com/$COPY_REPO_OWNER/$COPY_REPO$COPY_DEST"
-##		echo "---------------------------------------------"
-##		rm -rf $COPY_REPO
-##		git clone https://$GH_TOKEN@github.com/$COPY_REPO_OWNER/$COPY_REPO.git
-##		cd $COPY_REPO
-##		mkdir -p $COPY_DEST
-##		# Not currently built so use .bit instead
-##		#cp ../third_party/misoc/build/*.xsvf $COPY_DEST
-##		cp ../third_party/misoc/build/*.bit $COPY_DEST
-##		cp ../third_party/misoc/build/*.bin $COPY_DEST
-##		cp ../build/output.*.log $COPY_DEST/output.log
-##		echo ""
-##		echo "- Uploading .bit, .bin and logfile"
-##		# Only hdmi2usb is considered usable just now
-##		UNSTABLE_LINK="$PLATFORM/firmware/unstable"
-##		if [ "$TARGET" = "hdmi2usb" ]; then
-##			# Copy FX2 firmware
-##			cp ../firmware/fx2/hdmi2usb.hex $COPY_DEST
-##			# Create link to latest unstable build
-##			echo ""
-##			echo "- Uploading FX2 firmware"
-##			rm $UNSTABLE_LINK
-##			ln -s ../../$COPY_DEST $UNSTABLE_LINK
-##			echo ""
-##			echo "- Added symlink of $UNSTABLE_LINK -> $COPY_DEST"
-##		fi
-##		(
-##		cd $COPY_DEST
-##		sha256sum * > sha256sum.txt
-##		cat sha256sum.txt
-##		)
-##		export GIT_AUTHOR_EMAIL="$ORIG_COMMITTER_EMAIL"
-##		export GIT_AUTHOR_NAME="$ORIG_COMMITTER_NAME"
-##		export GIT_COMMITTER_EMAIL="robot@timvideos.us"
-##		export GIT_COMMITTER_NAME="Timvideos Robot"
-##		echo ""
-##		git pull
-##		git add -A .
-##		git commit -a -m "Travis build #$TRAVIS_BUILD_NUMBER of $GIT_REVISION for PLATFORM=$PLATFORM TARGET=$TARGET"
-##		git push --quiet origin master > /dev/null 2>&1
-##		cd ..
-##		rm -rf $COPY_REPO
-##		echo "============================================="
-##	fi
+	# Copy built files
+	if [ -z "$GH_TOKEN" ]; then
+		# Only if run by travis display error
+		if [ ! -z $TRAVIS_BUILD_NUMBER  ]; then
+			echo ""
+			echo ""
+			echo ""
+			echo "- No Github token so unable to copy built files"
+		fi
+	elif [ -z "$TRAVIS_BRANCH" ]; then
+		echo ""
+		echo ""
+		echo ""
+		echo "- No branch name, unable to copy built files"
+	else
+		# Look at repo we are running in to determine where to try pushing to if in a fork
+		COPY_REPO_OWNER=$(echo $TRAVIS_REPO_SLUG|awk -F'/' '{print $1}')
+		echo "COPY_REPO_OWNER = $COPY_REPO_OWNER"
+		COPY_REPO="HDMI2USB-firmware-prebuilt"
+		GIT_REVISION=$TRAVIS_BRANCH/$(git describe)
+		COPY_DEST="archive/$GIT_REVISION/$PLATFORM/${TARGET}/${CPU}/"
+		ORIG_COMMITTER_NAME=$(git log -1 --pretty=%an)
+		ORIG_COMMITTER_EMAIL=$(git log -1 --pretty=%ae)
+		echo ""
+		echo ""
+		echo ""
+		echo "- Uploading built files to github.com/$COPY_REPO_OWNER/$COPY_REPO$COPY_DEST"
+		echo "---------------------------------------------"
+		rm -rf $COPY_REPO
+		git clone https://$GH_TOKEN@github.com/$COPY_REPO_OWNER/$COPY_REPO.git
+		cd $COPY_REPO
+		mkdir -p $COPY_DEST
+		# Save the resulting binaries into the prebuilt repo. The
+		# gateware should always exist, but others might not.
+
+		# Version information
+		cp $TARGET_BUILD_DIR/software/firmware/version_data.c $COPY_DEST/version_data.c
+
+		# Gateware
+		cp $TARGET_BUILD_DIR/gateware/top.bit $COPY_DEST/gateware.bit
+		cp $TARGET_BUILD_DIR/gateware/top.bin $COPY_DEST/gateware.bin
+
+		# Gateware BIOS
+		BIOS=$TARGET_BUILD_DIR/software/bios/bios.bin
+		if [ -f $BIOS ]; then
+			cp $BIOS $COPY_DEST/
+		fi
+
+		# Soft CPU Firmware
+		SOFTCPU_FIRMWARE=$TARGET_BUILD_DIR/software/firmware/firmware.fbi
+		if [ -f $SOFTCPU_FIRMWARE ]; then
+			cp $SOFTCPU_FIRMWARE $COPY_DEST/
+		fi
+
+		# FX2 firmware
+		FX2_FIRMWARE=$TARGET_BUILD_DIR/suport/fx2.hex
+		if [ -f $FX2_FIRMWARE ]; then
+			cp $FX2_FIRMWARE $COPY_DEST
+		fi
+
+		# Combined flash binary
+		FLASH_IMAGE=$TARGET_BUILD_DIR/flash.bin
+		if [ -f $FLASH_IMAGE ]; then
+			cp $FLASH_IMAGE $COPY_DEST/
+		fi
+
+		# Log outputs
+		mkdir -p $COPY_DEST/logs/
+		cp $TARGET_BUILD_DIR/output.*.log $COPY_DEST/logs/
+		echo ""
+		echo "- Uploading binaries and logfiles"
+		# Only hdmi2usb is considered usable just now
+		UNSTABLE_LINK="$PLATFORM/firmware/unstable"
+		if [ "$TARGET" = "hdmi2usb" ]; then
+			# Create link to latest unstable build
+			rm $UNSTABLE_LINK
+			ln -s ../../$COPY_DEST $UNSTABLE_LINK
+			echo ""
+			echo "- Added symlink of $UNSTABLE_LINK -> $COPY_DEST"
+		fi
+		(
+		cd $COPY_DEST
+		sha256sum * > sha256sum.txt
+		cat sha256sum.txt
+		)
+		export GIT_AUTHOR_EMAIL="$ORIG_COMMITTER_EMAIL"
+		export GIT_AUTHOR_NAME="$ORIG_COMMITTER_NAME"
+		export GIT_COMMITTER_EMAIL="robot@timvideos.us"
+		export GIT_COMMITTER_NAME="TimVideos Robot"
+		echo ""
+		git pull
+		git add -A .
+		git commit -a -m "Travis build #$TRAVIS_BUILD_NUMBER of $GIT_REVISION for PLATFORM=$PLATFORM TARGET=$TARGET"
+		git diff origin/master
+		#git push --quiet origin master > /dev/null 2>&1
+		cd ..
+		rm -rf $COPY_REPO
+		echo "============================================="
+	fi
 
 	if [ ! -z "$CLEAN_CHECK" ]; then
 		echo ""
@@ -195,7 +251,7 @@ function build() {
 		echo ""
 		echo "- make clean ($PLATFORM $TARGET)"
 		echo "---------------------------------------------"
-		PLATFORM=$PLATFORM TARGET=$TARGET make clean
+		make clean || return 1
 		echo "============================================="
 
 		# Check that make clean didn't leave anything behind
