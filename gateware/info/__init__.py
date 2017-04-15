3ee9bcc4b4322ba2464cb5f954da4f29de388ef9
Tim 'mithro' Ansell <mithro@mithis.com>
2017-04-15 18:19:20 +1000

    gateware: Add xadc if device supports it.

--- gateware/info/__init__.py
+++ gateware/info/__init__.py
@@ -8,6 +8,7 @@ from litex.soc.interconnect.csr import *
 
 from gateware.info import git
 from gateware.info import dna
+from gateware.info import xadc
 from gateware.info import platform as platform_info
 
 
@@ -16,3 +17,7 @@ class Info(Module, AutoCSR):
         self.submodules.dna = dna.DNA()
         self.submodules.git = git.GitInfo()
         self.submodules.platform = platform_info.PlatformInfo(platform_name, target_name)
+
+        if "xc7" in platform.device:
+            self.submodules.xadc = xadc.XADC()
+
