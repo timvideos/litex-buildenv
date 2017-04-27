ad8b72c822c3cfa2d3b7ea1760400fc8ed95fb68
Florent Kermarrec <florent@enjoy-digital.fr>
2017-04-27 18:10:37 +0200

    nexys_video: integrate hdmi_in, probably not working but will allow debug over udp

--- nexys_video.py
+++ nexys_video.py
@@ -2,10 +2,10 @@
 
 from nexys_base import *
 
-from litex.soc.interconnect import stream
-
+from litevideo.input import HDMIIn
 from litevideo.output import VideoOut
-from litedram.common import LiteDRAMPort
+
+from gateware.freq_measurement import FrequencyMeasurement
 
 base_cls = MiniSoC
 
@@ -13,30 +13,37 @@ base_cls = MiniSoC
 class VideoOutSoC(base_cls):
     csr_map = {
         "hdmi_out0": 21,
-        "analyzer":  22
+        "hdmi_in0": 22,
+        "hdmi_in0_freq": 23,
+        "hdmi_in0_edid_mem": 24,
+        "analyzer": 25
     }
     csr_map.update(base_cls.csr_map)
+
+    interrupt_map = {
+        "hdmi_in0": 3,
+    }
+    interrupt_map.update(base_cls.interrupt_map)
 
     def __init__(self, platform, *args, **kwargs):
         base_cls.__init__(self, platform, *args, **kwargs)
 
         # # #
 
+        # hdmi in 0
+        self.submodules.hdmi_in0 = HDMIIn(platform.request("hdmi_in", 0),
+                                          self.sdram.crossbar.get_port(mode="write"),
+                                          fifo_depth=512,
+                                          device="xc7")
+        self.submodules.hdmi_in0_freq = FrequencyMeasurement(self.hdmi_in0.clocking.cd_pix.clk,
+                                                             self.clk_freq)
 
-        mode = "ycbcr422"
-
-        if mode == "ycbcr422":
-            hdmi_out0_dram_port = self.sdram.crossbar.get_port(mode="read", dw=16, cd="pix", reverse=True)
-            self.submodules.hdmi_out0 = VideoOut(platform.device,
-                                                 platform.request("hdmi_out"),
-                                                 hdmi_out0_dram_port,
-                                                 "ycbcr422")
-        elif mode == "rgb":
-            hdmi_out0_dram_port = self.sdram.crossbar.get_port(mode="read", dw=32, cd="pix", reverse=True)
-            self.submodules.hdmi_out0 = VideoOut(platform.device,
-                                                 platform.request("hdmi_out"),
-                                                 hdmi_out0_dram_port,
-                                                 "rgb")
+        # hdmi out 0
+        hdmi_out0_dram_port = self.sdram.crossbar.get_port(mode="read", dw=16, cd="hdmi_out0_pix", reverse=True)
+        self.submodules.hdmi_out0 = VideoOut(platform.device,
+                                             platform.request("hdmi_out"),
+                                             hdmi_out0_dram_port,
+                                             "ycbcr422")
 
         self.platform.add_period_constraint(self.hdmi_out0.driver.clocking.cd_pix.clk, 10.0)
         self.platform.add_false_path_constraints(
