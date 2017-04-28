57ec5f442a951cc04e7ad6865824987e04f810a5
Florent Kermarrec <florent@enjoy-digital.fr>
2017-04-28 13:42:46 +0200

    nexys_video: only keep input, add analyzer

--- nexys_video.py
+++ nexys_video.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 
-from nexys_base import *
+from nexys_etherbone import *
 
 from litevideo.input import HDMIIn
 from litevideo.output import VideoOut
 
+from litescope import LiteScopeAnalyzer
+
 from gateware.freq_measurement import FrequencyMeasurement
 
-base_cls = MiniSoC
+base_cls = EtherboneSoC
 
 
 class VideoOutSoC(base_cls):
     csr_map = {
-        "hdmi_out0": 21,
-        "hdmi_in0": 22,
-        "hdmi_in0_freq": 23,
-        "hdmi_in0_edid_mem": 24,
+        "hdmi_in": 22,
+        "hdmi_in_freq": 23,
+        "hdmi_in_edid_mem": 24,
         "analyzer": 25
     }
     csr_map.update(base_cls.csr_map)
 
     interrupt_map = {
-        "hdmi_in0": 3,
+        "hdmi_in": 3,
     }
     interrupt_map.update(base_cls.interrupt_map)
 
@@ -30,25 +31,38 @@ class VideoOutSoC(base_cls):
 
         # # #
 
-        # hdmi in 0
-        self.submodules.hdmi_in0 = HDMIIn(platform.request("hdmi_in", 0),
-                                          self.sdram.crossbar.get_port(mode="write"),
-                                          fifo_depth=512,
-                                          device="xc7")
-        self.submodules.hdmi_in0_freq = FrequencyMeasurement(self.hdmi_in0.clocking.cd_pix.clk,
-                                                             self.clk_freq)
+        # hdmi in
+        self.submodules.hdmi_in = HDMIIn(platform.request("hdmi_in", 0),
+                                         self.sdram.crossbar.get_port(mode="write"),
+                                         fifo_depth=512,
+                                         device="xc7")
+        self.submodules.hdmi_in_freq = FrequencyMeasurement(self.hdmi_in.clocking.cd_pix.clk,
+                                                            self.clk_freq)
 
-        # hdmi out 0
-        hdmi_out0_dram_port = self.sdram.crossbar.get_port(mode="read", dw=16, cd="hdmi_out0_pix", reverse=True)
-        self.submodules.hdmi_out0 = VideoOut(platform.device,
-                                             platform.request("hdmi_out"),
-                                             hdmi_out0_dram_port,
-                                             "ycbcr422")
 
-        self.platform.add_period_constraint(self.hdmi_out0.driver.clocking.cd_pix.clk, 10.0)
-        self.platform.add_false_path_constraints(
-            self.crg.cd_sys.clk,
-            self.hdmi_out0.driver.clocking.cd_pix.clk)
+        analyzer_signals = [
+            self.hdmi_in.data0_cap.alignment.delay_value,
+            self.hdmi_in.data0_cap.alignment.delay_ce,
+            self.hdmi_in.data0_cap.alignment.bitslip_value,
+            self.hdmi_in.data0_cap.alignment.invalid,
+            self.hdmi_in.data0_cap.d,
+
+            self.hdmi_in.data1_cap.alignment.delay_value,
+            self.hdmi_in.data1_cap.alignment.delay_ce,
+            self.hdmi_in.data1_cap.alignment.bitslip_value,
+            self.hdmi_in.data1_cap.alignment.invalid,
+            self.hdmi_in.data1_cap.d,
+
+            self.hdmi_in.data2_cap.alignment.delay_value,
+            self.hdmi_in.data2_cap.alignment.delay_ce,
+            self.hdmi_in.data2_cap.alignment.bitslip_value,
+            self.hdmi_in.data2_cap.alignment.invalid,
+            self.hdmi_in.data2_cap.d
+        ]
+        self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 2048, cd="pix")
+
+    def do_exit(self, vns):
+        self.analyzer.export_csv(vns, "test/analyzer.csv")
 
 
 def main():
