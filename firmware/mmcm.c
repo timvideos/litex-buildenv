10363603fbc69d28458c178a632974d79510919f
Florent Kermarrec <florent@enjoy-digital.fr>
2017-06-01 17:26:52 +0200

    firmware: cleaunup mmcm comments

--- firmware/mmcm.c
+++ firmware/mmcm.c
@@ -37,24 +37,24 @@ static int hdmi_in_clocking_mmcm_read(int adr) {
 }
 
 void hdmi_in_0_config_30_60mhz(void) {
-	hdmi_out0_mmcm_write(0x14, 0x1000 | (10<<6) | 10); // # clkfbout_mult=20
-	hdmi_out0_mmcm_write(0x08, 0x1000 | (10<<6) | 10); // # clkout0_divide=20
-	hdmi_out0_mmcm_write(0x0a, 0x1000 |  (8<<6) | 8);  // # clkout1_divide=16
-	hdmi_out0_mmcm_write(0x0c, 0x1000 |  (2<<6) | 2);  // # clkout2_divide=4
+	hdmi_out0_mmcm_write(0x14, 0x1000 | (10<<6) | 10); /* clkfbout_mult  = 20 */
+	hdmi_out0_mmcm_write(0x08, 0x1000 | (10<<6) | 10); /* clkout0_divide = 20 */
+	hdmi_out0_mmcm_write(0x0a, 0x1000 |  (8<<6) |  8); /* clkout1_divide = 16 */
+	hdmi_out0_mmcm_write(0x0c, 0x1000 |  (2<<6) |  2); /* clkout2_divide =  4 */
 }
 
 void hdmi_in_0_config_60_120mhz(void) {
-	hdmi_out0_mmcm_write(0x14, 0x1000 |  (5<<6) | 5);  // # clkfbout_mult=10
-	hdmi_out0_mmcm_write(0x08, 0x1000 |  (5<<6) | 5);  // # clkout0_divide=10
-	hdmi_out0_mmcm_write(0x0a, 0x1000 |  (4<<6) | 4);  // # clkout1_divide=8
-	hdmi_out0_mmcm_write(0x0c, 0x1000 |  (1<<6) | 1);  // # clkout2_divide=2
+	hdmi_out0_mmcm_write(0x14, 0x1000 |  (5<<6) | 5); /* clkfbout_mult  = 10 */
+	hdmi_out0_mmcm_write(0x08, 0x1000 |  (5<<6) | 5); /* clkout0_divide = 10 */
+	hdmi_out0_mmcm_write(0x0a, 0x1000 |  (4<<6) | 4); /* clkout1_divide =  8 */
+	hdmi_out0_mmcm_write(0x0c, 0x1000 |  (1<<6) | 1); /* clkout2_divide =  2 */
 }
 
 void hdmi_in_0_config_120_240mhz(void) {
-	hdmi_out0_mmcm_write(0x14, 0x1000 |  (2<<6) | 3);  // # clkfbout_mult=5
-	hdmi_out0_mmcm_write(0x08, 0x1000 |  (2<<6) | 3);  // # clkout0_divide=5
-	hdmi_out0_mmcm_write(0x0a, 0x1000 |  (2<<6) | 2);  // # clkout1_divide=4
-	hdmi_out0_mmcm_write(0x0c, 0x1000 |  (0<<6) | 0);  // # clkout2_divide=1
+	hdmi_out0_mmcm_write(0x14, 0x1000 |  (2<<6) | 3);  /* clkfbout_mult  = 5 */
+	hdmi_out0_mmcm_write(0x08, 0x1000 |  (2<<6) | 3);  /* clkout0_divide = 5 */
+	hdmi_out0_mmcm_write(0x0a, 0x1000 |  (2<<6) | 2);  /* clkout1_divide = 4 */
+	hdmi_out0_mmcm_write(0x0c, 0x1000 |  (0<<6) | 0);  /* clkout2_divide = 1 */
 }
 
 void mmcm_config_for_clock(int freq)
