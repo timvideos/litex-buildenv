e90fab57602d4d0aaa96cc0d1908d9c5e6e4aa05
Florent Kermarrec <florent@enjoy-digital.fr>
2017-06-06 17:45:07 +0200

    netv2_video: cleanup/update

--- netv2_video.py
+++ netv2_video.py
@@ -4,57 +4,80 @@ from netv2_base import *
 
 from litevideo.input import HDMIIn
 from litevideo.output import VideoOut
-from litedram.common import LiteDRAMPort
+
+from litex.soc.cores.frequency_meter import FrequencyMeter
+
+from litescope import LiteScopeAnalyzer
 
 base_cls = BaseSoC
 
 
-class VideoOutSoC(base_cls):
-    csr_map = {
-        "hdmi_out": 26,
-        "hdmi_in": 27,
-        "hdmi_in_edid_mem": 28,
+class VideoSoC(base_cls):
+    csr_peripherals = {
+        "hdmi_out0",
+        "hdmi_in0",
+        "hdmi_in0_freq",
+        "hdmi_in0_edid_mem",
+        "analyzer",
     }
-    csr_map.update(base_cls.csr_map)
+    csr_map_update(base_cls.csr_map, csr_peripherals)
 
     interrupt_map = {
-        "hdmi_in": 3,
+        "hdmi_in0": 3,
     }
     interrupt_map.update(base_cls.interrupt_map)
 
     def __init__(self, platform, *args, **kwargs):
-        base_cls.__init__(self, platform, *args, pcie_blink=False, **kwargs)
+        base_cls.__init__(self, platform, *args, **kwargs)
 
         # # #
 
+        pix_freq = 148.50e6
+
         # hdmi in
-        self.submodules.hdmi_in = HDMIIn(platform.request("hdmi_in", 0),
+        hdmi_in0_pads = platform.request("hdmi_in")
+        self.submodules.hdmi_in0_freq = FrequencyMeter(period=self.clk_freq)
+        self.submodules.hdmi_in0 = HDMIIn(hdmi_in0_pads,
                                          self.sdram.crossbar.get_port(mode="write"),
                                          fifo_depth=512,
                                          device="xc7")
+        self.comb += self.hdmi_in0_freq.clk.eq(self.hdmi_in0.clocking.cd_pix.clk)
+        self.platform.add_period_constraint(self.hdmi_in0.clocking.cd_pix.clk, period_ns(1*pix_freq))
+        self.platform.add_period_constraint(self.hdmi_in0.clocking.cd_pix1p25x.clk, period_ns(1.25*pix_freq))
+        self.platform.add_period_constraint(self.hdmi_in0.clocking.cd_pix5x.clk, period_ns(5*pix_freq))
 
-        # hdmi out
-        hdmi_out_dram_port = self.sdram.crossbar.get_port(mode="read", dw=16, cd="hdmi_out_pix", reverse=True)
-        self.submodules.hdmi_out = VideoOut(platform.device,
-                                            platform.request("hdmi_out"),
-                                            hdmi_out_dram_port,
-                                            "ycbcr422")
-
-        self.platform.add_period_constraint(self.crg.cd_sys.clk, 10.0)
-        self.platform.add_period_constraint(self.hdmi_out.driver.clocking.cd_pix.clk, 10.0)
         self.platform.add_false_path_constraints(
             self.crg.cd_sys.clk,
-            self.hdmi_out.driver.clocking.cd_pix.clk)
+            self.hdmi_in0.clocking.cd_pix.clk,
+            self.hdmi_in0.clocking.cd_pix1p25x.clk,
+            self.hdmi_in0.clocking.cd_pix5x.clk)
+
+        # hdmi out
+        hdmi_out0_dram_port = self.sdram.crossbar.get_port(mode="read", dw=16, cd="hdmi_out0_pix", reverse=True)
+        self.submodules.hdmi_out0 = VideoOut(platform.device,
+                                            platform.request("hdmi_out"),
+                                            hdmi_out0_dram_port,
+                                            "ycbcr422",
+                                            fifo_depth=4096)
+
+        self.platform.add_period_constraint(self.hdmi_out0.driver.clocking.cd_pix.clk, period_ns(1*pix_freq))
+        self.platform.add_period_constraint(self.hdmi_out0.driver.clocking.cd_pix5x.clk, period_ns(5*pix_freq))
+
+        self.platform.add_false_path_constraints(
+            self.crg.cd_sys.clk,
+            self.hdmi_out0.driver.clocking.cd_pix.clk,
+            self.hdmi_out0.driver.clocking.cd_pix5x.clk)
 
 
 def main():
     parser = argparse.ArgumentParser(description="NeTV2 LiteX SoC")
     builder_args(parser)
     soc_sdram_args(parser)
+    parser.add_argument("--nocompile-gateware", action="store_true")
     args = parser.parse_args()
 
     platform = netv2.Platform()
-    soc = VideoOutSoC(platform, **soc_sdram_argdict(args))
+    soc = VideoSoC(platform, **soc_sdram_argdict(args))
     builder = Builder(soc, output_dir="build")
     vns = builder.build()
 
