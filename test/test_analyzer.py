e4fab91c2a28a13bd6dbeaa286910989ec77790f
Florent Kermarrec <florent@enjoy-digital.fr>
2017-06-23 09:15:00 +0200

    update litescope

--- test/test_analyzer.py
+++ test/test_analyzer.py
@@ -9,16 +9,12 @@ wb.open()
 
 # # #
 
-logic_analyzer = LiteScopeAnalyzerDriver(wb.regs, "analyzer", debug=True)
-
-logic_analyzer.configure_trigger(cond={"charsync0_data": 0x354})
-logic_analyzer.run(offset=32, length=128)
-
-while not logic_analyzer.done():
-    pass
-logic_analyzer.upload()
-
-logic_analyzer.save("dump.vcd")
+analyzer = LiteScopeAnalyzerDriver(wb.regs, "analyzer", debug=True)
+analyzer.configure_trigger(cond={"charsync0_data": 0x354})
+analyzer.run(offset=32, length=128)
+analyzer.wait_done()
+analyzer.upload()
+analyzer.save("dump.vcd")
 
 # # #
 
