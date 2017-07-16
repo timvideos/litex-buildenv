3041aa5708039a8d5821205c7fdb9030733ec845
Tim 'mithro' Ansell <mithro@mithis.com>
2017-07-16 21:12:05 +1000

    targets/opsis: Fixing Makefile targets.

--- targets/opsis/Makefile.mk
+++ targets/opsis/Makefile.mk
@@ -2,20 +2,29 @@
 
 TARGET ?= video
 
-gateware-load-opsis: tftp
+gateware-load-opsis:
 	opsis-mode-switch --verbose --load-gateware $(TARGET_BUILD_DIR)/gateware/top.bit
+
+gateware-flash-opsis:
+	opsis-mode-switch --verbose --flash-gateware=$(TARGET_BUILD_DIR)/gateware/top.bin
 
 firmware-load-opsis:
 	opsis-mode-switch --verbose --mode=serial
 	flterm --port=$$(opsis-mode-switch --get-serial-dev) --kernel=$(TARGET_BUILD_DIR)/software/firmware/firmware.bin
 
+firmware-flash-opsis:
+	opsis-mode-switch --verbose --flash-lm32-firmware=$(TARGET_BUILD_DIR)/software/firmware/firmware.fbi
+
+firmware-connect-opsis:
+	opsis-mode-switch --verbose --mode=serial
+	flterm --port=$$(opsis-mode-switch --get-serial-dev)
+
+image-flash-opsis:
+	opsis-mode-switch --verbose --flash-gateware=$(TARGET_BUILD_DIR)/flash.bin
+
 reset-opsis:
 	opsis-mode-switch --verbose --mode=serial
 	opsis-mode-switch --verbose --mode=jtag
 	opsis-mode-switch --verbose --mode=serial
-
-flash-opsis:
-	opsis-mode-switch --verbose --flash-gateware=$(TARGET_BUILD_DIR)/gateware/top.bin
-	opsis-mode-switch --verbose --flash-lm32-firmware=$(TARGET_BUILD_DIR)/software/firmware/firmware.fbi
 
 .PHONY: gateware-load-opsis firmware-load-opsis reset-opsis flash-opsis
