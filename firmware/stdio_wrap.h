e56d634b1463b0ac706fa458d113e98bd4c3b080
Stefano Rivera <stefano@rivera.za.net>
2017-08-03 20:34:41 -0400

    Move LF->CR LF translation to stdio_wrap

--- firmware/stdio_wrap.h
+++ firmware/stdio_wrap.h
@@ -1,5 +1,10 @@
 #include <stdarg.h>
 
+#define STDIO_BUFFER_SIZE 256
+char stdio_write_buffer[STDIO_BUFFER_SIZE];
+
+char *translate_crlf(const char *s, int trailing_lf);
+
 int wputs(const char *s);
 int wputchar(int c);
 int wprintf(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
