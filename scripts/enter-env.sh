c42796ea19aff14e87aff1fbfb3936ffa4ff2df3
Tim Ansell <mithro@mithis.com>
2017-08-07 16:08:28 +1000

    Keep searching in old locations.

--- scripts/enter-env.sh
+++ scripts/enter-env.sh
@@ -42,7 +42,7 @@ if [ -d /lib/firmware/ixo-usb-jtag/ ]; then
 	return 1
 fi
 
-if [ -f /lib/udev/rules.d/60-hdmi2usb-udev.rules -o ! -z "$HDMI2USB_UDEV_IGNORE" ]; then
+if [ -f /etc/udev/rules.d/99-hdmi2usb-permissions.rules -o -f /lib/udev/rules.d/99-hdmi2usb-permissions.rules -o -f /lib/udev/rules.d/60-hdmi2usb-udev.rules -o	! -z "$HDMI2USB_UDEV_IGNORE"]; then
 	true
 else
 	echo "Please install the HDMI2USB udev rules."
