8a30b827498962ecb16dc2a9d999706f20d52947
Stefano Rivera <stefano@rivera.za.net>
2017-11-26 15:02:40 +0000

    Calculate a dynamic FRAMEBUFFERS_BASE for each HDMI input

--- firmware/hdmi_in0.h
+++ firmware/hdmi_in0.h
@@ -4,7 +4,8 @@
 #ifndef __HDMI_IN0_H
 #define __HDMI_IN0_H
 
-#define HDMI_IN0_FRAMEBUFFERS_BASE (0x01000000 + 0x100000)
+#define HDMI_IN0_INDEX 0
+#define HDMI_IN0_FRAMEBUFFERS_BASE ((HDMI_IN0_INDEX + 1) * 0x01000000 + 0x100000)
 
 extern int hdmi_in0_debug;
 extern int hdmi_in0_fb_index;
