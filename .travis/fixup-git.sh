c6a9a995628353fcafbcce40f9356473373b3ae7
Tim 'mithro' Ansell <me@mith.ro>
2018-01-11 19:18:51 +1100

    travis: Understand the git pull request failure problem now.
    See the following page - https://discourse.drone.io/t/github-claims-that-merge-refs-are-undocumented-feature/1100
    
    > There appears to be a bug in Github where upon pull request creation,
    > or pushing to an existing pull request, the PR webhook is fired before
    > the merge ref is created.
    >
    > This results in dangerous bugs in CI systems where the code being
    > tested in a CI job is not the code that the PR author thinks is being
    > tested.
    >
    > Specifically when this happens, one of two things happens:
    >
    > 1) The CI job fails because the merge ref cannot be found (if this is
    >    right when the PR is opened)
    >
    > 2) The CI job runs but uses the wrong commit (if this is a subsequent
    >    push to an existing PR) - This is because the FETCH_HEAD is
    >    pointing to an older commit.

---
+++ .travis/fixup-git.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+set -e
+
+echo ""
+echo ""
+echo ""
+echo "- Fetching non shallow to get git version"
+echo "---------------------------------------------"
+git fetch origin --unshallow || true
+git fetch origin --tags
+
+if [ z"$TRAVIS_PULL_REQUEST_SLUG" != z ]; then
+	echo ""
+	echo ""
+	echo ""
+	echo "- Fetching from pull request source"
+	echo "---------------------------------------------"
+	git remote add source https://github.com/$TRAVIS_PULL_REQUEST_SLUG.git
+	git fetch source && git fetch --tags
+
+	echo ""
+	echo ""
+	echo ""
+	echo "- Fetching the actual pull request"
+	echo "---------------------------------------------"
+	git fetch origin pull/$TRAVIS_PULL_REQUEST/head:pull-$TRAVIS_PULL_REQUEST-head
+	git fetch origin pull/$TRAVIS_PULL_REQUEST/merge:pull-$TRAVIS_PULL_REQUEST-merge
+	echo "---------------------------------------------"
+	git log -n 5 --graph pull-$TRAVIS_PULL_REQUEST-head
+	echo "---------------------------------------------"
+	git log -n 5 --graph pull-$TRAVIS_PULL_REQUEST-merge
+	echo "---------------------------------------------"
+
+	GITHUB_CURRENT_MERGE_SHA1="$(git log --pretty=format:'%H' -n 1 pull-$TRAVIS_PULL_REQUEST-merge)"
+	if [ "$GITHUB_CURRENT_MERGE_SHA1" != "$TRAVIS_COMMIT" ]; then
+		echo ""
+		echo ""
+		echo ""
+		echo "- Pull request triggered for $TRAVIS_COMMIT but now at $GITHUB_CURRENT_MERGE_SHA1"
+		echo ""
+		echo "  SKIPPING!"
+		echo ""
+		exit 1
+	fi
+
+	echo ""
+	echo ""
+	echo ""
+	echo "- Using pull request version of submodules (if they exist)"
+	echo "---------------------------------------------"
+	git submodule status | while read SHA1 MODULE_PATH
+	do
+		"$PWD/.travis/add-local-submodule.sh" "$TRAVIS_PULL_REQUEST_SLUG" "$MODULE_PATH"
+	done
+	echo "---------------------------------------------"
+	git submodule foreach --recursive 'git remote -v; echo'
+	echo "---------------------------------------------"
+fi
+
+if [ z"$TRAVIS_REPO_SLUG" != z ]; then
+	echo ""
+	echo ""
+	echo ""
+	echo "- Using local version of submodules (if they exist)"
+	echo "---------------------------------------------"
+	git submodule status | while read SHA1 MODULE_PATH DESC
+	do
+		"$PWD/.travis/add-local-submodule.sh" "$TRAVIS_REPO_SLUG" "$MODULE_PATH"
+	done
+	echo "---------------------------------------------"
+	git submodule foreach --recursive 'git remote -v; echo'
+	echo "---------------------------------------------"
+fi
+
+echo "---------------------------------------------"
+git show-ref
+echo "---------------------------------------------"
+
+if [ z"$TRAVIS_BRANCH" != z ]; then
+	TRAVIS_COMMIT_ACTUAL=$(git log --pretty=format:'%H' -n 1)
+	echo ""
+	echo ""
+	echo ""
+	echo "Fixing detached head (current $TRAVIS_COMMIT_ACTUAL -> $TRAVIS_COMMIT)"
+	echo "---------------------------------------------"
+	git log -n 5 --graph
+	echo "---------------------------------------------"
+	git fetch origin $TRAVIS_COMMIT
+	git branch -v
+	echo "---------------------------------------------"
+	git log -n 5 --graph
+	echo "---------------------------------------------"
+	git branch -D $TRAVIS_BRANCH || true
+	git checkout $TRAVIS_COMMIT -b $TRAVIS_BRANCH
+	git branch -v
+fi
+echo ""
+echo ""
+echo ""
+echo "Git Revision"
+echo "---------------------------------------------"
+git status
+echo "---------------------------------------------"
+git describe
+echo "============================================="
+GIT_REVISION=$(git describe)
+
+echo ""
+echo ""
+echo ""
+echo "- Disk space free (after fixing git)"
+echo "---------------------------------------------"
+df -h
+echo ""
+DF_AFTER_GIT="$(($(stat -f --format="%a*%S" .)))"
+awk "BEGIN {printf \"Git is using %.2f megabytes\n\",($DF_LAST-$DF_AFTER_GIT)/1024/1024}"
+DF_LAST="$DF_AFTER_GIT"
