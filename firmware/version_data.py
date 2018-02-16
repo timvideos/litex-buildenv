48a3b77f560f985379179c83cbc799558411db5b
gsmalik <gsmalik@uwaterloo.ca>
2018-02-15 22:15:38 -0500

    Moving to python3

--- firmware/version_data.py
+++ firmware/version_data.py
@@ -7,56 +7,56 @@ import  filecmp
 import  shutil
 
 commit = subprocess.check_output(['git', 'log', '--format="%H"', '-n', '1'])
-print "Showing commit variable"
-print commit[1:-2]
+print ("Showing commit variable")
+print (commit[1:-2])
 
 branch = subprocess.check_output(['git', 'symbolic-ref', '--short', 'HEAD'])
-print "Showing branch variable"
-print branch[:-1]
+print ("Showing branch variable")
+print (branch[:-1])
 
 describe = subprocess.check_output(['git', 'describe', '--dirty'])
-print   "Showing describe variable"
-print describe[:-1]
+print ("Showing describe variable")
+print (describe[:-1])
 
 status = subprocess.check_output(['git', 'status', '--short'])
+print (status)
 
-print "Showing uplatform"
+print ("Showing uplatform")
 if "PLATFORM" in os.environ:
     platform = os.environ['PLATFORM']
-    print platform.upper()
+    print (platform.upper())
 else:
     platform = ""
 
-print "Showing utarget"
+print ("Showing utarget")
 if "TARGET" in os.environ:
     target = os.environ['TARGET']
-    print target.upper()
+    print (target.upper())
 else:
     target = ""
 
 temp_h = tempfile.NamedTemporaryFile( suffix='.h',delete='true')
-print "Showing temp file .h"
-print temp_h.name
-temp_h.write ("""\
+print ("Showing temp file .h")
+print (temp_h.name)
+temp_h.write (b"""\
 #ifndef __VERSION_DATA_H
 #define __VERSION_DATA_H
-
 extern const char* board;
 extern const char* target;
-
 extern const char* git_commit;
 extern const char* git_branch;
 extern const char* git_describe;
 extern const char* git_status;
-
-#endif  // __VERSION_DATA_H""")
+#endif  // __VERSION_DATA_H""" )
 temp_h.seek(0)
 
-temp_c          = tempfile.NamedTemporaryFile( suffix='.c',delete='true')
-print   "Showing temp file .c"
-print   temp_c.name
+length = status.count(b'\n')
 
-temp_c.write    ("""\
+temp_c          = tempfile.NamedTemporaryFile( suffix='.c',delete='true')
+print ("Showing temp file .c")
+print (temp_c.name)
+
+temp_c.write    (b"""\
 
 #ifndef PLATFORM_%s
 #error \"Version mismatch - PLATFORM_%s not defined!\"
@@ -73,25 +73,25 @@ const char* git_branch = \"%s\";
 const char* git_describe = \"%s\";
 const char* git_status =
     \"    --\\r\\n\"
-""" % (platform.upper(),platform.upper(),platform,target.upper(),target.upper(),target,commit[1:-2],branch[:-1],describe[:-1]))
+""" % (platform.upper().encode(),platform.upper().encode(),platform.encode(),target.upper().encode(),target.upper().encode(),target.encode(),commit[1:-2],branch[:-1],describe[:-1]))
 
-for x in range ( 0, len(status.split('\n'))-1 ):
+for x in range ( 0, length):
     temp    = status.splitlines()[x]
-    temp    = '   "    ' + temp + '\\r\\n"'
-    temp_c.write    (temp)
-    temp_c.write    ('\n')
-temp_c.write    ('    "    --\\r\\n";')
+    temp    = '   "    ' + temp.decode() + '\\r\\n"'
+    temp_c.write    (temp.encode())
+    temp_c.write    (b'\n')
+temp_c.write    (b'    "    --\\r\\n";')
 temp_c.seek(0)
 
 
 if not ( filecmp.cmp(temp_h.name,'version_data.h') ):
-    print   "Updating version_data.h"
-    os.remove       ('version_data.h')
+    print (  "Updating version_data.h")
+    os.remove ('version_data.h')
     shutil.copyfile (temp_h.name,'version_data.h')
 
 if not ( filecmp.cmp(temp_c.name,'version_data.c') ):
-    print   "Updating version_data.c"
-    os.remove       ('version_data.c')
+    print (  "Updating version_data.c")
+    os.remove ('version_data.c')
     shutil.copyfile (temp_c.name,'version_data.c')
 temp_c.close()
 temp_h.close()
