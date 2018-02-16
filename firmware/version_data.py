42f5db00ef30b5a76a013701cf6395d39be5d08c
gsmalik <gsmalik@uwaterloo.ca>
2018-02-16 00:10:15 -0500

    pycodestyle in the build env O_O

--- firmware/version_data.py
+++ firmware/version_data.py
@@ -7,38 +7,38 @@ import filecmp
 import shutil
 
 commit = subprocess.check_output(['git', 'log', '--format="%H"', '-n', '1'])
-print ("Showing commit variable")
-print (commit[1:-2])
+print("Showing commit variable")
+print(commit[1:-2])
 
 branch = subprocess.check_output(['git', 'symbolic-ref', '--short', 'HEAD'])
-print ("Showing branch variable")
-print (branch[:-1])
+print("Showing branch variable")
+print(branch[:-1])
 
 describe = subprocess.check_output(['git', 'describe', '--dirty'])
-print ("Showing describe variable")
-print (describe[:-1])
+print("Showing describe variable")
+print(describe[:-1])
 
 status = subprocess.check_output(['git', 'status', '--short'])
 length = status.count(b'\n')
-print (status)
+print(status)
 
-print ("Showing uplatform")
+print("Showing uplatform")
 if "PLATFORM" in os.environ:
     platform = os.environ['PLATFORM']
-    print (platform.upper())
+    print(platform.upper())
 else:
     platform = ""
 
-print ("Showing utarget")
+print("Showing utarget")
 if "TARGET" in os.environ:
     target = os.environ['TARGET']
-    print (target.upper())
+    print(target.upper())
 else:
     target = ""
 
 temp_h = tempfile.NamedTemporaryFile(suffix='.h', delete='true')
-print ("Showing temp file .h")
-print (temp_h.name)
+print("Showing temp file .h")
+print(temp_h.name)
 temp_h.write(b"""\
 #ifndef __VERSION_DATA_H
 #define __VERSION_DATA_H
@@ -53,8 +53,8 @@ temp_h.seek(0)
 
 
 temp_c = tempfile.NamedTemporaryFile(suffix='.c', delete='true')
-print ("Showing temp file .c")
-print (temp_c.name)
+print("Showing temp file .c")
+print(temp_c.name)
 
 temp_c.write(b"""\
 
@@ -87,12 +87,12 @@ temp_c.seek(0)
 
 
 if not (filecmp.cmp(temp_h.name, 'version_data.h')):
-    print ("Updating version_data.h")
+    print("Updating version_data.h")
     os.remove('version_data.h')
     shutil.copyfile(temp_h.name, 'version_data.h')
 
 if not (filecmp.cmp(temp_c.name, 'version_data.c')):
-    print ("Updating version_data.c")
+    print("Updating version_data.c")
     os.remove('version_data.c')
     shutil.copyfile(temp_c.name, 'version_data.c')
 temp_c.close()
