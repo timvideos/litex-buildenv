dbf8b293b906c5133f31ec791f0c442ce6dda7b2
Tim 'mithro' Ansell <me@mith.ro>
2018-09-21 10:34:59 -0700

    scripts: Fix double equals.

--- scripts/download-env.sh
+++ scripts/download-env.sh
@@ -296,7 +296,7 @@ fi
 
 # FIXME: Remove this once @jimmo has finished his new firmware
 # MimasV2Config.py
-if [ "$PLATFORM" == "mimasv2" ]; then
+if [ "$PLATFORM" = "mimasv2" ]; then
 	MIMASV2CONFIG=$BUILD_DIR/conda/bin/MimasV2Config.py
 	echo
 	echo "Installing MimasV2Config.py (mimasv2 flashing tool)"
