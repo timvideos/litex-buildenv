45108363b9a280257e2e2b33674503b104b33788
Piotr Binkowski <pbinkowski@antmicro.com>
2019-11-27 18:32:17 +0100

    targets: use fx2crossbar in HDMI2USB targets

--- targets/opsis/hdmi2usb.py
+++ targets/opsis/hdmi2usb.py
@@ -4,6 +4,7 @@ from litex.soc.interconnect import stream
 
 from gateware.encoder import EncoderDMAReader, EncoderBuffer, Encoder
 from gateware.streamer import USBStreamer
+from gateware.fx2_crossbar import FX2Crossbar
 
 from targets.utils import csr_map_update
 from targets.opsis.video import SoC as BaseSoC
@@ -30,27 +31,20 @@ class HDMI2USBSoC(BaseSoC):
                                            "read": "encoder"})(encoder_cdc)
         encoder_buffer = ClockDomainsRenamer("encoder")(EncoderBuffer())
         encoder = Encoder(platform)
-        encoder_streamer = USBStreamer(platform, platform.request("fx2"))
-        self.submodules += encoder_cdc, encoder_buffer, encoder, encoder_streamer
+        xbar = FX2Crossbar(platform)
+        self.submodules += encoder_cdc, encoder_buffer, encoder, xbar
 
         self.comb += [
             self.encoder_reader.source.connect(encoder_cdc.sink),
             encoder_cdc.source.connect(encoder_buffer.sink),
             encoder_buffer.source.connect(encoder.sink),
-            encoder.source.connect(encoder_streamer.sink)
+            encoder.source.connect(xbar.get_in_fifo(0))
         ]
         self.add_wb_slave(mem_decoder(self.mem_map["encoder"]), encoder.bus)
         self.add_memory_region("encoder",
             self.mem_map["encoder"] + self.shadow_base, 0x2000)
 
-        self.platform.add_period_constraint(encoder_streamer.cd_usb.clk, 10.0)
-
-        encoder_streamer.cd_usb.clk.attr.add("keep")
         self.crg.cd_encoder.clk.attr.add("keep")
-        self.platform.add_false_path_constraints(
-            self.crg.cd_sys.clk,
-            self.crg.cd_encoder.clk,
-            encoder_streamer.cd_usb.clk)
 
 
 SoC = HDMI2USBSoC
