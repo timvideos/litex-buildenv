a29640c5e3a8218b0a30263663664c933d4101f7
Tim 'mithro' Ansell <me@mith.ro>
2020-01-15 12:56:49 +1000

    arty: Cleanup and modernize.

--- targets/arty/etherbone.py
+++ targets/arty/etherbone.py
@@ -1,16 +1,17 @@
+from litex.soc.integration.soc_core import mem_decoder
 from litex.soc.integration.soc_sdram import *
 
-from liteeth.core.mac import LiteEthMAC
+from liteeth.common import convert_ip
+from liteeth.core import LiteEthUDPIPCore
+from liteeth.frontend.etherbone import LiteEthEtherbone
+from liteeth.mac import LiteEthMAC
 from liteeth.phy import LiteEthPHY
 
-from .base import BaseSoC
+from targets.utils import csr_map_update
+from targets.arty.base import SoC as BaseSoC
 
 
-class NetSoC(BaseSoC):
-    mem_map = {**BaseSoC.mem_map, **{
-        "ethmac": 0xb0000000,
-    }}
-
+class EtherboneSoC(BaseSoC):
     def __init__(self, platform, *args, **kwargs):
         # Need a larger integrated ROM on or1k to fit the BIOS with TFTP support.
         if 'integrated_rom_size' not in kwargs and kwargs.get('cpu_type', 'lm32') != 'lm32':
@@ -19,23 +20,22 @@ class NetSoC(BaseSoC):
         BaseSoC.__init__(self, platform, *args, **kwargs)
 
         # Ethernet ---------------------------------------------------------------------------------
-        # Ethernet PHY
+        # Ethernet Phy
         self.submodules.ethphy = LiteEthPHY(
-            platform.request("eth_clocks"),
-            platform.request("eth"))
+            clock_pads = self.platform.request("eth_clocks"),
+            pads       = self.platform.request("eth"))
         self.add_csr("ethphy")
-
-        # Ethernet MAC
-        ethmac_win_size = 0x2000
-        self.submodules.ethmac = LiteEthMAC(
-            phy        = self.ethphy,
-            dw         = 32,
-            interface  = "wishbone",
-            endianness = self.cpu.endianness)
-        self.add_wb_slave(self.mem_map["ethmac"], self.ethmac.bus, ethmac_win_size)
-        self.add_memory_region("ethmac", self.mem_map["ethmac"], ethmac_win_size, type="io")
-        self.add_csr("ethmac")
-        self.add_interrupt("ethmac")
+        # Ethernet Core
+        etherbone_mac_address = 0x10e2d5000000
+        etherbone_ip_address  = "192.168.100.50"
+        self.submodules.ethcore = LiteEthUDPIPCore(
+            phy         = self.ethphy,
+            mac_address = etherbone_mac_address,
+            ip_address  = etherbone_ip_address,
+            clk_freq    = self.clk_freq)
+        # Etherbone Core
+        self.submodules.etherbone = LiteEthEtherbone(self.ethcore.udp, 1234)
+        self.add_wb_master(self.etherbone.wishbone.bus)
         # timing constraints
         self.platform.add_period_constraint(self.ethphy.crg.cd_eth_rx.clk, 1e9/25e6)
         self.platform.add_period_constraint(self.ethphy.crg.cd_eth_tx.clk, 1e9/25e6)
@@ -43,6 +43,15 @@ class NetSoC(BaseSoC):
             self.crg.cd_sys.clk,
             self.ethphy.crg.cd_eth_rx.clk,
             self.ethphy.crg.cd_eth_tx.clk)
+
+        # Analyzer ---------------------------------------------------------------------------------
+        #analyzer_signals = [
+        #    # FIXME: find interesting signals to probe
+        #    self.cpu.ibus,
+        #    self.cpu.dbus
+        #]
+        #self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 512)
+        #self.add_csr("analyzer")
 
     def configure_iprange(self, iprange):
         iprange = [int(x) for x in iprange.split(".")]
@@ -60,4 +69,4 @@ class NetSoC(BaseSoC):
             self.add_constant(s, e)
 
 
-SoC = NetSoC
+SoC = EtherboneSoC
