8c1f0709121edab59e3f90ed7624b6ba20299d03
Tim 'mithro' Ansell <me@mith.ro>
2020-01-17 13:36:38 +1000

    basys3: Cleanup and modernize.

--- targets/basys3/base.py
+++ targets/basys3/base.py
@@ -1,101 +1,82 @@
 # Support for the Digilent Arty Board
 from migen import *
-from migen.genlib.resetsync import AsyncResetSynchronizer
 
 from litex.soc.integration.soc_core import *
-from litex.soc.integration.soc_sdram import *
 from litex.soc.integration.builder import *
 
-from litedram.modules import MT41K128M16
-from litedram.phy import a7ddrphy
-from litedram.core import ControllerSettings
-
+from gateware import cas
 from gateware import info
-from gateware import led
 from gateware import spi_flash
 
-from targets.utils import csr_map_update, period_ns
+from targets.utils import period_ns
 
-
-class _CRG(Module):
-    def __init__(self, platform):
-        self.clock_domains.cd_sys = ClockDomain()
-        self.clock_domains.cd_sys4x = ClockDomain(reset_less=True)
-        self.clock_domains.cd_sys4x_dqs = ClockDomain(reset_less=True)
-        self.clock_domains.cd_clk200 = ClockDomain()
-        self.clock_domains.cd_clk50 = ClockDomain()
-
-        clk100 = platform.request("clk100")
-        rst = platform.request("user_btn")
-
-        pll_locked = Signal()
-        pll_fb = Signal()
-        self.pll_sys = Signal()
-        self.specials += [
-            Instance("PLLE2_BASE",
-                     p_STARTUP_WAIT="FALSE", o_LOCKED=pll_locked,
-
-                     # VCO @ 1600 MHz
-                     p_REF_JITTER1=0.01, p_CLKIN1_PERIOD=10.0,
-                     p_CLKFBOUT_MULT=16, p_DIVCLK_DIVIDE=1,
-                     i_CLKIN1=clk100, i_CLKFBIN=pll_fb, o_CLKFBOUT=pll_fb,
-
-                     # 100 MHz
-                     p_CLKOUT0_DIVIDE=16, p_CLKOUT0_PHASE=0.0,
-                     o_CLKOUT0=self.pll_sys,
-            ),
-            Instance("BUFG", i_I=self.pll_sys, o_O=self.cd_sys.clk),
-            AsyncResetSynchronizer(self.cd_sys, ~pll_locked | rst),
-        ]
+from .crg import _CRG
 
 
 class BaseSoC(SoCCore):
-    csr_peripherals = (
-        "spiflash",
-        "info",
-    )
-    csr_map_update(SoCCore.csr_map, csr_peripherals)
-
-    mem_map = {
-        "spiflash": 0x20000000,  # (default shadow @0xa0000000)
-    }
-    mem_map.update(SoCCore.mem_map)
+    mem_map = {**SoCCore.mem_map, **{
+        'spiflash': 0x20000000,
+    }}
 
     def __init__(self, platform, spiflash="spiflash_1x", **kwargs):
-        if 'integrated_rom_size' not in kwargs:
-            kwargs['integrated_rom_size']=0x8000
-        if 'integrated_sram_size' not in kwargs:
-            kwargs['integrated_sram_size']=0x8000
+        kwargs['integrated_rom_size']=0x8000
+        kwargs['integrated_sram_size']=0x8000
 
-        clk_freq = int(100e6)
-        SoCCore.__init__(self, platform, clk_freq, **kwargs)
+        sys_clk_freq = int(100e6)
+        # SoCCore ----------------------------------------------------------------------------------
+        SoCCore.__init__(self, platform, clk_freq=sys_clk_freq, **kwargs)
 
-        self.submodules.crg = _CRG(platform)
+        # CRG --------------------------------------------------------------------------------------
+        self.submodules.crg = _CRG(platform, sys_clk_freq)
         self.crg.cd_sys.clk.attr.add("keep")
-        self.platform.add_period_constraint(self.crg.cd_sys.clk, period_ns(clk_freq))
+        self.platform.add_period_constraint(self.crg.cd_sys.clk, period_ns(sys_clk_freq))
 
-        # Basic peripherals
+        # Basic peripherals ------------------------------------------------------------------------
         self.submodules.info = info.Info(platform, self.__class__.__name__)
+        self.add_csr("info")
+        self.submodules.cas = cas.ControlAndStatus(platform, sys_clk_freq)
+        self.add_csr("cas")
 
-        # spi flash
+        # Add debug interface if the CPU has one ---------------------------------------------------
+        if hasattr(self.cpu, "debug_bus"):
+            self.register_mem(
+                name="vexriscv_debug",
+                address=0xf00f0000,
+                interface=self.cpu.debug_bus,
+                size=0x100)
+
+        # Memory mapped SPI Flash ------------------------------------------------------------------
         spiflash_pads = platform.request(spiflash)
         spiflash_pads.clk = Signal()
-        self.specials += Instance("STARTUPE2",
-                                  i_CLK=0, i_GSR=0, i_GTS=0, i_KEYCLEARB=0, i_PACK=0,
-                                  i_USRCCLKO=spiflash_pads.clk, i_USRCCLKTS=0, i_USRDONEO=1, i_USRDONETS=1)
+        self.specials += Instance(
+            "STARTUPE2",
+            i_CLK=0, i_GSR=0, i_GTS=0, i_KEYCLEARB=0, i_PACK=0,
+            i_USRCCLKO=spiflash_pads.clk, i_USRCCLKTS=0, i_USRDONEO=1, i_USRDONETS=1)
         spiflash_dummy = {
             "spiflash_1x": 9,
             "spiflash_4x": 11,
         }
         self.submodules.spiflash = spi_flash.SpiFlash(
-                spiflash_pads,
-                dummy=spiflash_dummy[spiflash],
-                div=2)
-        self.add_constant("SPIFLASH_PAGE_SIZE", 256)
-        self.add_constant("SPIFLASH_SECTOR_SIZE", 0x10000)
-        self.add_wb_slave(mem_decoder(self.mem_map["spiflash"]), self.spiflash.bus)
+            spiflash_pads,
+            dummy=spiflash_dummy[spiflash],
+            div=2,
+            endianness=self.cpu.endianness)
+        self.add_csr("spiflash")
+        self.add_constant("SPIFLASH_PAGE_SIZE", platform.spiflash_page_size)
+        self.add_constant("SPIFLASH_SECTOR_SIZE", platform.spiflash_sector_size)
+        self.add_constant("SPIFLASH_TOTAL_SIZE", platform.spiflash_total_size)
+        self.add_wb_slave(
+            self.mem_map["spiflash"],
+            self.spiflash.bus,
+            platform.spiflash_total_size)
         self.add_memory_region(
-            "spiflash", self.mem_map["spiflash"], 16*1024*1024)
+            "spiflash",
+            self.mem_map["spiflash"],
+            platform.spiflash_total_size)
+
+        bios_size = 0x8000
+        self.flash_boot_address = self.mem_map["spiflash"]+platform.gateware_size+bios_size
+        self.add_constant("FLASH_BOOT_ADDRESS", self.flash_boot_address)
 
 
 SoC = BaseSoC
