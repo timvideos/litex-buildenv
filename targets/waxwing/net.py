61d4a8502a5b5ec53e3679e1037e7e005f282056
Tim 'mithro' Ansell <me@mith.ro>
2020-02-03 20:15:57 +0100

    waxwing

--- targets/waxwing/net.py
+++ targets/waxwing/net.py
@@ -35,10 +35,8 @@ class BaseSoC(SoCSDRAM):
     }}
 
     def __init__(self, platform, **kwargs):
-        if 'integrated_rom_size' not in kwargs:
-            kwargs['integrated_rom_size']=0x8000
-        if 'integrated_sram_size' not in kwargs:
-            kwargs['integrated_sram_size']=0x8000
+        kwargs['integrated_rom_size']=0x8000
+        kwargs['integrated_sram_size']=0x8000
 
         clk_freq = (31 + Fraction(1, 4))*1000*1000
         SoCSDRAM.__init__(self, platform, clk_freq, **kwargs)
