334bde994ff73fdc43ff5478a1ab7c174f16cd8a
Tim 'mithro' Ansell <me@mith.ro>
2020-03-05 17:03:05 +0100

    neso: Cleanup and modernize.

--- targets/neso/base.py
+++ targets/neso/base.py
@@ -13,7 +13,7 @@ from litedram.core import ControllerSettings
 from gateware import info
 from gateware import spi_flash
 
-from targets.utils import csr_map_update, period_ns, dict_set_max
+from targets.utils import period_ns, dict_set_max
 
 
 class _CRG(Module):
@@ -84,17 +84,9 @@ class _CRG(Module):
 
 
 class BaseSoC(SoCSDRAM):
-    csr_peripherals = (
-        "spiflash",
-        "ddrphy",
-        "info",
-    )
-    csr_map_update(SoCSDRAM.csr_map, csr_peripherals)
-
-    mem_map = {
-        "spiflash": 0x20000000,  # (default shadow @0xa0000000)
-    }
-    mem_map.update(SoCSDRAM.mem_map)
+    mem_map = {**SoCSDRAM.mem_map, **{
+        "spiflash": 0x20000000,
+    }}
 
     def __init__(self, platform, spiflash="spiflash_1x", **kwargs):
         dict_set_max(kwargs, 'integrated_rom_size', 0x8000)
@@ -109,6 +101,7 @@ class BaseSoC(SoCSDRAM):
 
         # Basic peripherals
         self.submodules.info = info.Info(platform, self.__class__.__name__)
+        self.add_csr("info")
 
         # spi flash
         spiflash_pads = platform.request(spiflash)
@@ -124,6 +117,7 @@ class BaseSoC(SoCSDRAM):
                 spiflash_pads,
                 dummy=spiflash_dummy[spiflash],
                 div=2)
+        self.add_csr("spiflash")
         self.add_constant("SPIFLASH_PAGE_SIZE", 256)
         self.add_constant("SPIFLASH_SECTOR_SIZE", 0x10000)
         self.add_wb_slave(self.mem_map["spiflash"], self.spiflash.bus)
@@ -135,6 +129,7 @@ class BaseSoC(SoCSDRAM):
         sdram_module = MT41K128M16(self.clk_freq, "1:4")
         self.submodules.ddrphy = a7ddrphy.A7DDRPHY(
             platform.request("ddram"))
+        self.add_csr("ddrphy")
         self.add_constant("READ_LEVELING_BITSLIP", 3)
         self.add_constant("READ_LEVELING_DELAY", 14)
         controller_settings = ControllerSettings(
