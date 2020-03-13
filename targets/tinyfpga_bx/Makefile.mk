abeeb9a8d097ad5bcaba348e8ea4f688fed78730
Ewen McNeill <ewen@naos.co.nz>
2020-03-13 21:21:25 +1300

    tinyfpga: Makefile: fix auto-build none firmware override
    Now that the target Makefile is included *before* the IMAGE_FILE is redefined,
    if FIRMWARE has been changed we will be using an old version of IMAGE_FILE from
    the environment, and thus were inadvertently generating two rules to build
    the "none" firmware bundle if FIRMWARE=none.

--- targets/tinyfpga_bx/Makefile.mk
+++ targets/tinyfpga_bx/Makefile.mk
@@ -29,7 +29,13 @@ gateware-load-$(PLATFORM):
 
 # As with Mimasv2, if the user asks to flash the gateware only, the BIOS must
 # be sent as well (because the BIOS is too big to fit into the bitstream).
+#
+# We have to pre-calculate what the image file will end up being, as we are
+# included before it has been defined (to get the default target), so we'll
+# end up comparing with an empty string/older value from the environment.
+#
 GATEWARE_BIOS_FILE = $(TARGET_BUILD_DIR)/image-gateware+bios+none.bin
+IMAGE_FW_FILE      = $(TARGET_BUILD_DIR)/image-gateware+bios+$(FIRMWARE).bin
 
 gateware-flash-$(PLATFORM): $(GATEWARE_BIOS_FILE)
 	tinyprog $(PROG_EXTRA_ARGS) --program-image $(GATEWARE_BIOS_FILE)
@@ -38,7 +44,7 @@ gateware-flash-$(PLATFORM): $(GATEWARE_BIOS_FILE)
 # already built a image-gateware+bios+none.bin, we call make recursively
 # to build one here, with the FIRMWARE=none override.
 #
-ifneq ($(GATEWARE_BIOS_FILE),$(IMAGE_FILE))
+ifneq ($(GATEWARE_BIOS_FILE),$(IMAGE_FW_FILE))
 $(GATEWARE_BIOS_FILE): $(GATEWARE_FILEBASE).bin $(BIOS_FILE) mkimage.py
 	FIRMWARE=none make image
 endif
