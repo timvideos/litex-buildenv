421a402a68824dda81395c58675c1d55c21a0261
Mateusz Holenko <mholenko@antmicro.com>
2020-04-03 11:02:18 +0200

    micropython: temporarily change the fork
    The new fork containt a commit removing an outdated `hw`
    directory generated by LiteX.

--- scripts/build-micropython.sh
+++ scripts/build-micropython.sh
@@ -48,7 +48,7 @@ MPY_SRC_DIR=$TOP_DIR/third_party/micropython
 if [ ! -d "$MPY_SRC_DIR" ]; then
 	(
 		cd $(dirname $MPY_SRC_DIR)
-		git clone https://github.com/fupy/micropython.git
+		git clone -b modern-litex https://github.com/ewenmcneill/fupy-micropython.git micropython
 		cd $MPY_SRC_DIR
 		git submodule update --init
 	)
